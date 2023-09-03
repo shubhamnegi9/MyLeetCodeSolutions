97c9b444172acf2f6a56e8cc1989f6d86bc53dc0
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2023-09-03 21:41:52 +0530

    Time: 15 ms (67.74%) | Memory: 19.6 MB (70.98%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> mpp;
-        int n = nums.size();
+    int n = nums.size();
+		int ele;
+		int count = 0;
 
-        for(int i = 0; i < n; i++) {
-            mpp[nums[i]]++;
-        }
-
-        int majorityEle = -1;
-        for(auto it: mpp){
-            if(it.second > n/2) {
-                majorityEle = it.first;
-            }
-        }
-        return majorityEle;
-    }
+		// Applying Moore's Voting Algo to find majority element 'ele'
+		for(int i = 0; i < n; i++) {
+			
+			if(count == 0) {
+				count = 1;
+				ele = nums[i];
+			}
+			else if(nums[i] == ele) {
+				count++;
+			}
+			else {
+				count--;
+			}
+			
+		}
+		
+		int cnt;
+		// Verifying if the 'ele' is majority element or not.
+		// This can be skipped and we can return 'ele' if given that array always has majority element.
+		for(int i = 0; i < n; i++) {
+			if(nums[i] == ele) {
+				cnt++;
+			}
+		}
+		if(cnt > n/2) {
+			return ele;
+		}
+		return -1;
+  }
 };
