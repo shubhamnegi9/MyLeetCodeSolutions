1b40f92da50416dd27304a411c24faf60e7c2bcf
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2023-12-02 20:46:49 +0530

    Time: 16 ms (18.42%) | Memory: 13.7 MB (76.19%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,24 +3,55 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
 
+        // Conversions:
+        // vector<int> row(n, 0);   --> matrix[..][0]
+        // vector<int> col(m, 0);   --> matrix[0][..]
+
+        int col0 = 1;
+        // Iterating and marking row and column vector elements as 0s
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+                    // marking the ith row element as 0
+                    matrix[i][0] = 0;
+                    // marking the jth column element as 0 if it is not first element
+                    // otherwise marking col0 as 0
+                    if(j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
                 }
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(row[i] == 1 || col[j] == 1) {
-                    matrix[i][j] = 0;
+        // Iterating and marking remaining elements as 0s
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+                if(matrix[i][j] != 0) {
+                    // Checking in row and column vectors for 0s
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
                 }
             }
         }
+
+        // If matrix[0][0] is 0, then all the column vector elements are 0s 
+        // (Need to mark for column vector elements first before row vector elements)
+        if(matrix[0][0] == 0) {
+            for(int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // If col0 is 0, then all the row vector elements are 0s
+        if(col0 == 0) {
+            for(int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+
     }
 };
