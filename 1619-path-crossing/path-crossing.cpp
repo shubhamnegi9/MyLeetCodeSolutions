b04a14adc2096be7887ea53a234934dbda38cb7b
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2023-12-25 11:58:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (64.59%) - LeetSync

--- 1619-path-crossing/path-crossing.cpp
+++ 1619-path-crossing/path-crossing.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     bool isPathCrossing(string path) {
         pair<int, int> p = {0, 0};
+        // Stores visited points in path
         set<pair<int, int>> visited;
+        // Adding origin in visited set
         visited.insert(p);
+
         for(auto ch: path) {
             if(ch == 'N') {
                 p.second+=1;
@@ -18,10 +21,12 @@ public:
                 p.second-=1;
             }
 
+            // If point is already found in visited set
             if(visited.find(p) != visited.end()) {
                return true;
             }
 
+            // Add current point to visited set
             visited.insert(p);
         }
         return false;
