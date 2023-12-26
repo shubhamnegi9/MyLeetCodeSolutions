021374d5a0ec4031a3e650ae4e99b0c616bf16b2
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2023-12-26 23:10:42 +0530

    Time: 36 ms (29.67%) | Memory: 10.8 MB (22.31%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -35,8 +35,9 @@ public:
         return ans;
     }
 
-    // Using Memoization 
-    long long numRollsToTargetHelper(int dices, int faces, int target, vector<vector<long long>>& dp) {
+
+    // Using Memoization (Top-Down Approach)
+    long long numRollsToTargetMemo(int dices, int faces, int target, vector<vector<long long>>& dp) {
         
         // Base cases
 
@@ -70,10 +71,41 @@ public:
         for(int i = 1; i <= faces; i++) {
             // For the next roll dice will decrease by 1 
             // and target will decrease by (target - current face value)
-            ans += (numRollsToTargetHelper(dices-1, faces, target-i, dp) % 1000000007);
+            ans += (numRollsToTargetMemo(dices-1, faces, target-i, dp) % 1000000007);
         }
 
         return dp[dices][target] = ans;
+    }
+
+
+    // Using Bottom-Up Approach
+    long long numRollsToTargetBottomUp(int dices, int faces, int target) {
+        
+        // created 2D array for DP and initialize all with 0 
+        // since for invalid dices and target value we returned 0
+        vector<vector<long long>> dp(dices+1, vector<long long>(target+1, 0));
+
+        // If no dice and target is also 0 then return 1
+        dp[0][0] = 1;
+
+        // Iterating in dices and faces
+        // in bottom-up way
+        for(int d = 1; d <= dices; d++) {
+            for(int t = 1; t <= target; t++) {
+                long long ans = 0;
+                // Iterating over each of the faces
+                for(int i = 1; i <= faces; i++) {
+                    // For the next roll dice will decrease by 1 
+                    // and target will decrease by (target - current face value)
+                    if(t-i >= 0)
+                        ans += dp[d-1][t-i] % 1000000007;
+
+                }
+                dp[d][t] = ans;
+            }
+        }
+
+        return dp[dices][target];
     }
 
 
@@ -81,11 +113,18 @@ public:
         // Using Recursive Way:
         // return numRollsToTargetRecursive(n, k, target);
         
-        // Using Memoization:
-        // Since 2 variables dice and target changes in 
-        // recursive way, so we created 2D array for DP
-        vector<vector<long long>> dp(n+1, vector<long long>(target+1, -1));
-        long long ans = numRollsToTargetHelper(n, k, target, dp);
+        /* Using Memoization:
+        Since 2 variables 'dices' and 'target' changes in 
+        recursive way, so we created 2D array for DP
+        and initialize all with -1
+        */
+
+        // vector<vector<long long>> dp(n+1, vector<long long>(target+1, -1));
+        // long long ans = numRollsToTargetMemo(n, k, target, dp);
+        // return ans % (1000000007);
+
+        // Using Bottom-Up Approach:
+        long long ans = numRollsToTargetBottomUp(n, k, target);
         return ans % (1000000007);
     }
 };
