087459ddbaea4f7657cce33aba1dffbb6b794361
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2023-12-29 00:11:50 +0530

    Time: 679 ms (55.06%) | Memory: 115.2 MB (38.20%) - LeetSync

--- 1637-string-compression-ii/string-compression-ii.cpp
+++ 1637-string-compression-ii/string-compression-ii.cpp
@@ -10,7 +10,35 @@ public:
     // can cause stack overflow
     // int dp[101][27][101][101];   
 
-    int solve(string &s, int i, int prev, int freq, int k ){
+    // Recursive Way
+    int solve(string &s, int i, int prev_ch, int prev_freq, int k) {
+        // Base Cases
+        if(i >= s.length()) {
+            return 0;
+        }
+        if(k < 0) {
+            return INT_MAX;
+        }
+
+        int delete_i = solve(s, i+1, prev_ch, prev_freq, k-1);
+
+        int keep_i = 0;
+        if(s[i] - 'a' != prev_ch) {
+            keep_i = 1 + solve(s, i+1, s[i] - 'a', 1, k); 
+        }
+        else {
+            int one_more_addition = 0;
+            if(prev_freq == 1 || prev_freq == 9 || prev_freq == 99) {
+                one_more_addition = 1;
+            }
+            keep_i = one_more_addition + solve(s, i+1, prev_ch, prev_freq+1, k);
+        }
+
+        return min(delete_i, keep_i);
+    }
+
+    // Using Memoization (Top-Down Approach)
+    int solveMem(string &s, int i, int prev, int freq, int k ){
         if(k < 0)
             return INT_MAX;
         
@@ -21,7 +49,7 @@ public:
             return dp[i][prev][freq][k];
         }
         
-        int delete_i = solve(s, i+1, prev, freq, k-1);
+        int delete_i = solveMem(s, i+1, prev, freq, k-1);
         
         int keep_i   = 0;
         
@@ -30,9 +58,9 @@ public:
              if(freq == 1 || freq == 9 || freq == 99) {
                  one_more_added = 1;
              }
-            keep_i = one_more_added + solve(s, i+1, prev, freq+1, k);
+            keep_i = one_more_added + solveMem(s, i+1, prev, freq+1, k);
         } else {
-            keep_i = 1 + solve(s, i+1, s[i]-'a', 1, k);
+            keep_i = 1 + solveMem(s, i+1, s[i]-'a', 1, k);
         }
         
         return dp[i][prev][freq][k] = min(delete_i, keep_i);
@@ -44,6 +72,6 @@ public:
 
         // Using Memoization (Top-Down Approach):
         memset(dp, -1, sizeof(dp));     // Initialized all value to -1
-        return solve(s, 0, 26, 0, k);    // Passed prev_char as 'z' in ASCII initially
+        return solveMem(s, 0, 26, 0, k);    // Passed prev_char as 'z' in ASCII initially
     }
 };
