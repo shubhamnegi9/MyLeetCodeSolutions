9c04bf35c968e731c001f0506f761976c2fbe477
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2023-12-30 19:47:13 +0530

    Time: 22 ms (12.84%) | Memory: 12.7 MB (21.01%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool makeEqual(vector<string>& words) {
+    bool makeEqualUsingMap(vector<string>& words) {
         // Using Unordered Map
         unordered_map<char,int> count;
         int n = words.size();
@@ -22,5 +22,35 @@ public:
         }
 
         return true;
+    }
+
+    bool makeEqualUsingArray(vector<string>& words) {
+        // Using Array/Vector
+        vector<int> count(26, 0);
+        int n = words.size();
+
+        for(string str: words) { 
+            for(char ch: str) {
+                // Converting to ASCII value and storing
+                count[ch - 'a']++;
+            }
+        }
+
+        for(auto freq: count) {
+            // If count of character is not divisible
+            // by n, then the character cannot be 
+            // distributed among each word in words array 
+            if(freq % n != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool makeEqual(vector<string>& words) {
+        // return makeEqualUsingMap(words);
+        
+        return makeEqualUsingArray(words);
     }
 };
