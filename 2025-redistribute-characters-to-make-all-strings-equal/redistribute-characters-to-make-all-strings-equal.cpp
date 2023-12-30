7aa8c6786ccc7e7c95f735883d41394fc0e7ea8f
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2023-12-30 20:11:02 +0530

    Time: 17 ms (35.80%) | Memory: 12.5 MB (44.75%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -48,9 +48,48 @@ public:
         return true;
     }
 
+    // Using STL and lambda
+    bool makeEqualUsingSTLLambda(vector<string>& words) {
+        vector<int> count(26, 0);
+        int n = words.size();
+
+        for(string str: words) { 
+            for(char ch: str) {
+                // Converting to ASCII value and storing
+                count[ch - 'a']++;
+            }
+        }
+
+        // Lambda function passed to all_of() STL
+        /*
+            Since 'count' vector contains integer values so
+            we passed 'int freq' as parameter to lambda function.
+            Since 'n' is not passed to lambda function and is 
+            taken from outside the lambda function, therefore
+            we passed '&' symbol inside []
+        */
+        auto lambda  = [&] (int freq) {
+            return (freq % n) == 0;
+        };  // don't forget semicolon here
+
+        // all_of() STL checks whether all the elements in 
+        // the container ('count' here) satisfies a given
+        // condition (written in lambda) 
+        return all_of(count.begin(), count.end(), lambda);
+
+        // Short hand notation for lambda function:
+        // return all_of(count.begin(), count.end(), [&] (int freq) {
+        //     return (freq % n) == 0;
+        // });
+    }
+
     bool makeEqual(vector<string>& words) {
         // return makeEqualUsingMap(words);
+
+        // return makeEqualUsingArray(words);
+
+        return makeEqualUsingSTLLambda(words);
+
         
-        return makeEqualUsingArray(words);
     }
 };
