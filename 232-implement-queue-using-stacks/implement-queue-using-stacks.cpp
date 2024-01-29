84235bb666855af4efa40a08b658b8b00fa99b6a
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2024-01-29 23:26:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (15.21%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -2,47 +2,49 @@ class MyQueue {
 public:
     stack<int> s1;
     stack<int> s2;
+    int peekEle;
     
     MyQueue() {
         
     }
     
+    // T.C. = O(1), S.C. = O(n)
     void push(int x) {
-         while(!s2.empty()) {
-            int ele = s2.top();
-            s2.pop();
-            s1.push(ele);
-        }
-        s1.push(x);
+        // Saving first element in s1 as peek element
+        if(s1.empty())
+            peekEle = x;
+        s1.push(x);   // T.C. = O(1)
     }
     
+    // Amortized T.C. = O(1)    (Most of the operations taken O(1) compared to O(n))
+    // S.C. = O(1)
     int pop() {
-        while(!s1.empty()) {
-            int ele = s1.top();
-            s1.pop();
-            s2.push(ele);
+        if(s2.empty()) {           // T.C. = O(n)
+            // Move all elements from s1 to s2
+            while(!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
         }
-        int popped = s2.top();
-        s2.pop();
-        return popped;
+
+        int poppedEle = s2.top();   // T.C. = O(1)
+        s2.pop();                   
+        return poppedEle;
     }
     
+    // T.C. = O(1), S.C. = O(1)
     int peek() {
-        while(!s1.empty()) {
-            int ele = s1.top();
-            s1.pop();
-            s2.push(ele);
+        // If s2 is not empty, return top of s2
+        if(!s2.empty()) {       
+            return s2.top();    // T.C. = O(1)
         }
-        return s2.top();
+
+        return peekEle;         // T.C. = O(1)
     }
     
+    // T.C. = O(1), S.C. = O(1)
     bool empty() {
-        while(!s1.empty()) {
-            int ele = s1.top();
-            s1.pop();
-            s2.push(ele);
-        }
-        return s2.empty();
+        return s1.empty() && s2.empty();
     }
 };
 
