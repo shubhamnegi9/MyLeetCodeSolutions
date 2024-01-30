703d73145343f51119c7944e9294ffe9c0a33f95
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2024-01-30 22:45:47 +0530

    Time: 8 ms (70.52%) | Memory: 15.6 MB (5.26%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
+    // Basic Approach Using Stack
+    int evalRPNUsingStack(vector<string>& tokens) {
         stack<int> st;
         for(string &tk: tokens) {
             if(tk != "/" && tk != "*" && tk != "+" && tk != "-") {
@@ -24,5 +25,42 @@ public:
             }
         }
         return st.top();
+    }
+
+    // Using fancy lambda on unordered map
+    /*
+        Syntax:
+        unordered_map<key, function<return_type(input_type, input_type)>>
+    */
+    unordered_map<string, function<int(int, int)>> mpp =
+        {
+            {"+", [] (int a, int b) {return a+b;}},
+            {"-", [] (int a, int b) {return a-b;}},
+            {"*", [] (int a, int b) {return a*b;}},
+            {"/", [] (int a, int b) {return a/b;}}
+        };
+
+    int evalRPNUsingStackAndLambda(vector<string>& tokens) {
+        stack<int> st;
+        for(string &tk: tokens) {
+            if(tk != "/" && tk != "*" && tk != "+" && tk != "-") {
+                st.push(stoi(tk));
+            }
+            else {
+                int opr1 = st.top();
+                st.pop();
+                int opr2 = st.top();
+                st.pop();
+                int res = mpp[tk](opr2, opr1);  // mpp[key](input, input)
+                st.push(res);
+            }
+        }
+        return st.top();
+    }
+
+    int evalRPN(vector<string>& tokens) {
+       // return evalRPNUsingStack(tokens);
+
+        return evalRPNUsingStackAndLambda(tokens);
     }
 };
