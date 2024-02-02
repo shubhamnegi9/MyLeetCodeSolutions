077e560c2c100343d2be8cb1f680d3f174530719
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2024-02-02 23:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (13.81%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
 
+    // T.C. = O(n), S.C. = O(n)
+    // where n are number of sequential digits between low and high
     vector<int> sequentialDigitsUsingBFS(int low, int high) {
         queue<int> q;
 
@@ -24,8 +26,35 @@ public:
 
         return result;
     }
+    
+    // T.C. = O(1), S.C. = O(1)
+    vector<int> sequentialDigitsOptimal(int low, int high) {
+        vector<int> allPossible = {12, 23, 34, 45, 56, 67, 78, 89,
+                          123, 234, 345, 456, 567, 678, 789,
+                          1234, 2345, 3456, 4567, 5678, 6789,
+                          12345, 23456, 34567, 45678, 56789,
+                          123456, 234567, 345678, 456789,
+                          1234567, 2345678, 3456789,
+                          12345678, 23456789,
+                          123456789};
+        
+        vector<int> result;
+        int n = allPossible.size();
+        for(int i = 0; i < n; i++) {
+            if(allPossible[i] < low) 
+                continue;
+            else if(allPossible[i] > high) 
+                break;
+            else
+                result.push_back(allPossible[i]);
+        }
+
+        return  result;
+    }
 
     vector<int> sequentialDigits(int low, int high) {
-        return sequentialDigitsUsingBFS(low, high);
+        // return sequentialDigitsUsingBFS(low, high);
+
+        return sequentialDigitsOptimal(low, high);
     }
 };
