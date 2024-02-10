bc9de8006eb6f2d9393d48e02565fa8cc5a1176c
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2024-02-10 20:03:15 +0530

    Time: 25 ms (26.84%) | Memory: 11.9 MB (52.79%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    bool isPalindromic(string s) {
-        int i = 0, j = s.size()-1;
+    bool isPalindromicIterative(int i, int j, string s) {
         while(i <= j) {
             if(s[i] != s[j])
                 return false;
@@ -12,19 +11,92 @@ public:
         return true;
     }
 
-    int countSubstrings(string s) {
-        int n = s.size();
+    bool isPalindromicRecursive(int i, int j, string &s) {
+        
+        if(i > j) 
+            return true;
+        
+        if(s[i] == s[j])
+            return isPalindromicRecursive(i+1, j-1, s);
 
+        return false;
+
+    }
+
+    bool isPalindromicMemo(int i, int j, string &s, int dp[][1001]) {
+        
+        if(i > j) 
+            return true;
+        
+        /*
+            dp[i][j] stores:
+            -1 -> If not solved yet
+            1 -> If palindromic
+            0 - > If not palindromic
+        */
+        if(dp[i][j] != -1)
+            return dp[i][j];    
+        
+        if(s[i] == s[j])
+            return dp[i][j] = isPalindromicMemo(i+1, j-1, s, dp);
+
+        return dp[i][j] = false;
+
+    }
+
+    // Iterative Way
+    int countSubstringsIterative(string s ){
+        int n = s.size();
         int count = 0;
         for(int i = 0; i < n; i++) {
-            string temp = "";
             for(int j = i; j < n; j++) {
-                temp+=s[j];
-                if(isPalindromic(temp))
+                if(isPalindromicIterative(i, j, s))
                     count++;
             }
         }
 
         return count;
+    }
+
+    // Recursive Way
+    int countSubstringsRecursive(string s ){
+        int n = s.size();
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(isPalindromicRecursive(i, j, s))
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    // Recursion + Memoization
+    int countSubstringsMemo(string s) {
+        int n = s.size();
+        int count = 0;
+        int dp[1001][1001];
+        memset(dp, -1, sizeof(dp));
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(isPalindromicMemo(i, j, s, dp))
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    int countSubstrings(string s) {
+        // Iterative Way
+        // return countSubstringsIterative(s);
+
+        // Recursive Way
+        // return countSubstringsRecursive(s);
+
+        // Recursion + Memoization
+        return countSubstringsMemo(s);
+    
     }
 };
