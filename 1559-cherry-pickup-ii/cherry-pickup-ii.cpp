0f02a3c564f7797813c48b743af71645911be524
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2024-02-11 21:22:58 +0530

    Time: 35 ms (94.60%) | Memory: 42.9 MB (5.03%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -4,6 +4,8 @@ public:
     // Taking 3D array because 3 variables are changing - rowIndex, colIndex1, colIndex2
     int dp[71][71][71];
 
+    // Using Recursion + Memoization
+    // Top Down Approach
     int solve(vector<vector<int>>& grid, int rowIndex, int colIndex1, int colIndex2, int m, int n) {
 
         // Base Case
@@ -41,6 +43,101 @@ public:
         return dp[rowIndex][colIndex1][colIndex2] = cherrySum+ans;
     }
 
+    // Using Bottom Up Approach
+    int solveBottomUp(vector<vector<int>>& grid, int m, int n) {
+        
+        // Taking 3D array because 3 variables are changing - rowIndex, colIndex1, colIndex2
+        int t[71][71][71] = {0};
+
+        // t[row][col1][col2] represents maximum cherries collected by robot1 till (row, col1) 
+        // and robot2 till (row, col2)
+        
+        // Filling for first row in t. Robot1 standing at 0th column and Robot2 standing at (n-1)th column
+        t[0][0][n-1] = (n==1) ? grid[0][0] : grid[0][0] + grid[0][n-1];     // Checking for same cell and picking cherry only one time
+
+        // Filling for remaining rows in t
+        for(int row = 1; row < m; row++) {
+            for(int col1 = 0; col1 <= min(row, n-1); col1++) {  // min(row, n-1) for checking bounds
+                for(int col2 = max(0, n-row-1); col2 < n; col2++) {    // max(0, n-row-1) for checking bounds
+                    
+                    // Checking for all the possibilities from previous row and finding maximum
+                    int prevRowMax = 0;
+                    for(int prevCol1 = max(0,col1-1); prevCol1 <= min(col1+1, n-1); prevCol1++) {   // max(0,col1-1) and min(col1+1, n-1) for checking bounds
+                        for(int prevCol2 = max(0, col2-1); prevCol2 <= min(col2+1, n-1); prevCol2++) {  // max(0, col2-1) and min(col2+1, n-1) for checking bounds
+                            prevRowMax = max(prevRowMax, t[row-1][prevCol1][prevCol2]);
+                        }
+                    }
+
+                    if(col1 == col2) {  // Checking for same cell and picking cherry only one time
+                        t[row][col1][col2] = prevRowMax + grid[row][col1];
+                    }
+                    else {
+                        t[row][col1][col2] = prevRowMax + grid[row][col1] + grid[row][col2];
+                    }
+                }
+            }
+        }
+
+        // Iterating in the last row of 't' and finding the maximum cherry sum
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                ans = max(ans, t[m-1][i][j]);
+            }
+        }
+
+        return ans;
+    } 
+
+    // Using Bottom Up Optimized Approach
+    int solveBottomUpOptimized(vector<vector<int>>& grid, int m, int n) {
+        
+        // Taking 2D array 'prev' for storing values for previous row/state
+        vector<vector<int>> prev(71, vector<int>(71, 0));
+        
+        // Filling for row in prev. Robot1 standing at 0th column and Robot2 standing at (n-1)th column
+        prev[0][n-1] = (n==1) ? grid[0][0] : grid[0][0] + grid[0][n-1];     // Checking for same cell and picking cherry only one time
+
+
+        for(int row = 1; row < m; row++) {
+            // 2D vector for current row
+            vector<vector<int>> curr(71, vector<int>(71, 0));
+
+            for(int col1 = 0; col1 <= min(row, n-1); col1++) {  // min(row, n-1) for checking bounds
+                for(int col2 = max(0, n-row-1); col2 < n; col2++) {    // max(0, n-row-1) for checking bounds
+                    
+                    // Checking for all the possibilities from previous row and finding maximum
+                    int prevRowMax = 0;
+                    for(int prevCol1 = max(0,col1-1); prevCol1 <= min(col1+1, n-1); prevCol1++) {   // max(0,col1-1) and min(col1+1, n-1) for checking bounds
+                        for(int prevCol2 = max(0, col2-1); prevCol2 <= min(col2+1, n-1); prevCol2++) {  // max(0, col2-1) and min(col2+1, n-1) for checking bounds
+                            prevRowMax = max(prevRowMax, prev[prevCol1][prevCol2]);
+                        }
+                    }
+
+                    if(col1 == col2) {  // Checking for same cell and picking cherry only one time
+                        curr[col1][col2] = prevRowMax + grid[row][col1];
+                    }
+                    else {
+                        curr[col1][col2] = prevRowMax + grid[row][col1] + grid[row][col2];
+                    }
+                }
+            }
+
+            prev = curr;    // Making current row as previous for next iteration
+        }
+
+        // Iterating in prev and finding the maximum cherry sum
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                ans = max(ans, prev[i][j]);
+            }
+        }
+
+        return ans;
+    } 
+
+
     int cherryPickup(vector<vector<int>>& grid) {
         // Number of Rows
         int m = grid.size();
@@ -50,7 +147,13 @@ public:
         // Using Recursion + Memoization
         // Starting from 0th row for both robots, and 0th column for first robot and (n-1)th
         // column for second robot 
-        memset(dp, -1, sizeof(dp));
-        return solve(grid, 0, 0, n-1, m, n);    
+        // memset(dp, -1, sizeof(dp));
+        // return solve(grid, 0, 0, n-1, m, n);    
+
+        // Using Bottom Up Approach
+        // return solveBottomUp(grid, m, n);
+
+        // Using Optimized Bottom Up Approach
+        return solveBottomUpOptimized(grid, m, n);
     }
 };
