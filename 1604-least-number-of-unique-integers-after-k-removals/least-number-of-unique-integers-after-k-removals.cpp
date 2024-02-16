cee64871bc98119afe1494d2524211d937318042
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2024-02-16 20:56:07 +0530

    Time: 109 ms (90.61%) | Memory: 65 MB (74.28%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -73,11 +73,57 @@ public:
         return 0;   // All the elements have been removed from freq array
     }
 
+    // Using Optimal Approach
+    // T.C. = O(n), S.C. = O(n)
+    int findLeastUsingOptimal(vector<int>& arr, int k) {
+
+        unordered_map<int, int> mpp;
+        int n = arr.size();
+        
+        // Storing counts of all elements in map
+        for(int ele: arr) {
+            mpp[ele]++;
+        }
+
+        // Count of unique elements
+        int uniqueElements = mpp.size();
+
+        // Vector for storing count of frequency in mpp
+        vector<int> freq(n+1);
+
+        for(auto it: mpp) {
+            freq[it.second]++;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            // Unique Types which can be deleted using k
+            int uniqueTypesDeleted = min(k/i, freq[i]);
+            
+            // Remaining elements to be deleted
+            k-=(uniqueTypesDeleted * i);
+
+            // Remaining unique elements left
+            uniqueElements -= uniqueTypesDeleted;
+            
+            if(k <=i) {
+                // elements can no longer be removed now
+                return uniqueElements;
+            }
+
+        }
+
+        return 0;   // All the elements have been removed 
+
+    }
+
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         // Using map + vector
         // return findLeastUsingMapAnndVector(arr, k);
 
         // Using map + priority queue
-        return findLeastUsingPriorityQueue(arr, k);
+        // return findLeastUsingPriorityQueue(arr, k);
+
+        // Using Optimal Approach
+        return findLeastUsingOptimal(arr, k);
     }
 };
