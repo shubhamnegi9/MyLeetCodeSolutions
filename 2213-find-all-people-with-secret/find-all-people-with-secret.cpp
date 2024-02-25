026758c003680606c84bcfb66dcd33122f50d69d
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-02-25 18:52:57 +0530

    Time: 490 ms (62.39%) | Memory: 198.9 MB (32.47%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     typedef pair<int, int> P;
-    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        // Unordered map for storing in increasing order of meeting times
-        map<int, vector<P>> meetingTimes;
+
+    // Approach 1 : Using ordered map of meeting times + BFS
+    /*  Time Complexity = O(m) for iterating in meetings vector * O(V+E) for BFS
+                        = O(m) * O(m+n)
+                        (m = number of meetings, n = number of persons)
+        Space Complexity = O(m+n) for queue in BFS + O(m) for meetingTimes map + O(n) for knowsSecret + O(n) for visited set
+                         = O(m+n) overall  
+    */
+    vector<int> findAllPeopleUtil1(int n, vector<vector<int>>& meetings, int firstPerson) {
+       // Ordered map for storing in increasing order of meeting times
+        map<int, vector<P>> meetingTimes;   // time -> [[firstPerson, secondPerson], ....], ...
 
         for(vector<int> meeting: meetings) {
             int firstPerson = meeting[0];
@@ -11,7 +19,7 @@ public:
             int time = meeting[2];
 
             // Storing meetings in increasing order of time as key
-            meetingTimes[time].push_back({firstPerson, secondPerson});
+            meetingTimes[time].push_back({firstPerson, secondPerson});  // 3 -> [[3, 1], [0, 3]], 2 -> [1, 2]... 
         }
 
         vector<bool> knowsSecret(n, false);
@@ -30,7 +38,7 @@ public:
             for(auto &[firstPerson, secondPerson]: meets) {
                 // Adding to adjacency list
                 // Both persons can share secret to each other
-                adj[firstPerson].push_back(secondPerson);
+                adj[firstPerson].push_back(secondPerson);   // 3 -> [1, 0], 1 -> [3, 2], 2 -> [1]... 
                 adj[secondPerson].push_back(firstPerson);
 
                 // If firstPerson knows secret and it is not added in queue the add it 
@@ -51,10 +59,11 @@ public:
             // Now we have the adjacency list of the meetings in sorted order of time
             // Now spread the secret using BFS
             while(!q.empty()) {
-                int person = q.front();
+                int person = q.front();  // Current person who knows the secret and will spread it further
                 q.pop();
 
                 for(auto &nextPerson: adj[person]) {
+                    // Spread secret to nextPerson only when he does not already know the secret
                     if(!knowsSecret[nextPerson]) {
                         knowsSecret[nextPerson] = true; // Secret is spread to nextPerson
                         q.push(nextPerson); // Adding nextPerson to queue so we can spread secret further from it
@@ -72,6 +81,81 @@ public:
         }
 
         return result;
+    }
+    
+    // Approach 2 : Without using ordered map of meeting times + BFS
+    /*
+        In approach 1, we were first creating ordered map of meeting times and then creating 
+        the adjacency list from it and doing BFS.
+        In this approach, we will directly create the adjacency list from meetings vector 
+        and do BFS.
+    */
+    vector<int> findAllPeopleUtil2(int n, vector<vector<int>>& meetings, int firstPerson) {
 
+        // Adjacency list for doing BFS
+        unordered_map<int, vector<P>> adj;  // firstPerson -> [[secondPerson, time], ...], ...
+
+        // Filling adjacency list
+        for(vector<int> meeting: meetings) {
+            int person1 = meeting[0];
+            int person2 = meeting[1];
+            int time = meeting[2];
+
+            // Both persons can share secret to each other
+            adj[person1].push_back({person2, time});
+            adj[person2].push_back({person1, time});
+        }
+
+        // Vector to store the minimum times when each person knows secret
+        vector<int> timeToKnowSecret(n, INT_MAX);
+        timeToKnowSecret[0] = 0;            // Person 0 knows the secret at time 0
+        timeToKnowSecret[firstPerson] = 0;  // firstPerson knows the secret at time 0
+
+        // Queue for BFS
+        queue<P> q; // Stores pair of {person, time}
+        // Adding person0 and firstPerson to queue so that we can start spreading secret from them 
+        q.push({0, 0});    
+        q.push({firstPerson, 0});
+
+        // Now spread the secret using BFS
+        while(!q.empty()) {
+            // person: Current person who knows the secret and will spread it further
+            // time: Time when the current person knows the secret
+            auto [person, time] = q.front();    
+            q.pop();
+
+            for(auto &ngbr: adj[person]) {
+                int nextPerson = ngbr.first;    // Next person to whom secret needs to be spread
+                int meetTime = ngbr.second;     // Time when the next person knows the secret
+
+                // Spread secret to nextPerson only when the time when the next person knows the secret
+                // is greater than or equal to time when the current person knows the secret, as well as
+                // it is less than the minimum time when the nextPerson knows the secret
+                if(meetTime >= time && meetTime < timeToKnowSecret[nextPerson]) {
+                    timeToKnowSecret[nextPerson] = meetTime;    // Update the minimum time when next person knows the secret
+                    q.push({nextPerson, meetTime});        // Adding nextPerson to queue so we can spread secret further from it
+                }
+            }
+        }
+
+        vector<int> result;
+        // Finally iterate in timeToKnowSecret vector and check who all knows the secret
+        for(int i = 0; i < n; i++) {
+            if(timeToKnowSecret[i] != INT_MAX) {
+                result.push_back(i);
+            }
+        }
+
+        return result;
+
+    }
+
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        
+        // Approach 1
+        // return findAllPeopleUtil1(n, meetings, firstPerson);
+
+        // Approach 2
+        return findAllPeopleUtil2(n, meetings, firstPerson);
     }
 };
