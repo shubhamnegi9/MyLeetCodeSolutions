80d755498c706e27fead14cd23f789f5333ba650
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-02-26 23:35:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (5.64%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -23,7 +23,7 @@ public:
 
         // Checking for values at nodes
         if(p->val != q->val) {
-            return false;
+            return false;   // Not Same Tree
         }
 
         // Checking for equalness of left subtree as well as right subtreee using recurison
@@ -31,8 +31,63 @@ public:
 
     }
 
+    // Using BFS
+    bool isSameTreeBFS(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL)
+            return true;    // Same Tree
+        
+        if(p == NULL || q == NULL) {    // Same as (p == NULL and q != NULL) || (p != NULL and q == NULL)
+            return false;   // Not Same Tree
+        }
+
+        // Queue for BFS traversal
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+
+        q1.push(p);
+        q2.push(q);
+
+        while(!q1.empty() && !q2.empty()) {
+            TreeNode* node1 = q1.front();
+            q1.pop();
+            TreeNode* node2 = q2.front();
+            q2.pop();
+
+            // Checking for values at nodes
+            if(node1->val != node2->val) {
+                return false;   // Not Same Tree
+            }
+            
+            // If left subtree of both nodes are not null, then push them into queue
+            if(node1->left != NULL && node2->left != NULL) {
+                q1.push(node1->left);
+                q2.push(node2->left);
+            }
+            // Left subtree of any node is null
+            else if(node1->left != NULL || node2->left != NULL) {
+                return false;   // Not Same Tree
+            }
+
+            // If right subtree of both nodes are not null, then push them into queue
+            if(node1->right != NULL && node2->right != NULL) {
+                q1.push(node1->right);
+                q2.push(node2->right);
+            }
+            // Right subtree of any node is null
+            else if(node1->right != NULL || node2->right != NULL) {
+                return false;   // Not Same Tree
+            }
+
+        }
+
+        return true;    // Same Tree
+    }
+
     bool isSameTree(TreeNode* p, TreeNode* q) {
         // Using DFS
-        return isSameTreeDFS(p, q);
+        // return isSameTreeDFS(p, q);
+
+        // Using BFS
+        return isSameTreeBFS(p, q);
     }
 };
