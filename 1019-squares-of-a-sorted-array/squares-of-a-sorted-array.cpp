a22d6922a4c48d9b632023e06ff143c3f92cad39
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-02 10:54:53 +0530

    Time: 19 ms (83.11%) | Memory: 28.2 MB (67.40%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,12 +1,50 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
+    // Brute Force
+    // T.C. = O(n) + O(nlogn) for sorting
+    // S.C. = O(1)
+    void sortedSquaresBrute(vector<int> &nums) {
         int n = nums.size();
         for(int i = 0; i < n; i++) {
-            int temp = nums[i];
-            nums[i] = temp * temp;
+            nums[i] = nums[i] * nums[i];
         }
         sort(nums.begin(), nums.end());
-        return nums;
+    }
+
+    // Optimal Approach Using 2 Pointer
+    // T.C. = O(n)
+    // S.C. = O(n)
+    vector<int> sortedSquaresOptimal(vector<int> &nums) {
+        int n = nums.size();
+        int i = 0, j = n-1;
+        vector<int> res(n);
+        int k = n-1;
+
+        while(k >= 0) {
+            int first = nums[i] * nums[i];
+            int second = nums[j] * nums[j];
+
+            // Putting greater of the two at end in res vector
+            if(first < second) {
+                res[k] = second;
+                j--;
+            }
+            else {
+                res[k] = first;
+                i++;
+            }
+            k--;
+        }
+
+        return res;
+    }
+
+    vector<int> sortedSquares(vector<int>& nums) {
+        // Brute Force
+        // sortedSquaresBrute(nums);
+        // return nums;
+        
+        // Optimal Approach
+        return sortedSquaresOptimal(nums);
     }
 };
