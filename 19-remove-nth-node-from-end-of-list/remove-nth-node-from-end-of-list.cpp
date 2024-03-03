e812b922a99ae9553be93085f4cf8e72d8e4c571
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-03 10:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (45.90%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -13,31 +13,38 @@ public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode* prev = NULL;
         ListNode* curr = head;
+        // Only one node is present in LL
+        // Remove the node by making head = NULL and return head
         if(head->next == NULL) {
             head = NULL;
             return head;
         }
         int sz = 1;
+        // Finding the size of LL 
         while(curr->next) {
             sz+=1;
             curr = curr->next;
         }
+        // Node to remove from front/beginning
         int nodeToRemove = sz - n;
+        // For tracking current node 
         int currNode = 0;
         curr = head;
+        // Iterate till the end of LL
         while(curr) {
-            cout << currNode << endl;
-            cout << nodeToRemove << endl;
             if(currNode == nodeToRemove) {
                 if(prev != NULL) {
+                    // Link prev node to next node of curr to remove curr node
                     prev->next = curr->next;
-                    cout << curr->next;
                 }
                 else {
+                    // This is the first node in LL to be removed
+                    // Simply move head to next node to remove it
                     head = head->next;
                 }
             }
             currNode++;
+            // Make curr node as prev node before moving to next
             prev = curr;
             curr = curr->next;
         }
