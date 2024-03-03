9d4826e5657a3f2ef4fe1cc94ff23f1763740a79
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-03 11:57:15 +0530

    Time: 3 ms (64.94%) | Memory: 13.2 MB (45.90%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,7 +10,14 @@
  */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
+    /*
+        Using Two Passes
+        Time Complexity = O(len) + O(len-n)     
+                        = O(2*len) in worst case
+        Space Complexity = O(1)                
+        len = length of Linked List, n = nth node to be deleted from end 
+    */
+    ListNode* removeUsingTwoPass(ListNode* head, int n) {
         // For tracking previous node
         ListNode* prev = NULL;
         ListNode* curr = head;
@@ -55,5 +62,52 @@ public:
         }
 
         return head;
+    }
+
+    /*
+        Using One Pass
+        T.C. = O(n) for the fast pointer
+        S.C. = O(1)
+    */
+    ListNode* removeUsingOnePass(ListNode* head, int n) {
+        // Fast and Slow Pointers
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        // Moving fast pointer to 'n' nodes ahead 
+        for(int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+
+        // If fast pointer reaches after end of LL
+        // (in case n = length of LL)
+        if(fast == NULL) {
+            // Node to be removed from end is the first node in the LL
+            // Move head to next node to remove it and return new head
+            ListNode* newHead = head->next;
+            delete(head);
+            return newHead;
+        }
+
+        // Iterate until fast pointer is pointing to last node
+        while(fast->next) {
+            // Move fast and slow pointer simultaneously
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        // slow pointer is now pointing to one node before the node to be deleted from end
+        ListNode* nodeToDelete = slow->next;
+        // Linking to node which is next to the node to be deleted 
+        slow->next = slow->next->next;
+        delete(nodeToDelete);
+
+        return head;
+    }
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // return removeUsingTwoPass(head, n);
+
+        return removeUsingOnePass(head, n);
     }
 };
