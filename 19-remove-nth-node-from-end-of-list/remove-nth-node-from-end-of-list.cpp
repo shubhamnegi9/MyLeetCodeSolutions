3c6c78fc7a675459d0493e78eae1f35b093dd8e9
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-03 11:28:44 +0530

    Time: 8 ms (6.35%) | Memory: 13.2 MB (66.71%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,42 +11,47 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // For tracking previous node
         ListNode* prev = NULL;
         ListNode* curr = head;
-        // Only one node is present in LL
-        // Remove the node by making head = NULL and return head
-        if(head->next == NULL) {
-            head = NULL;
-            return head;
-        }
-        int sz = 1;
+        int sz = 0;
+
         // Finding the size of LL 
-        while(curr->next) {
+        while(curr) {
             sz+=1;
             curr = curr->next;
         }
+
         // Node to remove from front/beginning
         int nodeToRemove = sz - n;
         // For tracking current node 
         int currNode = 0;
         curr = head;
+
+        if(nodeToRemove == 0) {
+            // This is the first node in LL to be removed
+            // Simply move head to next node to remove it
+            ListNode* newHead = head->next;
+            delete (head);  // Delete the memory allocated to head node
+            return newHead;
+        }
+
         // Iterate till the end of LL
         while(curr) {
             if(currNode == nodeToRemove) {
-                if(prev != NULL) {
-                    // Link prev node to next node of curr to remove curr node
-                    prev->next = curr->next;
-                }
-                else {
-                    // This is the first node in LL to be removed
-                    // Simply move head to next node to remove it
-                    head = head->next;
-                }
+                // Link prev node to next node of curr to remove curr node
+                ListNode* temp = curr;
+                prev->next = curr->next;
+                curr = curr->next;
+                delete(temp);   // Delete the memory allocated to curr node
+                break;
             }
-            currNode++;
-            // Make curr node as prev node before moving to next
-            prev = curr;
-            curr = curr->next;
+            else {
+                currNode++;
+                // Make curr node as prev node before moving to next
+                prev = curr;
+                curr = curr->next;
+            }
         }
 
         return head;
