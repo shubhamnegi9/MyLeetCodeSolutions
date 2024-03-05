141e155c0dd2a74c63ad312233c670ab0f07e6c2
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-05 23:56:30 +0530

    Time: 23 ms (80.00%) | Memory: 14 MB (13.45%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -3,21 +3,18 @@ public:
     int minimumLength(string s) {
         int n = s.length();
         int i = 0, j = n-1;
-        while(i < j) {
-            if(s[i] != s[j]) {
-                return j-i+1;
-            }
-            else {
-                while(i < j && s[i] == s[i+1]) {
-                    i++;
-                }
-                while(i < j && s[j] == s[j-1]) {
-                    j--;
-                }
+        while(i < j && s[i] == s[j]) {
+            // Move the pointers only if characters at both are equal
+            char ch = s[i];
+            // Move i to next character after prefix
+            while(i < j && s[i] == ch) {
                 i++;
+            }
+            
+            while(i <= j && s[j] == ch) {   // i <= j in case i reaches to j because of previous while loop
                 j--;
             }
         }
-        return i==j?1:0;
+        return j-i+1;
     }
 };
