8079614de6c506cf59bc6b842f998ca02a17f964
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-08 22:00:43 +0530

    Time: 18 ms (5.05%) | Memory: 12.2 MB (14.29%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -10,16 +10,21 @@ class Solution {
 public:
     // Using Brute Force
     ListNode* detectCycleUsingBrute(ListNode *head) {
+        // Set for storing the nodes traversed so far
         set<ListNode*> s;
-        int begin = -1;
+
         while(head) {
+            // If node found in set, then we have reached to same node again.
+            // This node is begining of cycle.
             if(s.find(head) != s.end()) {
                 return head;
             }
+            // Insert current node in set
             s.insert(head);
             head = head->next;
         }
 
+        // No cycle is present 
         return NULL;
     }
 
