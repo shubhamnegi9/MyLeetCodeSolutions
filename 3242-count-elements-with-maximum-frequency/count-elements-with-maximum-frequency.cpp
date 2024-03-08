ecaebcbee0cc56a5e00c89ecff66cf07ca7da499
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-08 20:07:12 +0530

    Time: 4 ms (68.91%) | Memory: 22 MB (17.21%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int maxFrequencyElements(vector<int>& nums) {
+    // Usign Two Pass
+    // T.C. = O(2n)
+    int usingTwoPass(vector<int> &nums) {
         // Hash array of size (maxElement+1)
         vector<int> hash(101, 0);
 
@@ -13,15 +15,50 @@ public:
 
         // Finding max freq element and increasing its freq count
         int res = 0;
-        // for(int i = 0; i < 101; i++) {
-        //     if(hash[i] == maxFreq) {
-        //         res += hash[i];
-        //     }
-        // }
+        for(int i = 0; i < 101; i++) {
+            if(hash[i] == maxFreq) {
+                res += hash[i];
+            }
+        }
 
-        // Finding count of maxFreq elements using count() STL function
-        res = count(hash.begin(), hash.end(), maxFreq) * maxFreq;
+        // Finding count of maxFreq elements using count() STL function:
+        // res = count(hash.begin(), hash.end(), maxFreq) * maxFreq;
 
         return res;
+    }
+
+    // Usign One Pass
+    // T.C. = O(n)
+    int usingOnePass(vector<int> &nums) {
+        // Hash array of size (maxElement+1)
+        vector<int> hash(101, 0);
+
+        int maxFreq= -1;
+        int res = 0;
+
+        for(int ele: nums) {
+            // Increase freq of current element
+            hash[ele]++;
+
+            // If the freq of element is more than maxFreq, update maxFreq and reset 'res' and initialize with the element's freq (or initialize with maxFreq)
+            if(hash[ele] > maxFreq) {
+                maxFreq = hash[ele];
+                res = hash[ele];
+            }
+            // If the freq of element is equal to maxFreq, keep adding it to 'res'
+            else if(hash[ele] == maxFreq) {
+                res += hash[ele];
+            }
+        }
+
+        return res;
+    }
+
+    int maxFrequencyElements(vector<int>& nums) {
+        // Using two pass
+        // return usingTwoPass(nums);
+
+        // Usig one pass
+        return usingOnePass(nums);
     }
 };
