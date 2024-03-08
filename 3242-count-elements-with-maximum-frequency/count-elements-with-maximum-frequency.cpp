a46b9c7605c8f4e12994e4e29b2b6f1089580d16
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-08 19:10:10 +0530

    Time: 7 ms (40.22%) | Memory: 22 MB (17.21%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -3,23 +3,21 @@ public:
     int maxFrequencyElements(vector<int>& nums) {
         int mx = *max_element(nums.begin(), nums.end());
 
+        // Hash array of size (maxElement+1)
         vector<int> hash(mx+1, 0);
 
+        // Storing freq of elements in hash array
         for(int ele: nums) {
             hash[ele]++;
         }
 
-        int maxFreq = -1;
-        for(int i = 0; i <= mx; i++) {
-            if(hash[i] > maxFreq) {
-                maxFreq = hash[i];
-            }
-        }
-
+        // Finding max freq element and increasing its freq count
+        int maxFreq = *max_element(hash.begin(), hash.end());
         int count = 0;
         for(int i = 0; i <= mx; i++) {
-            if(hash[i] == maxFreq)
-                count+=hash[i];
+            if(hash[i] == maxFreq) {
+                count += hash[i];
+            }
         }
 
         return count;
