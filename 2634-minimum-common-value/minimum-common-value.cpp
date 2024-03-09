3aec157ddbfd2bce41892b28c04235db4a1d1491
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-09 19:36:42 +0530

    Time: 102 ms (25.23%) | Memory: 69.1 MB (18.41%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+
+    // Using set
+    // T.C. = O(m) for adding in set + O(n) for finding in set
+    //      = O(m+n)
+    // S.C. = O(m) or O(n) 
+    int getCommonUsingSet(vector<int>& nums1, vector<int>& nums2) {
+        
+        // Taking unordered set and inserting all values of nums1 in set
+        unordered_set<int> s(nums1.begin(), nums1.end());
+
+        // Iterate in nums2 and check which element is present in set and return it
+        for(int ele: nums2) {
+            if(s.find(ele) != s.end()) {
+                return ele;
+            }
+        }
+
+        return -1;
+    }
+
+    // Using 2 pointer approach
+    // T.C. = O(m+n), S.C. = O(1)
+    int getCommonUsing2Pointer(vector<int>& nums1, vector<int>& nums2) {
         int p1 = 0, p2 = 0;
         int m = nums1.size(), n = nums2.size();
 
@@ -17,5 +39,14 @@ public:
         }
 
         return -1;
+    }
+
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        
+        // Using set
+        return getCommonUsingSet(nums1, nums2);
+
+        // Using 2 pointer approach
+        // return getCommonUsing2Pointer(nums1, nums2);
     }
 };
