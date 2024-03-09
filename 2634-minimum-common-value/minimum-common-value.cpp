15a5b83fc33a822618f401d7d4d5256072b00643
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-09 19:43:34 +0530

    Time: 62 ms (89.16%) | Memory: 52.9 MB (73.96%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -20,6 +20,41 @@ public:
         return -1;
     }
 
+    // Using Binary Search
+    // T.C. = O(m*logn(n)) or O(n*logn(m))
+    // S.C. = O(1)
+    bool binarySearch(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+
+            if(nums[mid] == target) {
+                return true;
+            }
+            else if(nums[mid] < target) {
+                l = mid+1;
+            }
+            else {
+                r = mid-1;
+            }
+        }
+
+        return false;
+    }
+
+    int getCommonUsingBinarySearch(vector<int>& nums1, vector<int>& nums2) {
+        
+        // Iterate in each element in nums1 and find it in nums2 using binary search
+        for(int ele: nums1) {
+            if(binarySearch(nums2, ele)) {
+                return ele;
+            }
+        }
+
+        return -1;
+    }
+
     // Using 2 pointer approach
     // T.C. = O(m+n), S.C. = O(1)
     int getCommonUsing2Pointer(vector<int>& nums1, vector<int>& nums2) {
@@ -44,7 +79,10 @@ public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         
         // Using set
-        return getCommonUsingSet(nums1, nums2);
+        // return getCommonUsingSet(nums1, nums2);
+
+        // Using Binary Search
+        return getCommonUsingBinarySearch(nums1, nums2);
 
         // Using 2 pointer approach
         // return getCommonUsing2Pointer(nums1, nums2);
