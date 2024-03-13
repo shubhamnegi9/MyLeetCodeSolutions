260478c709bc6eb66138f9fb952419f7a7e992b7
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-13 22:42:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (73.82%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
-    int pivotInteger(int n) {
+    // Approach 1:
+    // Brute Force
+    int findPivot1(int n) {
+        for(int pivot = 1; pivot <= n; pivot++) {
+            int leftSum = 0, rightSum = 0;
+
+            for(int i = 1; i <= pivot; i++) {
+                leftSum+=i;
+            } 
+            for(int i = pivot; i <= n; i++) {
+                rightSum+=i;
+            }
+            if(leftSum == rightSum)
+                return pivot;
+        }
+        return -1;
+    } 
+
+    // Approach 2:
+    // Using sum of first n numbers formula
+    int findPivot2(int n) {
         int total = n*(n+1)/2;
         int sum = 0;
         for(int i = 1; i <= n; i++) {
@@ -9,5 +29,28 @@ public:
                 return i;
         }
         return -1;
+    } 
+
+    // Approach 3:
+    // Using mathmematical formula
+    int findPivot3(int n) {
+        int totalSum = n*(n+1)/2;
+        int pivot = sqrt(totalSum);
+        return (pivot*pivot == totalSum) ? pivot : -1;
+    } 
+
+    // // Approach 4: 
+    // int findPivot4(int n) {
+        
+    // } 
+
+    // // Approach 5:
+    // int findPivot5(int n) {
+        
+    // }
+
+    int pivotInteger(int n) {
+        
+        return findPivot3(n);
     }
 };
