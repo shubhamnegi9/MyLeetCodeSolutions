810f53fb93b66a204693a4a97bc56215b11bde5a
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-30 00:03:11 +0530

    Time: 213 ms (55.43%) | Memory: 148.4 MB (86.23%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 
     // Approach 1: Using Sliding Window
+    // T.C. = O(n), S.C. = O(n)
     int maxSubarrayLength1(vector<int>& nums, int k) {
         int n = nums.size();
         unordered_map<int, int> mpp;
@@ -24,8 +25,44 @@ public:
         return maxLen;
     }
 
+    // Approach 2: Using Sliding Window without nested loop
+    // T.C. = O(n), S.C. = O(n)
+    int maxSubarrayLength2(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+        int i = 0, j = 0;
+        int maxLen = 0;
+        int culprit = 0;
+
+        while(j < n) {
+
+            mpp[nums[j]]++;
+
+            if(mpp[nums[j]] == k+1) {
+                culprit++;
+            }
+
+            if(culprit > 0) {
+                mpp[nums[i]]--;
+                if(mpp[nums[i]] == k) {
+                    culprit--;
+                }
+                i++;
+            }
+
+            if(culprit == 0)
+                maxLen = max(maxLen, j-i+1);
+            j++;
+        }
+
+        return maxLen;
+    }
+
     int maxSubarrayLength(vector<int>& nums, int k) {
         // Approach 1: Using Sliding Window
-        return maxSubarrayLength1(nums, k);
+        // return maxSubarrayLength1(nums, k);
+
+        // Approach 2: Using Sliding Window without nested loop
+        return maxSubarrayLength2(nums, k);
     }
 };
