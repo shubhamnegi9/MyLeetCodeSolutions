b9c5d103f6de112edee4d6012f302065a4335716
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-29 20:01:58 +0530

    Time: 147 ms (32.56%) | Memory: 123.6 MB (38.76%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    long long countSubarrays(vector<int>& nums, int k) {
+    // Approach 1: Using Sliding Window
+    // T.C. = O(n), S.C. = O(1)
+    long long countSubarrays1(vector<int>& nums, int k) {
         int n = nums.size();
-        int maxEle = *max_element(nums.begin(), nums.end());
-        int maxEleCount = 0;
         int i = 0, j = 0;
         long long res = 0;
+        
+        int maxEle = *max_element(nums.begin(), nums.end());
+        int maxEleCount = 0;
 
         while(j < n) {
             if(nums[j] == maxEle) 
@@ -22,5 +25,40 @@ public:
         }
 
         return res;
+    }
+
+    // Approach 2: By tracking Indexes of Max Element
+    // T.C. = O(n), S.C. = O(n)
+    long long countSubarrays2(vector<int>& nums, int k) {
+        vector<int> maxElementIndices;
+        int n = nums.size();
+        long long res = 0;
+
+        int maxEle = *max_element(nums.begin(), nums.end());
+
+        for(int i = 0; i < n; i++) {
+
+            if(nums[i] == maxEle) {
+                maxElementIndices.push_back(i);
+            }
+
+            int size = maxElementIndices.size();
+
+            if(size >= k) {
+                int lastIndexOfMaxEle = maxElementIndices[size - k];
+                res += (lastIndexOfMaxEle+1);
+            }
+        }
+
+        return res;
+    }
+    
+    long long countSubarrays(vector<int>& nums, int k) {
+
+        // Approach 1: Using Sliding Window
+        // return countSubarrays1(nums, k);
+
+        // Approach 2: By tracking Indexes of Max Element
+        return countSubarrays2(nums, k);
     }
 };
