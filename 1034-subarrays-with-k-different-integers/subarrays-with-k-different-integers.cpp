28ebf27846ba60f3f6c8dabd0944abf15de16a03
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-03-30 23:43:16 +0530

    Time: 81 ms (85.22%) | Memory: 43.1 MB (89.89%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -32,8 +32,51 @@ public:
         return slidingWindow(nums, k) - slidingWindow(nums, k-1);
     }
 
+
+    //Approach-2 (One Pass Flow)
+    //T.C : O(n)
+    //S.C : O(n)
+    int subarraysWithKDistinct2(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        int i = 0, j = 0, i_largest = 0;
+        int count = 0;
+
+        while(j < n) {
+            mp[nums[j]]++;
+
+            // Handling Invalid Subarray
+            // Shrink the window
+            while(mp.size() > k) {
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0)
+                    mp.erase(nums[i]);  // Remove entry from map
+                i++;
+                i_largest = i;  // Move i_largest to i
+            }
+
+            // Handling to find smallest valid subarray ending at j
+            while(mp[nums[i]] > 1) {
+                mp[nums[i]]--;
+                i++;
+            }
+
+            // Smallest valid subarray ending at j found
+            if(mp.size() == k) {
+                count += (i - i_largest + 1);
+            } 
+
+            j++;
+        }
+
+        return count;
+    }
+
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         // Approach 1 
-        return subarraysWithKDistinct1(nums, k);
+        // return subarraysWithKDistinct1(nums, k);
+
+        // Approach 2
+        return subarraysWithKDistinct2(nums, k);
     }
 };
