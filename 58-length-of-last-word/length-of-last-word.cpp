926ac7c462988739b161a4522ff19a8e5a96d9ec
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-04-01 22:34:58 +0530

    Time: 4 ms (47.87%) | Memory: 7.8 MB (20.26%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -11,7 +11,7 @@ public:
         }
 
         int len= 0;
-        // Move 'i' until space character and increase lenght
+        // Move 'i' until space character and increase length
         while(i >= 0 && s[i] != ' ') {
             i--;
             len++;
@@ -20,8 +20,26 @@ public:
         return len;
     }
 
+    // Approach 2: Using STL
+    int lengthOfLastWordUsingSTL(string s) {
+
+        // find_last_not_of(' ') : Returns first index from last character which is not a space
+        // find_last_not_of(' ') + 1: Returns index of first space character from last
+        int index = s.find_last_not_of(' ') + 1;
+        // Erase all characters starting from index till end
+        s.erase(index);
+
+        // find_last_of(' ') : Returns first index from last character which is a space
+        int last_space_index = s.find_last_of(' ');
+        
+        return last_space_index == string::npos ? s.length() : s.length() - last_space_index - 1;
+    } 
+
     int lengthOfLastWord(string s) {
         // Approach 1
-        return lengthOfLastWordUsingLoop(s);
+        // return lengthOfLastWordUsingLoop(s);
+
+        // Approach 2
+        return lengthOfLastWordUsingSTL(s);
     }
 };
