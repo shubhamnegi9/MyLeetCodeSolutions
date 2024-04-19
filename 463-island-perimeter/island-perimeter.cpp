e922e0d462b496b59e56113950fee07ba2780c92
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-04-19 11:56:15 +0530

    Time: 101 ms (10.57%) | Memory: 109.4 MB (6.18%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -26,20 +26,63 @@ public:
         DFS(grid, i, j-1, m, n, peri);
     }
 
+    void BFS(vector<vector<int>>& grid, int i, int j, int m, int n, vector<vector<int>> directions, int &peri) {
+
+        queue<pair<int, int>> q;
+        // Pushing currrent cell position in queue
+        q.push({i, j});
+        
+        grid[i][j] = -1;
+
+        while(!q.empty()) {
+            auto it = q.front();
+            q.pop();
+
+            for(auto &dir: directions) {
+                int new_i = it.first + dir[0];
+                int new_j = it.second + dir[1];
+
+                if(new_i < 0 || new_i >= m || new_j < 0 || new_j >= n || grid[new_i][new_j] == 0) {
+                    peri++;
+                    continue;
+                }
+                else if(grid[new_i][new_j] == -1) {
+                    continue;
+                }
+                else {
+                    grid[new_i][new_j] = -1;
+                    q.push({new_i, new_j});
+                }
+            }
+        }
+    }
+
     int islandPerimeter(vector<vector<int>>& grid) {
         int m = grid.size();        // Rows
         int n = grid[0].size();     // Columns
         int perimeter = 0;
 
         // Using DFS
+        // for(int i = 0; i < m; i++) {
+        //     for(int j = 0; j < n; j++) {
+        //         // Start DFS from the cell containing 1
+        //         if(grid[i][j] == 1) {
+        //             DFS(grid, i, j, m, n, perimeter);
+        //         }
+        //     }
+        // }
+
+        // Using BFS
+        vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                // Start DFS from the cell containing 1
+                // Start BFS from the cell containing 1
                 if(grid[i][j] == 1) {
-                    DFS(grid, i, j, m, n, perimeter);
+                    BFS(grid, i, j, m, n, directions, perimeter);
                 }
             }
         }
+
 
         return perimeter;
     }
