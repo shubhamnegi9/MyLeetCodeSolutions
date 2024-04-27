8b9d8d837435a3177652bf81aaa1381c93119c55
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-04-28 00:16:05 +0530

    Time: 31 ms (81.91%) | Memory: 18.2 MB (57.39%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -55,6 +55,8 @@ public:
     }
     
     // Using Bottom Up Approach
+    // T.C. = O(n*n * n)
+    // S.C. = O(n*n)
     int solveBottomUp(vector<vector<int>>& grid, vector<vector<int>>& t, int m, int n) {
     
         // t[i][j] : Minimum Falling Path Sum from row 'i' to row 'm-1' if we choose column 'j' in the 'i'th row 
@@ -90,6 +92,82 @@ public:
         return minSum;
     }
     
+    // Using Bottom Up Optimized Approach
+    int solveBottomUpOptimized(vector<vector<int>>& grid, vector<vector<int>>& t, int m, int n) {
+    
+        // t[i][j] : Minimum Falling Path Sum from row 'i' to row 'm-1' if we choose column 'j' in the 'i'th row 
+        // Since in vector 't', for t[row][col], we don't known the value of t[row+i][col+j], therefore we will fill vector 't' in reverse order from bottom to up
+        
+        // Variables for storing the column index containing the minimum and next minimum value in the next row 
+        int nextMinCol1 = -1;
+        int nextMinCol2 = -1;
+        
+        // Filling last row of 't' vector same as grid
+        for(int col = 0; col < n; col++) {
+            t[m-1][col] = grid[m-1][col];
+            
+            // Intializing nextMinCol1 and nextMinCol2
+            if(nextMinCol1 == -1 || t[m-1][col] <= t[m-1][nextMinCol1]) {
+                nextMinCol2 = nextMinCol1;
+                nextMinCol1 = col;
+            }
+            else if(nextMinCol2 == -1 || t[m-1][col] <= t[m-1][nextMinCol2]) {
+                nextMinCol2 = col;
+            }
+                
+        }
+        
+        // Filling remaining rows of 't' in bottom-up manner
+        for(int row = m-2; row >= 0; row--) {
+            // Variables for storing the column index containing the minimum and next minimum value in the current row
+            int minCol1 = -1;
+            int minCol2 = -1;
+            for(int col = 0; col < n; col++) {
+                int curr = grid[row][col];
+                int nextRow = row+1;
+                
+                // This for-loop is not required anymore now:
+                // for(int nextCol = 0; nextCol < n; nextCol++) {
+                //     // If element in next row is not in same column and is not out of bound
+                //     if(nextRow < m && nextCol != col) {
+                //         ans = min(ans, curr+t[nextRow][nextCol]);
+                //     }
+                // }
+                // t[row][col] = ans;
+                
+                
+                // Taking nextMinCol1 value if it is not matching with current column value, otherwise taking nextMinCol2 value
+                if(col != nextMinCol1) {
+                    t[row][col] = curr+t[nextRow][nextMinCol1];
+                }
+                else {
+                    t[row][col] = curr+t[nextRow][nextMinCol2];
+                }
+                
+                // Intializing minCol1 and minCol2
+                if(minCol1 == -1 || t[row][col] <= t[row][minCol1]) {
+                    minCol2 = minCol1;
+                    minCol1 = col;
+                }
+                else if(minCol2 == -1 || t[row][col] <= t[row][minCol2]) {
+                    minCol2 = col;
+                }
+            }
+            // Assigning minCol1 and minCol2 to nextMinCol1 and nextMinCol2 for further iteration
+            nextMinCol1 = minCol1;
+            nextMinCol2 = minCol2;
+        }
+        
+        // This for-loop is not required anymore now:
+        // int minSum = INT_MAX;
+        // // Iterating in the first row of 't' vector and finding the minimum sum
+        // for(int col = 0; col < n; col++) {
+        //     minSum = min(minSum, t[0][col]);
+        // }
+        
+        // t[0][nextMinCol1] cell contains minimum sum
+        return t[0][nextMinCol1];
+    }
     
     int minFallingPathSum(vector<vector<int>>& grid) {
         int m = grid.size();        
@@ -110,8 +188,12 @@ public:
         // }
         
         // Using Bottom Up Approach
+        // vector<vector<int>> t(m, vector<int> (n, INT_MAX));  // Vector of size (mXn) and intitalized with INT_MAX (Since we need to find minimum)
+        // minSum = solveBottomUp(grid, t, m, n);  
+        
+        // Using Bottom Up Optimized Approach
         vector<vector<int>> t(m, vector<int> (n, INT_MAX));  // Vector of size (mXn) and intitalized with INT_MAX (Since we need to find minimum)
-        minSum = solveBottomUp(grid, t, m, n);        
+        minSum = solveBottomUpOptimized(grid, t, m, n);  
         
         return minSum;
     }
