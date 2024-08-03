467e7952288f53d7c03682004d79c1122c1bbbfb
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-08-03 16:51:17 +0530

    Time: 55 ms (96.52%) | Memory: 90.1 MB (18.76%) - LeetSync

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> temp(2*n);  // Creating another array of twice the size
+        
+        // For appending same array at the end to handle circular array case
+        for(int i = 0; i < 2*n; i++) {
+            temp[i] = nums[i%n];
+        }
+        
+        // Applying Sliding Window 
+        int windowSize = count(nums.begin(), nums.end(), 1); // Window size using original nums array
+        int currCountOf1s = 0;
+        int maxCountOf1s = 0;
+        int i = 0, j = 0;
+        
+        while(j < 2*n) {
+            currCountOf1s+=temp[j];
+            
+            if(j-i+1 > windowSize) {
+                // Shrink the window
+                currCountOf1s-=temp[i];
+                i++;
+            }
+            
+            // Keeping track of max count of 1s for each window
+            maxCountOf1s = max(maxCountOf1s, currCountOf1s);
+            j++;
+        }
+        
+        // Returning min. no. of swaps
+        return (windowSize - maxCountOf1s);
+    }
+};
