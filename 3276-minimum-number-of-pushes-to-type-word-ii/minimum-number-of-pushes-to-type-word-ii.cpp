e2d91a62f6682e3995497a63bbee9d0e8515955a
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-08-06 23:28:24 +0530

    Time: 171 ms (15.98%) | Memory: 28.5 MB (7.66%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    // make the "cmp" function as static, as sort() function takes static function pointer as an argument
     static bool cmp(pair<char, int> &p1, pair<char, int> &p2) {
         return p1.second > p2.second;
     }
@@ -16,11 +17,12 @@ public:
             v.push_back(it);
         }
         
-        sort(v.begin(), v.end(), cmp);
+        // Sorting by non-increasing order so that keys with more frequencies gets less pushes
+        sort(v.begin(), v.end(), cmp); 
         
         int totalCost = 0;
         for(int i = 0; i < v.size(); i++) {
-            int times = (i/8)+1;
+            int times = (i/8)+1; // Number of pushes for each key
             totalCost += (v[i].second * times);
         }
         
