e8451ee364ac0845e30a05f3c3f826a7ee39f4b2
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-08-25 19:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (92.17%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -14,15 +14,27 @@ public:
     // Approach 2
     int findComplement2(int num) {
         int numOfBits = log2(num)+1;
-        unsigned int mask = pow(2,numOfBits)-1;
+        unsigned int mask = (1U << numOfBits) - 1;   // Taking unsigned int as (1 << numOfBits) will make it go out of int bound
         return num ^ mask;
+    }
+    
+        // Approach 3
+    int findComplement3(int num) {
+        int complement = 0;
+        while(num) {
+            complement |= !(num & 1);
+        }
+        return complement;
     }
     
     int findComplement(int num) {
         // Approach 1
-        return findComplement1(num);
+        // return findComplement1(num);
         
         // Approach 2
         return findComplement2(num);
+        
+        // Approach 3
+        // return findComplement2(num);
     }
 };
