ad9d1eb0ef59dad9c2773e7db4130c3b83ab2842
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-08-26 22:39:18 +0530

    Time: 17 ms (28.07%) | Memory: 15.3 MB (49.47%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -22,20 +22,26 @@ class Solution {
 public:
     
     void traverse(Node* root, vector<int> &result) {
-        
+        // Base Case
         if(!root)
             return;
         
+        // First, visit all children
         for(Node* &node: root->children) {
             traverse(node, result);
         }
         
+        // Then, add the current node's value
         result.push_back(root->val);
     }
     
-    vector<int> postorder(Node* root) {
+    vector<int> postOrderRecursive(Node* root) {
         vector<int> result;
         traverse(root, result);
         return result;
+    }
+    
+    vector<int> postorder(Node* root) {
+        return postOrderRecursive(root);
     }
 };
