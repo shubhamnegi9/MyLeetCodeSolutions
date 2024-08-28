3ed53d5858d6676928b335dd52a53bdafcba9af9
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-08-28 23:57:23 +0530

    Time: 212 ms (66.83%) | Memory: 95.7 MB (60.76%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,23 +1,61 @@
 class Solution {
 public:
     
-    bool isSubIsland(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j) {
+    bool isSubIslandDFS(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j) {
         
+        /*
+            When you hit a cell != 1, it means that while traversing the Island, you have hit a point where it’s water.             This doesn’t violate the requirement of the problem. So we return true.
+            We can only return false when a cell is land in grid2 but it’s not a land in grid1
+        */
         if(i < 0 || i >= grid2.size() || j < 0 || j >= grid2[0].size() || grid2[i][j] != 1) {
             return true;
         }
         
-        grid2[i][j] = -1;
+        grid2[i][j] = -1;   //mark visited
         
         bool result = (grid1[i][j] == 1);
         
-        result = result & (isSubIsland(grid1, grid2, i+1, j));
-        result = result & (isSubIsland(grid1, grid2, i, j+1));
-        result = result & (isSubIsland(grid1, grid2, i-1, j));
-        result = result & (isSubIsland(grid1, grid2, i, j-1));
+        result = result & isSubIslandDFS(grid1, grid2, i+1, j);
+        result = result & isSubIslandDFS(grid1, grid2, i, j+1);
+        result = result & isSubIslandDFS(grid1, grid2, i-1, j);
+        result = result & isSubIslandDFS(grid1, grid2, i, j-1);
         
         return result;
     } 
+    
+    //BFS
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; //down, up, right , left
+    bool isSubIslandBFS(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j) {
+        int m = grid1.size();
+        int n = grid1[0].size();
+
+        bool result = true;
+
+        queue<pair<int, int>> que; //{i, j} //BFS me we use queue
+        que.push({i, j});
+        grid2[i][j] = -1; //mark visited
+
+        while(!que.empty()) {
+            auto [x, y] = que.front();
+            que.pop();
+
+            if(grid1[x][y] != 1) { //grid1 must have 1 at that same co-ordinate
+                result = false;
+            }
+
+            for(vector<int>& dir : directions) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+
+                if(newX >= 0 && newX < m && newY >= 0 && newY < n && grid2[newX][newY] == 1) {
+                    grid2[newX][newY] = -1; //mark visited
+                    que.push({newX, newY});
+                }
+            }
+        }
+
+        return result;
+    }
         
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int m = grid2.size();
@@ -26,7 +64,7 @@ public:
         int subIslands = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid2[i][j] == 1 && isSubIsland(grid1, grid2, i, j)) {
+                if(grid2[i][j] == 1 && isSubIslandDFS(grid1, grid2, i, j)) {
                     subIslands++;
                 }
             }
