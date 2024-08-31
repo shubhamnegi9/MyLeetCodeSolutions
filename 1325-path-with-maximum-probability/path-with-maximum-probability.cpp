57b22b6f972acf7b289640e25255f1de7f4f96cf
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-08-31 20:21:30 +0530

    Time: 147 ms (18.33%) | Memory: 74.1 MB (16.21%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    typedef pair<double, int> P;
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
         unordered_map<int, vector<pair<int, double>>> adj;
         vector<double> result(n, 0);
         
-        for(int i=0;i<edges.size();i++)
-        {
-            int u       = edges[i][0];
-            int v       = edges[i][1];
-            double prob = succProb[i];
-            adj[u].push_back({v, prob});
-            adj[v].push_back({u, prob});
+        // Creating adjacency list/map
+        for(int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double p = succProb[i];    // Probability of path b/w u and v
+            adj[u].push_back({v, p});
+            adj[v].push_back({u, p});   // Since graph is undirected
         }
-
-        priority_queue<P> pq; //max heap because we need maximum probability
-
-		result[start] = 1;
-		pq.push({1.0, start});
-
-		while(!pq.empty()) {
-
-		    int curnode    = pq.top().second;
-            double curProb = pq.top().first;
+        
+        priority_queue<pair<double, int>> pq; // Max Heap
+        result[start_node] = 1.0;       // Max prob. of going from start node to start node is 1
+        pq.push({1.0, start_node});     // Pushing start node and its probability into max heap as {prob, start_node}
+        
+        // Iterate until map heap is empty
+        while(!pq.empty()) {
+            int currNode = pq.top().second;
+            double currProb = pq.top().first;
             pq.pop();
-
-            for(auto &child : adj[curnode])
-            {
-                if(result[child.first] < curProb*child.second) //we want maximum this time
-                {
-                    result[child.first] = curProb*child.second;
-                    pq.push({result[child.first], child.first});
+            
+            for(auto &temp: adj[currNode]) {
+                int adjNode = temp.first;
+                double adjProb = temp.second;
+                
+                if(currProb*adjProb > result[adjNode]) {
+                    result[adjNode] = currProb*adjProb;    // Update probability in result vector
+                    pq.push({currProb*adjProb, adjNode});   // Push node to max heap
                 }
             }
-
-		}
-
-		return result[end];
+        }
+        
+        return result[end_node];
     }
 };
