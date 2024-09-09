9c8d0cdc38ac51024926bc6cb664cd2ad293868f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-09-09 22:54:02 +0530

    Time: 3 ms (36.89%) | Memory: 8.3 MB (82.13%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,47 +1,61 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size();      // ROWS
-        int m = matrix[0].size();   // COLUMNS
-        vector<int> ans;
-
-        // Boundaries of spiral traversal
-        int left = 0, right = m-1, top = 0, bottom = n-1;
-
-        while((left <= right) && (top <= bottom)) {
-
-            // Printing towards right
-            for(int i = left; i <= right; i++) {
-                // cout << matrix[top][i] << " ";
-                ans.push_back(matrix[top][i]);
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        vector<int> result;
+        
+        int top = 0;
+        int bottom = m-1;
+        int left = 0;
+        int right = n-1;
+        
+        int id = 0;     // For tracking the direction
+        //0   -> left  to right
+        //1   -> top   to down
+        //2   -> right to left
+        //3   -> down  to top
+        
+        while(top <= bottom && left <= right) {
+            
+            if(id == 0) {
+                // Left to Right
+                for(int i = left; i <= right; i++) {
+                    result.push_back(matrix[top][i]);
+                }
+                top++;  // Increment the constant 'top' in loop
             }
-            top++;
-
-            // Printing towards bottom
-            for(int i = top; i <= bottom; i++) {
-                // cout << matrix[i][right] << " ";
-                ans.push_back(matrix[i][right]);
-            }
-            right--;
-
-            // Printing towards left
-            if(top <= bottom) {
+            
+            if(id == 1) {
+                // Top to Bottom
+                for(int i = top; i <= bottom; i++) {
+                    result.push_back(matrix[i][right]);
+                }
+                right--;    // Increment the constant 'right' in loop
+            }   
+            
+            if(id == 2) {
+                // Right to Left
                 for(int i = right; i >= left; i--) {
-                    // cout << matrix[bottom][i] << " ";
-                    ans.push_back(matrix[bottom][i]);
+                    result.push_back(matrix[bottom][i]);
                 }
-                bottom--;
+                bottom--;   // Increment the constant 'bottom' in loop
             }
-
-            // Printing towards top
-            if(left <= right) {
+            
+            if(id == 3) { 
+                // Bottom to Top
                 for(int i = bottom; i >= top; i--) {
-                    // cout << matrix[i][left] << " ";
-                    ans.push_back(matrix[i][left]);
+                    result.push_back(matrix[i][left]);
                 }
-                left++;
+                left++; // Increment the constant 'left' in loop
             }
+            
+            id = (id+1)%4;  // Increment the 'id' mod 4 so it repeates after reaching 3
+            
         }
-        return ans;
+        
+        return result;
     }
 };
