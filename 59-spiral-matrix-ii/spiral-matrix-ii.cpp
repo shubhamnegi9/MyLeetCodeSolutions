d4d6e4da9ac2dbbd068807681abefa8692762bec
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-09-09 23:16:15 +0530

    Time: 3 ms (34.57%) | Memory: 7.9 MB (75.86%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -12,38 +12,53 @@ public:
         int right = n-1;
         int top = 0;
         int bottom = n-1;
+
+        int id = 0;     // For tracking the direction
+        //0   -> left  to right
+        //1   -> top   to down
+        //2   -> right to left
+        //3   -> down  to top
         
         int num = 1;
         while(left <= right && top <= bottom) {
             
-            // Left to Right
-            for(int i = left; i <= right; i++) {
-                matrix[top][i] = num;
-                num++;
+            if(id == 0) {
+                // Left to Right
+                for(int i = left; i <= right; i++) {
+                    matrix[top][i] = num;
+                    num++;
+                }
+                top++;
             }
-            top++;
             
-            // Top to Bottom
-            for(int i = top; i <= bottom; i++) {
-                matrix[i][right] = num;
-                num++;
+            if(id == 1) {
+                // Top to Bottom
+                for(int i = top; i <= bottom; i++) {
+                    matrix[i][right] = num;
+                    num++;
+                }
+                right--;
             }
-            right--;
             
-            // Right to Left
-            for(int i = right; i >= left; i--) {
-                matrix[bottom][i] = num;
-                num++;
+            if(id == 2) {
+                // Right to Left
+                for(int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num;
+                    num++;
+                }
+                bottom--;
             }
-            bottom--;
             
-            // Bottom to Top
-            for(int i = bottom; i >= top; i--) {
-                matrix[i][left] = num;
-                num++;
+            if(id == 3) {
+                // Bottom to Top
+                for(int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num;
+                    num++;
+                }
+                left++;
             }
-            left++;
             
+            id = (id+1)%4;  // Increment the 'id' mod 4 so it repeates after reaching 3
         }
         
         return matrix;
