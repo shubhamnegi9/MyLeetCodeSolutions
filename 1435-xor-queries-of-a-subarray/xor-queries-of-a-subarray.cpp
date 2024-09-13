fd42a17911ecd19fe67dc19dccefcc8c5bab7788
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-09-13 22:25:10 +0530

    Time: 59 ms (65.18%) | Memory: 45 MB (14.22%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -1,18 +1,50 @@
 class Solution {
 public:
-    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+    // Brute Force Approach
+    vector<int> xorQueriesBrute(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int> result;
         
-        for(vector<int> ele: queries) {
-            int start = ele[0];
-            int end = ele[1];
-            int XOR = arr[start];
-            for(int i = start+1; i <= end; i++) {
+        for(vector<int> query: queries) {
+            int left = query[0];
+            int right = query[1];
+            int XOR = arr[left];
+            for(int i = left+1; i <= right; i++) {
                 XOR ^= arr[i];
             }
             result.push_back(XOR);
         }
         
         return result;
+    }
+    
+    // Optimal Approach
+    vector<int> xorQueriesOptimal(vector<int>& arr, vector<vector<int>>& queries) {
+        int n = arr.size();
+        
+        vector<int> prefixXOR(n, 0);
+        
+        // Precomputing prefix XOR
+        prefixXOR[0] = arr[0];
+        for(int i = 1; i < n; i++) {
+            prefixXOR[i] = prefixXOR[i-1] ^ arr[i];
+        }
+        
+        vector<int> result;
+        for(vector<int> query: queries) {
+            int left = query[0];
+            int right = query[1];
+            int XORResult = prefixXOR[right] ^ (left == 0 ? 0 : prefixXOR[left-1]);
+            result.push_back(XORResult);
+        }
+        
+        return result;
+    }
+    
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        // Brute Force Approach
+        // return xorQueriesBrute(arr, queries);
+        
+        // Optimal Approach
+        return xorQueriesOptimal(arr, queries);
     }
 };
