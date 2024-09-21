394f289440c8787a9d0a37e3632f6fb9aef84767
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-09-21 16:11:35 +0530

    Time: 9 ms (70.67%) | Memory: 13.8 MB (55.23%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -22,15 +22,15 @@ public:
     
     // Approach 2: Using Recursion as DFS
     // T.C. = O(n), S.C. = O(1)
-    void solve(int startNum, int n, vector<int> &result) {
+    void solve(int currNum, int n, vector<int> &result) {
         
-        if(startNum > n) {
+        if(currNum > n) {
             return;
         }
         
-        result.push_back(startNum);
+        result.push_back(currNum);
         for(int append = 0; append <= 9; append++) {
-            int newNum = startNum*10 + append;
+            int newNum = currNum*10 + append;
             if(newNum > n) {
                 return;
             }
@@ -41,8 +41,8 @@ public:
     vector<int> lexicalOrder2(int n) {
         vector<int> result;
         
-        for(int startNum = 1; startNum <= 9; startNum++) {
-            solve(startNum, n, result);
+        for(int currNum = 1; currNum <= 9; currNum++) {
+            solve(currNum, n, result);
         }
         
         return result;
