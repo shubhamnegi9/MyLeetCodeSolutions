47bd96ea7acf70533898a160f5873326d916fcce
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-09-24 20:59:23 +0530

    Time: 249 ms (75.76%) | Memory: 160.8 MB (60.49%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     // Approach 1: Using set to store all possible prefixes
+    //T.C : O(m⋅log10*M + n⋅log10*N)
+    //S.C : O(m⋅log10*M) for set 
     int longestCommonPrefix1(vector<int>& arr1, vector<int>& arr2) {
         
         unordered_set<int> st;
@@ -32,8 +34,90 @@ public:
         
     }
     
+    struct TrieNode {
+        // int data;    // Not required as we can check using child pointers
+        TrieNode* children[10];
+    };
+    
+    TrieNode* getTrieNode() {
+        
+        TrieNode* node = new TrieNode();
+        for(int i = 0; i < 10; i++) {
+            node->children[i] = nullptr;
+        }
+        return node;
+    }
+    
+    void insertNode(int num, TrieNode* root) {
+        // Crawler Pointer which crawls/traverse the Trie 
+        TrieNode* crawl = root;
+        string numStr = to_string(num);     // Converting to string to add easily in Trie
+        
+        for(char ch: numStr) {
+            int idx = ch-'0';       // Index of the current digit in children array
+            
+            // If the pointer at this index in children array is nullptr,
+            // insert the current digit at this pointer
+            if(!crawl->children[idx]) {      
+                crawl->children[idx] = getTrieNode();
+            }
+            
+            // Moving crawler pointer ahead
+            crawl = crawl->children[idx];
+        }
+    }
+    
+    int searchNode(int num, TrieNode* root) {
+        TrieNode* crawl = root;
+        string numStr = to_string(num); 
+        int length = 0;     // For storing length of longest prefix
+        
+        for(char ch: numStr) {
+            int idx = ch-'0';       // Index of the current digit in children array
+            
+            // If the pointer at this index in children array is not a nullptr,
+            // common prefix is found using current num
+            // Increase the length of prefix
+            if(crawl->children[idx]) {      
+                length++;
+                // Moving crawler pointer ahead
+                crawl = crawl->children[idx];
+            }
+            else {
+                // If the pointer at this index in children array is a nullptr,
+                // common prefix not found using current num
+                break;  // No need to check for remaining digits in Trie
+            }
+            
+        }
+        
+        return length;
+    }
+    
+    int longestCommonPrefix2(vector<int>& arr1, vector<int>& arr2) {
+        
+        TrieNode* root = getTrieNode();
+        
+        // Inseting the numbers of arr1 in Trie
+        for(int num: arr1) {
+            insertNode(num, root);
+        }
+        
+        int result = 0;
+        // Searcing the numbers of arr2 in Trie and returning length of longest prefix
+        for(int num: arr2) {
+            result = max(result, searchNode(num, root));
+        }
+        
+        return result;
+    }
+    
+    
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         // Approach 1: Using set to store all possible prefixes
-        return longestCommonPrefix1(arr1, arr2);
+        // return longestCommonPrefix1(arr1, arr2);
+        
+        // Approach 2: Using Trie Data Structure
+        return longestCommonPrefix2(arr1, arr2);
     }
 };
