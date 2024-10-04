f21013e49b9568ad3f70860b893db0028d7d192d
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-10-04 22:50:21 +0530

    Time: 60 ms (76.40%) | Memory: 56.4 MB (56.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
-    long long dividePlayers(vector<int>& skill) {
-        if(skill.size() == 2) {
+    // Approach 1: Using Sorting 
+    // T.C. = o(nlogn), S.C. = O(1)
+    long long dividePlayers1(vector<int>& skill) {
+        int n = skill.size();
+        if(n == 2) {
             return skill[0]*skill[1];
         }
         
@@ -10,15 +13,14 @@ public:
             sum+=num;
         }
         
-        int parts = skill.size()/2;
-        int partSum = sum/parts;
+        int teams = skill.size()/2;     // Total teams
+        int teamSum = sum/teams;        // Sum of each team
         
         sort(skill.begin(), skill.end());
         
-        int n = skill.size();
         long long result = 0;
         for(int i = 0; i < n/2; i++) {
-            if(skill[i]+skill[n-i-1] == partSum) {
+            if(skill[i]+skill[n-i-1] == teamSum) {
                 result += (skill[i]*skill[n-i-1]);
             } else {
                 return -1;
@@ -26,5 +28,38 @@ public:
         }
         
         return result;
+    }
+    
+    // Approach 2: Using Sorting+2-Pointer
+    // T.C. = o(nlogn), S.C. = O(1)
+    long long dividePlayers2(vector<int>& skill) {
+        int n = skill.size();
+        if(n == 2) {
+            return skill[0]*skill[1];
+        }
+        
+        sort(skill.begin(), skill.end());
+        
+        long long result = 0;
+        int i = 0, j = n-1;
+        int sum = skill[i]+skill[j];
+        while(i < j) {
+            if(skill[i]+skill[j] != sum) {
+                return -1;
+            }
+            result += ((long long) skill[i] * (long long) skill[j]);
+            i++;
+            j--;
+        }
+        
+        return result;
+    }
+    
+    long long dividePlayers(vector<int>& skill) {
+        // Approach 1: Using Sorting 
+        // return dividePlayers1(skill);
+        
+        // Approach 2: Using Sorting+2-Pointer
+        return dividePlayers2(skill);
     }
 };
