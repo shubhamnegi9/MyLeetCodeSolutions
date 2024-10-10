9f6a6f93283e06cb1319412f323b35d5ffb656bb
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-10-10 23:11:11 +0530

    Time: 3 ms (43.11%) | Memory: 7.8 MB (81.18%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,17 +1,48 @@
 class Solution {
 public:
-    int minAddToMakeValid(string s) {
-       stack<int> stk;
-       for(char ch: s) {
-           if(ch == '(') {
-               stk.push(ch);
-           } else if(stk.size() > 0 && stk.top() == '(') {
-               stk.pop();
-           } else {
-               stk.push(ch);
-           }
-       } 
-        
+    // Approach 1: Using stack
+    //T.C : O(n)
+    //S.C : O(n)
+    int minAddToMakeValid1(string s) {
+        stack<int> stk;
+        for(char ch: s) {
+            if(ch == '(') {
+                stk.push(ch);
+            } else if(stk.size() > 0 && stk.top() == '(') {
+                stk.pop();
+            } else {
+                stk.push(ch);
+            }
+        } 
+
         return stk.size();
+    }
+    
+    // Approach 2: Without using stack
+    //T.C : O(n)
+    //S.C : O(1)
+    int minAddToMakeValid2(string s) {
+        int size = 0;   // Closing brackets required to make valid
+        int open = 0;   // Opening brackets required to make valid
+        for(char ch: s) {
+            if(ch == '(') {
+                size++;
+            } else if(size > 0) {   // Current character is ')' and there are some '(' previously
+                size--;
+            } else {                // Current character is ')' and there are no '(' previously
+                open++;
+            }
+        }
+        
+        return size+open;
+    }
+    
+    int minAddToMakeValid(string s) {
+        // Approach 1: Using stack
+        // return minAddToMakeValid1(s);
+        
+        // Approach 2: Without using stack
+        return minAddToMakeValid2(s);
+     
     }
 };
