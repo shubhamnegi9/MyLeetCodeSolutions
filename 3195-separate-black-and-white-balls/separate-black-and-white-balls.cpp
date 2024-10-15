38672b19c54dc661659a6f20011739c514cf52f0
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-10-15 22:41:52 +0530

    Time: 25 ms (91.79%) | Memory: 17.3 MB (10.32%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -4,7 +4,7 @@ public:
     long long minimumSteps1(string s) {
         long long swap = 0;
         int blackBalls = 0;
-        for(int i = 0; i < s.size(); i++) {
+        for(int i = 0; i < s.length(); i++) {
             if(s[i] == '1') {
                 blackBalls++;
             } else {
@@ -15,7 +15,22 @@ public:
         return swap;
     }
     
+    // Counting white balls going right to left and adding to swap
+    long long minimumSteps2(string s) {
+        long long swap = 0;
+        int whiteBalls = 0;
+        for(int i = s.length()-1; i >=0; i--) {
+            if(s[i] == '0') {
+                whiteBalls++;
+            } else {
+                swap += whiteBalls;
+            }
+        }
+        return swap;
+    }
+    
     long long minimumSteps(string s) {
-        return minimumSteps1(s);
+        // return minimumSteps1(s);
+        return minimumSteps2(s);
     }
 };
