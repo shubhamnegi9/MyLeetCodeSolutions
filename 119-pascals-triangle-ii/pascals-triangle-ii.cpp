622a60820ea0f25dbbc52da90ab1a9e9a8b322d7
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-10-18 09:49:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (51.80%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
+    // Approach 1: By creating whole Pascal Triangle and returning last row
+    vector<int> getRow1(int rowIndex) {
         int numRows = rowIndex+1;
         vector<vector<int>> result(numRows);
         
@@ -13,5 +14,31 @@ public:
         }
         
         return result[rowIndex];
+    }
+    
+    // Approach 2: Space Optimized Approach
+    vector<int> getRow2(int rowIndex) {
+        vector<int> prev;
+        
+        for(int i = 0; i < rowIndex+1; i++) {
+            vector<int> curr(i+1, 1);
+            
+            for(int j = 1; j < i; j++) {
+                curr[j] = prev[j] + prev[j-1];
+            } 
+            
+            prev = curr;
+        }
+        
+        return prev;
+    
+    }
+    vector<int> getRow(int rowIndex) {
+        // Approach 1
+        // return getRow1(rowIndex);
+        
+        
+        // Approach 2
+        return getRow2(rowIndex);
     }
 };
