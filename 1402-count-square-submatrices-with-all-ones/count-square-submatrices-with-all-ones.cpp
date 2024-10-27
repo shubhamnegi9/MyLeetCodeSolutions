82b1445710ea8c0389166ae52780b36ad731a5dc
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-10-27 19:37:49 +0530

    Time: 7 ms (51.85%) | Memory: 29.2 MB (27.15%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -111,6 +111,31 @@ public:
         return count;
     }
     
+    // Optimal Approaoch using Bottom Up DP
+    int countSquares4(vector<vector<int>>& matrix) {
+        m = matrix.size();
+        n = matrix[0].size();
+        int count = 0;
+        
+        vector<vector<int>> t(m, vector<int>(n, 0));
+        
+        // t[i][j] = total square submmatrix having all 1s and ending at cell [i][j]
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || j == 0) {
+                    t[i][j] = matrix[i][j];
+                }
+                else if(matrix[i][j] == 1) {
+                    t[i][j] = 1+ min({t[i-1][j], t[i-1][j-1], t[i][j-1]});
+                }
+                count+=t[i][j];
+            }
+        }
+        
+        return count;
+    }
+    
     int countSquares(vector<vector<int>>& matrix) {
         // Brute Force Approach
         // return countSquares1(matrix);
@@ -119,8 +144,9 @@ public:
         // return countSquares2(matrix);
         
         // Optimal Approach using Top Down DP (Memoization)
-        return countSquares3(matrix);
+        // return countSquares3(matrix);
         
-      
+        // Optimal Approaoch using Bottom Up DP
+        return countSquares4(matrix);
     }
 };
