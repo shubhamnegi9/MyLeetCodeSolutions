7fc048bf8fc9754ae095dd0f53e9e789e90c395d
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-10-28 23:43:39 +0530

    Time: 63 ms (73.33%) | Memory: 86.5 MB (98.07%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -28,7 +28,7 @@ public:
         unordered_set<int> st(nums.begin(), nums.end());
         int maxStreak = 0;
         
-        for(int num: nums) {            // O(n)
+        for(int &num: nums) {            // O(n)
             int streak = 0;
             long long curr = num;
             
@@ -48,11 +48,58 @@ public:
         return maxStreak >= 2 ? maxStreak : -1;
     }
     
+    bool find(long long curr, vector<int>& nums) {
+        
+        int l = 0;
+        int r = nums.size()-1;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            
+            if(nums[mid] == curr) {
+                return true;
+            }
+            else if(nums[mid] > curr) {
+                r = mid-1;
+            }
+            else {
+                l = mid+1;
+            }
+        }
+        return false;
+    }
+    
+    int longestSquareStreak3(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int maxStreak = 0;
+        
+        for(int &num: nums) {
+            int streak = 0;
+            long long curr = num;
+            
+            while(find(curr, nums)) {
+                streak++;
+                
+                if(curr*curr > 1e5) {
+                    break;
+                }
+                
+                curr = curr*curr;
+            }
+            
+            maxStreak = max(maxStreak, streak);
+        }
+        
+        return maxStreak >= 2 ? maxStreak : -1;
+    }
+    
     int longestSquareStreak(vector<int>& nums) {
         // Apporach 1: Using Sorting + Map
         // return longestSquareStreak1(nums);
         
         // Approach 2: Without sorting + Set
-        return longestSquareStreak2(nums);
+        // return longestSquareStreak2(nums);
+        
+        return longestSquareStreak3(nums);
     }
 };
