0570c2e13f37008a40feba9410c71cb40282e578
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-01 21:46:30 +0530

    Time: 53 ms (69.70%) | Memory: 67.6 MB (26.38%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -20,7 +20,6 @@ public:
     }
     
     // Using Top Down Memoization approach
-    // T.C. = O(m*n), m = size of robot vector, n = size of position vector
     long long solveMemo(int ri, int pi, vector<int>& robot, vector<int>& position, vector<vector<long long>>& dp) {
         
         // Base Cases
@@ -43,6 +42,27 @@ public:
         return dp[ri][pi] = min(take, skip);
     }
     
+    // Using Bottom Up approach
+    long long solveBottomUp(int ri, int pi, vector<int>& robot, vector<int>& position, vector<vector<long long>>& t) {
+        
+        int m = robot.size();
+        int n = position.size();
+        
+        for(int i = 0; i < m; i++) {
+            t[i][n] = 1e12;     // This is invalid case as per problem
+        }
+        
+        for(int ri = m-1; ri >= 0; ri--) {
+            for(int pi = n-1; pi >= 0; pi--) {
+                long long take = abs(robot[ri] - position[pi]) + t[ri+1][pi+1];
+                long long skip = t[ri][pi+1];
+                t[ri][pi] = min(take, skip);
+            }
+        }
+        // Minimum distance starting from first robot and factory
+        return t[0][0];
+    }
+    
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
         
         // Sort robot and factory vectors
@@ -63,10 +83,15 @@ public:
         // return solve(0, 0, robot, position);
         
         // Using Top Down Memoization approach
+        // int m = robot.size();
+        // int n = position.size();
+        // vector<vector<long long>> dp(m+1, vector<long long> (n+1,-1));
+        // return solveMemo(0, 0, robot, position, dp);
+        
+        // Using Bottom Up approach
         int m = robot.size();
         int n = position.size();
-        vector<vector<long long>> dp(m+1, vector<long long> (n+1,-1));
-        return solveMemo(0, 0, robot, position, dp);
-        
+        vector<vector<long long>> t(m+1, vector<long long> (n+1, 0));
+        return solveBottomUp(0, 0, robot, position, t);
     }
 };
