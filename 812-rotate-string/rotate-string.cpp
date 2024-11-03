910014264c07701bac11ee33985838a9307b86c2
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-03 11:40:46 +0530

    Time: 2 ms (15.25%) | Memory: 7.7 MB (37.13%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -47,9 +47,29 @@ public:
         return false;
     }
     
+    // Optimal Approach
+    // T.C. = O(n)
+    // S.C. = O(2*n) = O(n)
+    bool rotateString3(string s, string goal) {
+        int n = s.length();
+        int m  = goal.length();
+        
+        if(n != m)
+            return false;
+        
+        string concatenated = s+s;
+        if(concatenated.find(goal) != string::npos) {
+            return true;
+        }
+        
+        return false;
+    }
+    
     bool rotateString(string s, string goal) {
        // return rotateString1(s, goal);
         
-        return rotateString2(s, goal);
+        // return rotateString2(s, goal);
+        
+        return rotateString3(s, goal);
     }
 };
