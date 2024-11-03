cb4b47d35afdf3fc2644f71a4facc4a4f1cc062c
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-03 11:37:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (55.81%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
+    
+    // Brute Force by rotating the string each time and checking
+    // T.C. = O(n^2)
+    // S.C. = O(1)
+    bool rotateString1(string s, string goal) {
         int n = s.length();
         int m  = goal.length();
         
@@ -10,6 +14,8 @@ public:
         for(int i = 0; i < n; i++) {
             if(s == goal)
                 return true;
+            
+            // Rotating the string to left
             char temp = s[0];    
             for(int j = 0; j < n; j++) {
                 s[j] = s[j+1];
@@ -18,5 +24,32 @@ public:
         }
         
         return false;
+    }
+    
+    // Brute Force by rotating the string each time using STL
+    // T.C. = O(n^2)
+    // S.C. = O(1)
+    bool rotateString2(string s, string goal) {
+        int n = s.length();
+        int m  = goal.length();
+        
+        if(n != m)
+            return false;
+        
+        for(int i = 0; i < n; i++) {
+            if(s == goal)
+                return true;
+            
+            // Rotating the string to left using rotate STL
+            rotate(s.begin(), s.begin()+1, s.end());
+        }
+        
+        return false;
+    }
+    
+    bool rotateString(string s, string goal) {
+       // return rotateString1(s, goal);
+        
+        return rotateString2(s, goal);
     }
 };
