bbd83a24f518eb86a4c7ecba3412f52b299dc51f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-04 22:23:13 +0530

    Time: 59 ms (21.37%) | Memory: 33.2 MB (5.03%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-    string compressedString(string word) {
+    // Approach 1
+    // T.C. = O(n)
+    // S.C. = O(1)
+    string compressedString1(string word) {
         int count = 1;
         int n = word.length();
         
@@ -24,5 +27,34 @@ public:
         result+=to_string(count);
         result+=word[n-1];
         return result;
+    }
+    
+    // Approach 2
+    // T.C. = O(n)
+    // S.C. = O(1)
+    string compressedString2(string word) {
+        int n = word.length();
+        int i = 0;
+        string result = "";
+        while(i < n) {
+            int count = 1;
+            while(i < n-1 && count < 9 && word[i] == word[i+1]) {
+                count++;
+                i++;
+            }
+            result+=to_string(count);
+            result+=word[i];
+            i++;
+        }
+        
+        return result;
+    }
+    
+    string compressedString(string word) {
+        // Approach 1
+        // return compressedString1(word);
+        
+        // Approach 2
+        return compressedString2(word);
     }
 };
