ba314700e2d0f36c78890fe5457964335971a5d0
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-06 22:59:53 +0530

    Time: 1 ms (56.83%) | Memory: 31.3 MB (49.73%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -1,5 +1,43 @@
 class Solution {
 public:
+    // Approach 1: Using Modified Bubble Sort
+    bool canSortArray1(vector<int>& nums) {
+        int n = nums.size();
+        
+        for(int i = 0; i < n-1; i++) {
+            bool swapped = false;
+            for(int j = 0; j < n-i-1; j++) {
+                
+                if(nums[j] < nums[j+1]) {
+                    // Already sorted order
+                    // Continue to next element as no need to check for equal set bits
+                    continue;   
+                } else {
+                    // Not in sorted order
+                    int currSetBits = __builtin_popcount(nums[j]);
+                    int nextSetBits = __builtin_popcount(nums[j+1]);
+                    if(currSetBits == nextSetBits) {
+                        // nums[j] and nums[j+1] can be swapped
+                        swap(nums[j], nums[j+1]);
+                        swapped = true;
+                    } else {
+                        // nums[j] and nums[j+1] cannot be swapped 
+                        // So array can never be sorted
+                        return false;
+                    }
+                }
+            }
+            
+            if(swapped == false) {
+                // Array is already sorted
+                // No need to do further passes
+                break;
+            }
+        }
+        
+        return true;
+    }
+    
     bool isSorted(vector<int>& nums) {
         for(int i = 0; i < nums.size()-1; i++) {
             if(nums[i] > nums[i+1])
@@ -8,8 +46,9 @@ public:
         return true;
     }
     
-    bool canSortArray(vector<int>& nums) {
-        
+    // Approach 2: By creating set of segments in map and checking if segments are sorted
+    // (Solved by own)
+    bool canSortArray2(vector<int>& nums) {
         if(isSorted(nums))
             return true;
         
@@ -37,5 +76,15 @@ public:
              prev = it;
         }
         return true;
+    }
+    
+    bool canSortArray(vector<int>& nums) {
+        // Approach 1
+        return canSortArray1(nums); 
+        
+        // Approach 2
+        // return canSortArray2(nums);
+        
+        
     }
 };
