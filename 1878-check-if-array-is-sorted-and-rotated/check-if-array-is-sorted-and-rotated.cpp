3e4193e401f1a0bde555462c50c10fdc33806e8f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-07 09:45:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (57.19%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // Approach 1: Left rotate array everytime and check if sorted
+    // T.C. = O(n^2)
+    // S.C. = O(1)
     bool isSorted(vector<int> nums) {
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i] < nums[i-1])
@@ -8,15 +11,41 @@ public:
         return true;
     }
     
-    bool check(vector<int>& nums) {
+    bool check1(vector<int>& nums) {
         int n = nums.size();
         for(int i = 0; i < n; i++) {
             vector<int> copy = nums;
+            // Left rotate original array 'i' times
             rotate(copy.begin(), copy.begin()+i, copy.end());
             if(isSorted(copy))
                 return true;
         }
         
         return false;
+    }
+    
+    // Approach 2: By counting non-sorted pair of elements
+    // T.C. = O(n)
+    // S.C. = O(1)
+    bool check2(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        for(int i = 1; i < n; i++) {
+            if(nums[i] < nums[i-1])     // non-sorted pair
+                count++;
+        }
+        
+        if(nums[n-1] > nums[0])         // non-sorted pair
+            count++;
+        
+        return count <= 1;
+    }
+    
+    bool check(vector<int>& nums) {
+        // Approach 1
+        // return check1(nums);
+        
+        // Approach 2
+        return check2(nums);
     }
 };
