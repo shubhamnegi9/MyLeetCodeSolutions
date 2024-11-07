239f0b4bf970d938d08e26822f71237556d41c72
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-07 22:41:17 +0530

    Time: 23 ms (79.85%) | Memory: 60 MB (88.86%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -18,7 +18,29 @@ public:
         return maxSize;
     }
     
+    // Approach 2: Checking the largest combination having ith bit as set using count variable
+    // T.C. = O(24*n) = O(n)
+    // S.C. = O(1)
+    int largestCombination2(vector<int>& candidates) {
+        int maxSize = 0;
+        for(int i = 0; i < 24; i++) {                   // O(24)
+            int count = 0;  // Intialize count of numbers having ith bit as set as 0 for each position
+            for(int &num: candidates) {                 // O(n)
+                // Check if ith bit in num is set
+                if((num & (1 << i)) > 0) {     // Put brackets carefully!
+                    count++;
+                }
+            }
+            maxSize = max(maxSize, count);
+        }
+        return maxSize;
+    }
+    
     int largestCombination(vector<int>& candidates) {
-        return largestCombination1(candidates);
+        // Approach 1
+        // return largestCombination1(candidates);
+        
+        // Approach 2
+        return largestCombination2(candidates);
     }
 };
