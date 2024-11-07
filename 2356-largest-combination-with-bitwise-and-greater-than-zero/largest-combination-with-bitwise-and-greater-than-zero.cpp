00ed7af075ed68a461028d13eaace6618f22bedb
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-07 23:26:23 +0530

    Time: 31 ms (60.45%) | Memory: 60.2 MB (32.98%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -36,11 +36,61 @@ public:
         return maxSize;
     }
     
+    // Approach 3: Using Recursion (Will give TLE)
+    int solve(vector<int>& candidates, int i, int bitwiseAND, int count) {
+        
+        if(i == candidates.size())
+            return bitwiseAND > 0 ? count : 0;
+        
+        // Take the current element in combination
+        int take = solve(candidates, i+1, bitwiseAND & candidates[i], count+1);
+        
+        // Not take the current element in combination
+        int notTake = solve(candidates, i+1, bitwiseAND, count);
+        
+        return max(take, notTake);
+    }
+    
+    int largestCombination3(vector<int>& candidates) {
+        return solve(candidates, 0, -1, 0);
+    }
+    
+    // Approach 4: Using Top Down Memoization (Will give TLE)
+    int solveMemo(vector<int>& candidates, int i, int bitwiseAND, int count, map<int, map<int, map<int, int>>> &memo) {
+        
+        if(i == candidates.size())
+            return bitwiseAND > 0 ? count : 0;
+        
+        if(memo[i][bitwiseAND].find(count) != memo[i][bitwiseAND].end())
+        return memo[i][bitwiseAND][count];
+        
+        // Take the current element in combination
+        int take = solveMemo(candidates, i+1, bitwiseAND & candidates[i], count+1, memo);
+        
+        // Not take the current element in combination
+        int notTake = solveMemo(candidates, i+1, bitwiseAND, count, memo);
+        
+        return memo[i][bitwiseAND][count] = max(take, notTake);
+    }
+    
+    int largestCombination4(vector<int>& candidates) {
+        // Define a nested map for memoization
+        // We need to store 3 variables: i, bitwiseAND, count as key and 1 value
+        map<int, map<int, map<int, int>>> memo;
+        return solveMemo(candidates, 0, -1, 0, memo);
+    }
+    
     int largestCombination(vector<int>& candidates) {
         // Approach 1
         // return largestCombination1(candidates);
         
         // Approach 2
         return largestCombination2(candidates);
+        
+        // Approach 3 (Will give TLE)
+        // return largestCombination3(candidates);
+        
+        // Approach 4 (Will give TLE)
+        // return largestCombination4(candidates);
     }
 };
