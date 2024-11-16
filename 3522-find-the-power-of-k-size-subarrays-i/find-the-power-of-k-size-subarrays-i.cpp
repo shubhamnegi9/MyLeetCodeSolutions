8fd85424d09cacbd36de041c39d45b9050025055
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-16 22:07:21 +0530

    Time: 0 ms (100.00%) | Memory: 32.8 MB (91.28%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+    // Brute Force Approach
+    // T.C. = O(n-k+1) * O(k) = O(n*k)
+    // S.C. = O(k) extra space for solving problem + O(n-k+1) space for returning result 
     bool isSortedAndConsecutive(vector<int>& nums) {
         int n = nums.size();
         for(int i = 1; i < n; i++) {
@@ -9,7 +13,7 @@ public:
         return true;
     }
     
-    vector<int> resultsArray(vector<int>& nums, int k) {
+    vector<int> resultsArray1(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> result;
         for(int i = 0; i < n-k+1; i++) {
@@ -21,5 +25,56 @@ public:
             result.push_back(power);
         }
         return result;
+    }
+    
+    // Optimal Approach using Sliding Window
+    // T.C. = O(n)
+    // S.C. = O(n-k+1) space for returning result 
+    vector<int> resultsArray2(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> result(n-k+1, -1);
+        int count = 1;      // Count of consecutive elements
+        
+        // Preprocess first window
+        // First window will be from index 0 to index k-1
+        for(int i = 1; i < k; i++) {
+            if(nums[i] == nums[i-1]+1)  
+                count++;
+            else 
+                count = 1;  // Reset count of consecutive elements
+        }
+        if(count == k) {
+            result[0] = nums[k-1];  // Last element of window will be maximum 
+        }
+        
+        // Process futher windows
+        // Next window will be from index 1 to index k
+        int i = 1;
+        int j = k;
+        while(j < n) {
+            if(nums[j] == nums[j-1]+1) {
+                count++;
+            } else {
+                count = 1;  // Reset count of consecutive elements
+            }
+            
+            if(count >= k) {
+                result[i] = nums[j];    // Last element of window will be maximum 
+            }
+            
+            // Slide the window
+            i++;
+            j++;
+        }
+        
+        return result;
+    }
+    
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        // Brute Force Approach
+        // return resultsArray1(nums, k);
+        
+        // Optimal Approach
+        return resultsArray2(nums, k);
     }
 };
