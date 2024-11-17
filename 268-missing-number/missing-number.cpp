9c281331ad922b33b683fafd8e57cb976d16b8ab
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-17 10:55:16 +0530

    Time: 15 ms (11.42%) | Memory: 28.1 MB (5.03%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Approach 1: Brute Force
     // T.C. = O(n^2)
     // S.C. = O(1)
     int missingNumber1(vector<int>& nums) {
@@ -17,7 +18,49 @@ public:
         }
         return -1;
     }
+    
+    // Approach 2: Better Approach using Hashing
+    // T.C. = O(2n) = O(n)
+    // S.C. = O(n)
+    int missingNumber2(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> hash(n+1, 0);
+        
+        for(int i = 0; i < n; i++) {
+            hash[nums[i]] = 1;
+        }
+        
+        for(int i = 0; i <= n; i++) {
+            if(hash[i] == 0)
+                return i;
+        }
+        
+        return -1;
+    }
+    
+    // Approach 3: Better Approach using Set
+    // T.C. = O(n)
+    // S.C. = O(n)
+    int missingNumber3(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        for(int i = 0; i <= n; i++) {
+            if(st.find(i) == st.end())
+                return i;
+        }
+        
+        return -1;
+    }
+    
     int missingNumber(vector<int>& nums) {
-        return missingNumber1(nums);
+        // Approach 1: Brute Force
+        // return missingNumber1(nums);
+        
+        // Approach 2: Better Approach using Hashing
+        // return missingNumber2(nums);
+        
+        // Approach 3: Better Approach using Set
+        return missingNumber3(nums);
     }
 };
