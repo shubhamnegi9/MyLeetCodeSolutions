736b942df43eb599a39fb596fd09035f5ce44fb5
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-20 09:47:36 +0530

    Time: 2741 ms (8.52%) | Memory: 33.2 MB (80.18%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
+    // Brute Force Approach
+    int subarraySum1(vector<int>& nums, int k) {
         int n = nums.size();
         int count = 0;
-        int preSum = 0;
-        unordered_map<int, int> prefixSumMap;
-        // Initially storing <0, 1> in hashmap
-        prefixSumMap[0] = 1;
+        
         for(int i = 0; i < n; i++) {
-            preSum += nums[i];
-            // If (preSum - k) is present in prefixSumMap 
-            if(prefixSumMap.find(preSum - k) != prefixSumMap.end()) {
-                count += prefixSumMap[preSum - k];
+            int sum = 0;
+            for(int j = i; j < n; j++) {
+                sum+=nums[j];
+                if(sum == k)
+                    count++;
             }
-            prefixSumMap[preSum]++;
         }
+        
         return count;
+    }
+    
+    int subarraySum(vector<int>& nums, int k) {
+        // Brute Force Approach
+        return subarraySum1(nums, k);
     }
 };
