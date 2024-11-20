4171dcde82f99655a435c638ed49b3a881130597
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-20 09:55:04 +0530

    Time: 46 ms (67.50%) | Memory: 51 MB (22.00%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     // Brute Force Approach
+    // T.C. = O(n^2)
+    // S.C. = O(1)
     int subarraySum1(vector<int>& nums, int k) {
         int n = nums.size();
         int count = 0;
@@ -17,8 +19,31 @@ public:
         return count;
     }
     
+    // Optimal Approach using hashing
+    // T.C. = O(n)
+    // S.C. = O(n)
+    int subarraySum2(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        unordered_map<int, int> prefixSumMap;
+        // Adding entry <0, 1> in map initially
+        prefixSumMap[0] = 1;
+        int sum = 0;
+        
+        for(int i = 0; i < n; i++) {
+            sum+=nums[i];
+            count+=prefixSumMap[sum-k];
+            prefixSumMap[sum]++;
+        }
+        
+        return count;
+    }
+    
     int subarraySum(vector<int>& nums, int k) {
         // Brute Force Approach
-        return subarraySum1(nums, k);
+        // return subarraySum1(nums, k);
+        
+        // Optimal Approach using hashing
+        return subarraySum2(nums, k);
     }
 };
