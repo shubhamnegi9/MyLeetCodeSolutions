78f61ace1f9244deb4db65d0b6ff828478547db1
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-24 18:56:53 +0530

    Time: 7 ms (11.44%) | Memory: 38.4 MB (72.97%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
+    // T.C. = O(n*n)
+    // S.C. = O(1)
     long long maxMatrixSum(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int n = matrix.size();
         
         long long sum = 0;
-        int count = 0;
+        int countNegative = 0;
         int minEle = INT_MAX;
         
-        for(int i = 0; i < m; i++) {
+        for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 int currEleAbs = abs(matrix[i][j]);
                 sum += currEleAbs;
                 minEle = min(minEle, currEleAbs);
                 if(matrix[i][j] < 0)
-                    count++;
+                    countNegative++;
             }
         }
         
-        if(count%2 == 0) {
+        if(countNegative%2 == 0) {
             return sum;
         } else {
             return sum - 2*minEle;
