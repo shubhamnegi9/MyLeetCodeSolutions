c62436faa57a1b2948f4d11a73835c53a8407545
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-11-29 22:36:56 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (5.24%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,51 +1,16 @@
 class Solution {
 public:
-    // Approach 1: Left rotate array everytime and check if sorted
-    // T.C. = O(n^2)
-    // S.C. = O(1)
-    bool isSorted(vector<int> nums) {
-        for(int i = 1; i < nums.size(); i++) {
-            if(nums[i] < nums[i-1])
-                return false;
-        }    
-        return true;
-    }
-    
-    bool check1(vector<int>& nums) {
-        int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            vector<int> copy = nums;
-            // Left rotate original array 'i' times
-            rotate(copy.begin(), copy.begin()+i, copy.end());
-            if(isSorted(copy))
-                return true;
-        }
-        
-        return false;
-    }
-    
-    // Approach 2: By counting non-sorted pair of elements
-    // T.C. = O(n)
-    // S.C. = O(1)
-    bool check2(vector<int>& nums) {
-        int n = nums.size();
+    bool check(vector<int>& nums) {
         int count = 0;
-        for(int i = 1; i < n; i++) {
-            if(nums[i] < nums[i-1])     // non-sorted pair
+        int n = nums.size();
+        if(nums.back() > nums.front())
+            count++;
+        
+        for(int i = 0; i < n-1; i++) {
+            if(nums[i] > nums[i+1])
                 count++;
         }
         
-        if(nums[n-1] > nums[0])         // non-sorted pair
-            count++;
-        
-        return count <= 1;
-    }
-    
-    bool check(vector<int>& nums) {
-        // Approach 1
-        // return check1(nums);
-        
-        // Approach 2
-        return check2(nums);
+        return (count <= 1);
     }
 };
