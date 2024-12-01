3be813897b7e54f6539fdc3e4b9416107bb00ea2
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-01 18:40:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (17.49%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     // Approach 1: Using brute force
+    // T.C. = O(n^2)
+    // S.C. = O(1)
     bool checkIfExist1(vector<int>& arr) {
         int n = arr.size();
         for(int i = 0; i < n; i++) {
             for(int j = 1; j < n; j++) {
-                if(i != j && arr[j] == 2*arr[i]) {
+                if(i != j && (arr[j] == 2*arr[i] || (arr[i] % 2 == 0 && arr[j] == arr[i]/2))) {
                     return true;
                 }
             }
@@ -29,7 +31,7 @@ public:
     
     bool checkIfExist(vector<int>& arr) {
         // Approach 1: Using brute force
-        return checkIfExist4(arr);
+        return checkIfExist1(arr);
         
         // Approach 4: Using map
         // return checkIfExist4(arr);
