8cd2573aa9c3318746d8cf0bcd7e7975eea71058
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-02 21:53:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (6.42%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Approach 1
     int isPrefixOfWord1(string sentence, string searchWord) {
         string word = "";
         vector<string> wordList;
@@ -26,8 +27,35 @@ public:
     
         return res;
     }
+    
+    // Approach 2: Using iStringStream 
+    int isPrefixOfWord2(string sentence, string searchWord) {
+        string word = "";
+        vector<string> wordList;
+        istringstream iss(sentence);
+        
+        while(iss >> word) {
+            wordList.push_back(word);
+        }
+        
+        // Checking if searchWord is prefix of any word in wordList
+        int l = searchWord.length();
+        int res = -1;
+        for(int i = 0; i < wordList.size(); i++) {
+            if(wordList[i].length() >= l && wordList[i].substr(0, l) == searchWord) {
+                res = i+1;
+                break;
+            }
+        }
+    
+        return res;
+    }
 
     int isPrefixOfWord(string sentence, string searchWord) {
-        return isPrefixOfWord1(sentence, searchWord);
+        // Approach 1
+        // return isPrefixOfWord1(sentence, searchWord);
+        
+        // Approach 2: Using iStringStream 
+        return isPrefixOfWord2(sentence, searchWord);
     }
 };
