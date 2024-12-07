ff903f57676e9234f3f328343c5dea0fa12f3785
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-07 11:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (8.37%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -18,9 +18,19 @@ public:
         }
     }
     
+    void rotateArray2(vector<int>& arr, int k, int n) {
+        k = k%n;
+
+        reverse(arr.begin(), arr.end());
+        reverse(arr.begin(), arr.begin()+k);
+        reverse(arr.begin()+k, arr.end());
+    }
+    
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         
-        rotateArray1(nums, k, n);
+        // rotateArray1(nums, k, n);
+        
+        rotateArray2(nums, k, n);
     }
 };
