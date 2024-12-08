d2327395a35b9bed8b7e85c3209ec4f7cfb2e204
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-08 10:26:17 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (10.02%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,37 +1,60 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-    int n = nums.size();
-		int ele;
-		int count = 0;
+    int majorityElement1(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            int count = 0;
+            for(int j = 0; j < n; j++) {
+                if(nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if(count > n/2)
+                return nums[i];
+        }
+        return -1;
+    }
+    
+    int majorityElementUsingHashing(vector<int> &nums) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        
+        for(int& ele: nums) {
+            mp[ele]++;
+        }
+        
+        for(auto it: mp) {
+            if(it.second> n/2) {
+                return it.first;
+            }
+        }
+        
+        return -1;
+    }
 
-		// Applying Moore's Voting Algo to find majority element 'ele'
-		for(int i = 0; i < n; i++) {
-			
-			if(count == 0) {
-				count = 1;
-				ele = nums[i];
-			}
-			else if(nums[i] == ele) {
-				count++;
-			}
-			else {
-				count--;
-			}
-			
-		}
-		
-		int cnt;
-		// Verifying if the 'ele' is majority element or not.
-		// This can be skipped and we can return 'ele' if given that array always has majority element.
-		for(int i = 0; i < n; i++) {
-			if(nums[i] == ele) {
-				cnt++;
-			}
-		}
-		if(cnt > n/2) {
-			return ele;
-		}
-		return -1;
-  }
+    int majorityElementUsingMooreVoting(vector<int> &nums) {
+        int count = 0, majorityEle = 0;
+        
+        for(int& ele: nums) {
+            if(count == 0) {
+                majorityEle = ele;
+            }
+            
+            if(ele == majorityEle) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return majorityEle;
+    }
+
+    int majorityElement(vector<int>& nums) {
+        // return majorityElement1(nums);
+        
+        // return majorityElementUsingHashing(nums);
+
+        return majorityElementUsingMooreVoting(nums);
+    }
 };
