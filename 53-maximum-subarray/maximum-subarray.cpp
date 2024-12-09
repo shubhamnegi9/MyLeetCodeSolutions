30d2cbf94c98ba7235c238cf328a62a03ef56570
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-09 08:03:48 +0530

    Time: 6 ms (6.20%) | Memory: 71.7 MB (13.45%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,14 +1,45 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int sum = 0, maxSum = INT_MIN;
+    // Brute Force Aproach
+    // T.C. = O(n^2)
+    // S.C. = O(1)
+    int maxSubArray1(vector<int>& nums) {
         int n = nums.size();
+        int maxSum = INT_MIN;
+        
         for(int i = 0; i < n; i++) {
-            sum += nums[i];
-            maxSum = max(maxSum, sum);
-            if(sum < 0) sum = 0;
+            int sum = 0;
+            for(long j = i; j < n; j++) {
+                sum += nums[j];
+                maxSum = max(maxSum, sum);
+            }
         }
-
+        
         return maxSum;
+    }
+    
+    // Optimal Approach using Kadane's Algo
+    // T.C. = O(n)
+    // S.C. = O(1)
+    int maxSubArray2(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        int maxSum = INT_MIN;
+        for(int i = 0; i < n; i++) {
+            sum+=nums[i];
+            maxSum = max(maxSum, sum);
+            if(sum < 0)
+                sum=  0;
+        }
+        
+        return maxSum;
+    }
+    
+    int maxSubArray(vector<int>& nums) {
+        // Brute Force Aproach
+        // return maxSubArray1(nums);
+        
+        // Optimal Approach using Kadane's Algo
+        return maxSubArray2(nums);
     }
 };
