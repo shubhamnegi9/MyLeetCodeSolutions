de8fec9e5a532246822554187aa40d0e0f685372
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-18 23:18:09 +0530

    Time: 10 ms (39.63%) | Memory: 138.9 MB (21.72%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
-    vector<int> rearrangeArray1(vector<int>& nums) {
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 0);
         vector<int> positives;
         vector<int> negatives;
         
         for(int& ele: nums) {
-            if(ele > 0)
-                positives.push_back(ele);
-            else
+            if(ele < 0)
                 negatives.push_back(ele);
+            else
+                positives.push_back(ele);
         }
         
-        int n = nums.size();
         for(int i = 0; i < n/2; i++) {
-            nums[2*i] = positives[i];
-            nums[2*i+1] = negatives[i];
-        }
-        
-        return nums;
-    }
-    
-    vector<int> rearrangeArray2(vector<int>& nums) {
-        int posIndex = 0, negIndex = 1;
-        int n = nums.size();
-        vector<int> result(n, 0);
-        
-        for(int& ele: nums) {
-            if(ele > 0) {
-                result[posIndex] = ele;
-                posIndex+=2;
-            } else{
-                result[negIndex] = ele;
-                negIndex+=2;
-            }
+            result[2*i] = positives[i];
+            result[2*i+1] = negatives[i];
         }
         
         return result;
-    }
-    
-    vector<int> rearrangeArray(vector<int>& nums) {
-        // return rearrangeArray1(nums);
-        
-        return rearrangeArray2(nums);
     }
 };
