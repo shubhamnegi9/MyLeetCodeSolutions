cd555bbec28de6f6c9af51f14a3610e2fd0b44d1
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-18 21:52:51 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (9.86%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -8,24 +8,53 @@ public:
         }
         return true;
     }
-    vector<int> finalPrices(vector<int>& prices) {
+    
+    // BruteForce Approach
+    // T.C. = O(n^2)
+    // S.C. = O(1) (O(n) if separate result vector is used)
+    vector<int> finalPrices1(vector<int>& prices) {
         if(isIncreasingSorted(prices)) {
             return prices;
         }
         
         int n = prices.size();
         for(int i = 0; i < n-1; i++) {
-            int discount = -1;
             for(int j = i+1; j < n; j++) {
                 if(prices[j] <= prices[i]) {
-                    discount = prices[j];
+                    prices[i]-=prices[j]; 
                     break;
                 }
             }
-            if(discount != -1) 
-               prices[i]-=discount; 
         }
         
         return prices;
+    }
+    
+    // Optimal Approach using Monotonic Stack
+    vector<int> finalPrices2(vector<int>& prices) {
+        vector<int> result(prices); // Copy prices vector into result vector so that input is not modified
+        int n = prices.size();
+        stack<int> st;
+        
+        for(int i = 0; i < n; i++) {
+            
+            while(!st.empty() && prices[i] <= result[st.top()]) {
+                result[st.top()]-=prices[i];
+                st.pop();
+            }
+            
+            st.push(i);
+        }
+        
+        return result;
+    }
+    
+    vector<int> finalPrices(vector<int>& prices) {
+        
+        // BruteForce Approach
+        // return finalPrices1(prices);
+        
+        // Optimal Approach using Monotonic Stack
+        return finalPrices2(prices);
     }
 };
