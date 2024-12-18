685c288963e5626d89eb7b60b9696f09b48a63e4
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-18 22:57:19 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (23.04%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,47 +1,16 @@
 class Solution {
 public:
-    
-    // Brute Force Approach
-    // T.C. = O(n^2)
-    // S.C. = O(1)
-    vector<int> twoSum1(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> result;
-        for(int i = 0; i < n-1; i++) {
-            for(int j = i+1; j < n; j++) {
-                if(nums[i] + nums[j] == target) {
-                    result = {i, j};
-                    break;
-                }
-            }
-        }
-        
-        return result;
-    }
-    
-    // Optimal Approach
-    // T.C. = O(n) for loop + O(1) for find/insert in unordered map
-    //      = O(n)
-    // S.C. = O(n) for map
-    vector<int> twoSum2(vector<int> &nums, int target) {
-        int n = nums.size();
+    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> mp;
+        int n = nums.size();
+        
         for(int i = 0; i < n; i++) {
-            int remaining = target - nums[i];
-            if(mp.find(remaining) != mp.end()){     // Element is present in map
-                return {mp[remaining], i};
+            if(mp.find(target - nums[i]) != mp.end()) {
+                return {mp[target-nums[i]], i};
             }
             mp[nums[i]] = i;
         }
         
         return {};
-    }
-    
-    vector<int> twoSum(vector<int>& nums, int target) {
-        // Brute Force Approach
-        // return twoSum1(nums, target);
-        
-        // Optimal Approach
-        return twoSum2(nums, target);
     }
 };
