d896dbd4fe5d131d3af654fdf3627f4e4d0251e3
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-21 23:22:00 +0530

    Time: 15 ms (86.29%) | Memory: 138.2 MB (10.97%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -8,65 +8,51 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-     
-        vector<vector<int>> matrix(m, vector<int> (n, -1));
+        int left = 0, right = n-1, top = 0, bottom = m-1;
+        int id = 0;
+        vector<vector<int>> result(m, vector<int> (n, -1));
         
-        int top = 0;
-        int bottom = m-1;
-        int left = 0;
-        int right = n-1;
-        
-        int id = 0;     // For tracking the direction
-        //0   -> left  to right
-        //1   -> top   to down
-        //2   -> right to left
-        //3   -> down  to top
-
-        while(top <= bottom && left <= right) {     // We will not check for (head != NULL) here, as it can be possible that head is not null when it reaches here but later it can become null in any of the inner loops and on doing head->val it will give error. 
+        while(left <= right && top <= bottom) {
             
             if(id == 0) {
-                // Left to Right
+                // Left
                 for(int i = left; i <= right && head != NULL; i++) {
-                    matrix[top][i] = head->val;
+                    result[top][i] = head->val;
                     head = head->next;
                 }
                 top++;
             }
-            
-            if(id == 1) {
-                // Top to Bottom
-                for(int i = top; i <= bottom && head != NULL; i++) {
-                    matrix[i][right] = head->val;
+            else if(id == 1) {
+                // Down
+                for(int i = top; i <= bottom && head != NULL ; i++) {
+                    result[i][right] = head->val;
                     head = head->next;
                 }
                 right--;
             }
-            
-            if(id == 2) {
-                // Right to Left
+            else if(id == 2) {
+                // Right
                 for(int i = right; i >= left && head != NULL; i--) {
-                    matrix[bottom][i] = head->val;
+                    result[bottom][i] = head->val;
                     head = head->next;
                 }
                 bottom--;
             }
-            
-            if(id == 3) {
-                // Down to Top
+            else {
+                // Top
                 for(int i = bottom; i >= top && head != NULL; i--) {
-                    matrix[i][left] = head->val;
+                    result[i][left] = head->val;
                     head = head->next;
                 }
                 left++;
             }
             
-            id = (id+1)%4;  // Increment the 'id' mod 4 so it repeates after reaching 3
+            id = (id+1)%4;
         }
         
-        return matrix;
+        return result;
     }
 };
