2246d767d0c4db14ac0e198c2b8d7e2c8814135f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-21 22:24:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (15.43%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,66 +1,56 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        
-        if(n == 0) {
-            return {};
-        }
-        
         vector<vector<int>> matrix(n, vector<int> (n, 0));
-        
-        int left = 0;
-        int right = n-1;
         int top = 0;
         int bottom = n-1;
+        int left = 0;
+        int right = n-1;
 
-        int id = 0;     // For tracking the direction
-        //0   -> left  to right
-        //1   -> top   to down
-        //2   -> right to left
-        //3   -> down  to top
-        
-        int num = 1;
-        while(left <= right && top <= bottom) {
-            
+        int id = 0, num = 1; 
+
+        while(top <= bottom && left <= right) {
+
             if(id == 0) {
                 // Left to Right
                 for(int i = left; i <= right; i++) {
                     matrix[top][i] = num;
                     num++;
                 }
-                top++;
+                top++;  // Increment the constant 'top' in loop
             }
-            
+
             if(id == 1) {
                 // Top to Bottom
                 for(int i = top; i <= bottom; i++) {
                     matrix[i][right] = num;
                     num++;
                 }
-                right--;
-            }
-            
+                right--;    // Decrement the constant 'right' in loop
+            }   
+
             if(id == 2) {
                 // Right to Left
                 for(int i = right; i >= left; i--) {
                     matrix[bottom][i] = num;
                     num++;
                 }
-                bottom--;
+                bottom--;   // Decrement the constant 'bottom' in loop
             }
-            
-            if(id == 3) {
+
+            if(id == 3) { 
                 // Bottom to Top
                 for(int i = bottom; i >= top; i--) {
                     matrix[i][left] = num;
                     num++;
                 }
-                left++;
+                left++; // Increment the constant 'left' in loop
             }
-            
+
             id = (id+1)%4;  // Increment the 'id' mod 4 so it repeates after reaching 3
+
         }
-        
+
         return matrix;
     }
 };
