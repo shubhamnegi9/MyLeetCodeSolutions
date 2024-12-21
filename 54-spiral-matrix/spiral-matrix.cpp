6acefff47d05cdb11332e6276ee3c4f3370c4980
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-21 22:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (18.85%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,61 +1,47 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        
+    // Optimal Approach 1
+    vector<int> spiralOrder1(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        
+        int left = 0, right = n-1, top =- 0, bottom = m-1;
         vector<int> result;
         
-        int top = 0;
-        int bottom = m-1;
-        int left = 0;
-        int right = n-1;
-        
-        int id = 0;     // For tracking the direction
-        //0   -> left  to right
-        //1   -> top   to down
-        //2   -> right to left
-        //3   -> down  to top
-        
-        while(top <= bottom && left <= right) {
+        while(left <= right && top <= bottom) {
             
-            if(id == 0) {
-                // Left to Right
-                for(int i = left; i <= right; i++) {
-                    result.push_back(matrix[top][i]);
-                }
-                top++;  // Increment the constant 'top' in loop
+            // Right
+            for(int i = left; i <= right; i++) {
+                result.push_back(matrix[top][i]);
             }
+            top++;
             
-            if(id == 1) {
-                // Top to Bottom
-                for(int i = top; i <= bottom; i++) {
-                    result.push_back(matrix[i][right]);
-                }
-                right--;    // Increment the constant 'right' in loop
-            }   
+            // Down
+            for(int i = top; i <= bottom; i++) {
+                result.push_back(matrix[i][right]);
+            }
+            right--;
             
-            if(id == 2) {
-                // Right to Left
+            // Left
+            if(top <= bottom) {
                 for(int i = right; i >= left; i--) {
                     result.push_back(matrix[bottom][i]);
                 }
-                bottom--;   // Increment the constant 'bottom' in loop
+                bottom--;
             }
             
-            if(id == 3) { 
-                // Bottom to Top
+            // Top
+            if(left <= right) {
                 for(int i = bottom; i >= top; i--) {
                     result.push_back(matrix[i][left]);
                 }
-                left++; // Increment the constant 'left' in loop
+                left++;
             }
-            
-            id = (id+1)%4;  // Increment the 'id' mod 4 so it repeates after reaching 3
-            
         }
         
         return result;
+    }
+    
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        return spiralOrder1(matrix);
     }
 };
