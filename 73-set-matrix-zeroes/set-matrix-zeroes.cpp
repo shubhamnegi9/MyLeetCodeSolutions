f37f7f32b882b32133f91c5f7d413f4640c75f3e
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-21 20:41:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (9.72%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -81,6 +81,9 @@ public:
         }
     }
     
+    // Optimal Approach
+    // T.C. = 2 * O(m*n)
+    // S.C. = O(1)
     void setZeroes4(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
@@ -125,6 +128,59 @@ public:
         }
     }
     
+    // Another Optimal Approach
+    // T.C. = O(m*n) overall
+    // S.C. = O(1)
+    void setZeroes5(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        bool firstRowZero = false;
+        bool firstColZero = false;
+        
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        
+        for(int j = 0; j < n; j++) {
+            if(matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if(firstRowZero) {
+            for(int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(firstColZero) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+    
     void setZeroes(vector<vector<int>>& matrix) {
         // Brute Force Approach with extra space
         // return setZeroes1(matrix);
@@ -136,6 +192,9 @@ public:
         // return setZeroes3(matrix);
         
         // Optimal Approach
-        return setZeroes4(matrix);
+        // return setZeroes4(matrix);
+        
+        // Another Optimal Approach
+        return setZeroes5(matrix);
     }
 };
