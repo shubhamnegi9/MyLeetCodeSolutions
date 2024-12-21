5b1c0780d704c9ccfa1093659ca377a9d2a04f6f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-21 20:03:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (30.56%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -81,6 +81,50 @@ public:
         }
     }
     
+    void setZeroes4(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int col0 = 1;
+        
+        // Filling row vector (matrix[i][0]) and column vector (matrix[0][j], col0) with 0s
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    // Filling row vector
+                    matrix[i][0] = 0;
+                    
+                    // Filling column vector
+                    if(j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+        
+        // Setting remaining elements to 0s based on row and column vector values
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+        
+        // Setting column vector values to 0s first based on matrix[0][0]
+        if(matrix[0][0] == 0) {
+            for(int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        // Setting row vector values to 0s then based on col0
+        if(col0 == 0) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+    
     void setZeroes(vector<vector<int>>& matrix) {
         // Brute Force Approach with extra space
         // return setZeroes1(matrix);
@@ -89,6 +133,9 @@ public:
         // return setZeroes2(matrix);
         
         // Better Approach using row and col vectors
-        return setZeroes3(matrix);
+        // return setZeroes3(matrix);
+        
+        // Optimal Approach
+        return setZeroes4(matrix);
     }
 };
