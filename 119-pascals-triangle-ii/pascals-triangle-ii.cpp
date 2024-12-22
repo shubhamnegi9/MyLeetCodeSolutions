1582fe16f390e18b162da079ea1b9dd862709374
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-22 14:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (22.43%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -57,6 +57,24 @@ public:
         return curr;
     }
     
+    // Optimal Approach
+    // T.C. = O(rowIndex)
+    // S.C. = O(1) extra space
+    vector<int> getRow4(int rowIndex) {
+        int n = rowIndex+1;
+        long long res = 1;
+        vector<int> ans;
+        ans.push_back((int) res);
+        
+        for(int i = 1; i < n; i++) {
+            res = res * (n-i);
+            res /= i;
+            ans.push_back((int) res);
+        }
+        
+        return ans;
+    }
+    
     vector<int> getRow(int rowIndex) {
         // Brute Force Approach 1
         // return getRow1(rowIndex);
@@ -65,8 +83,9 @@ public:
         // return getRow2(rowIndex);
         
         // Better Approach space-wise
-        return getRow3(rowIndex);
+        // return getRow3(rowIndex);
         
         // Optimal Approach
+        return getRow4(rowIndex);
     }
 };
