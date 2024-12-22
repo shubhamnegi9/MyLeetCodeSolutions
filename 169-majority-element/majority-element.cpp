71097a48134cbc1efc16c7adfd2d078581591af8
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-22 14:18:19 +0530

    Time: 3 ms (36.92%) | Memory: 28.2 MB (14.95%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,60 +1,20 @@
 class Solution {
 public:
-    int majorityElement1(vector<int>& nums) {
-        int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            int count = 0;
-            for(int j = 0; j < n; j++) {
-                if(nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-            if(count > n/2)
-                return nums[i];
-        }
-        return -1;
-    }
-    
-    int majorityElementUsingHashing(vector<int> &nums) {
-        unordered_map<int, int> mp;
-        int n = nums.size();
+    int majorityElement(vector<int>& nums) {
+        int majority = nums[0];
+        int count = 1;
         
-        for(int& ele: nums) {
-            mp[ele]++;
-        }
-        
-        for(auto it: mp) {
-            if(it.second> n/2) {
-                return it.first;
-            }
-        }
-        
-        return -1;
-    }
-
-    int majorityElementUsingMooreVoting(vector<int> &nums) {
-        int count = 0, majorityEle = 0;
-        
-        for(int& ele: nums) {
+        for(int i = 1; i < nums.size(); i++) {
             if(count == 0) {
-                majorityEle = ele;
-            }
-            
-            if(ele == majorityEle) {
+                majority = nums[i];
+                count = 1;
+            } else if(majority == nums[i]) {
                 count++;
             } else {
                 count--;
             }
         }
         
-        return majorityEle;
-    }
-
-    int majorityElement(vector<int>& nums) {
-        // return majorityElement1(nums);
-        
-        // return majorityElementUsingHashing(nums);
-
-        return majorityElementUsingMooreVoting(nums);
+        return majority;
     }
 };
