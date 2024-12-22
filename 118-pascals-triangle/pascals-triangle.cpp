21bf534104554b29f59addd5c62b9296a7baa33e
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-22 13:23:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (6.74%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,16 +1,61 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
+    
+    int findnCr(int n, int r) {     // O(numRows)
+        int res = 1;
+        for(int i = 0; i < r; i++) {
+            res *= (n-i);
+            res /= (i+1);
+        }
+        return res;
+    }
+    
+    // Brute Force Approach
+    // T.C. = O(numRows*numRows) * O(numRows)
+    // S.C. = O(1) extra space
+    vector<vector<int>> generate1(int numRows) {
         vector<vector<int>> result(numRows);
         
-        for(int i = 0; i < numRows; i++) {
-            result[i] = vector<int>(i+1, 1);
-            
-            for(int j = 1; j < i; j++) {
-                result[i][j] = result[i-1][j] + result[i-1][j-1];
-            } 
+        for(int r = 1; r <= numRows; r++) {
+            result[r-1] = vector<int>(r);
+            for(int c = 1; c <= r; c++) {
+                result[r-1][c-1] = findnCr(r-1, c-1);
+            }
         }
         
         return result;
+    }
+    
+    vector<int> generateRow(int n) {     // O(numRows)
+        vector<int> ans;
+        int res = 1;
+        ans.push_back(res);
+        for(int i = 1; i < n; i++) {
+            res *= (n-i);
+            res /= i;
+            ans.push_back(res);
+        }
+        return ans;
+    }
+    
+    // Optimal Approach 1
+    // T.C. = O(numRows*numRows)
+    // S.C. = O(1) extra space
+    vector<vector<int>> generate2(int numRows) {
+        vector<vector<int>> result(numRows);
+        
+        for(int r = 1; r <= numRows; r++) {
+            result[r-1] = generateRow(r);
+        }
+        
+        return result;  
+    }
+    
+    vector<vector<int>> generate(int numRows) {
+        // Brute Force Approach
+        return generate1(numRows);
+        
+        // Optimal Approach 1
+        // return generate2(numRows);
     }
 };
