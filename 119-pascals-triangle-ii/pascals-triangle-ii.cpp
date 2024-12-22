f8f23cff3b7e6f555710cbb401463a1b4ac5e2a5
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-22 13:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (9.27%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -5,14 +5,14 @@ public:
         long long res = 1;
         
         for(int i = 0; i < r; i++) {
-            res = (long long) res * (n-i);
+            res = (long long) res * (n-i);  // Handle signed int overflow
             res /= (i+1);
         }
         
         return (int) res;
     }
     
-    // Brute Force Approach
+    // Brute Force Approach 1
     // T.C. = O(rowIndex*rowIndex)
     // S.C. = O(1) extra space
     vector<int> getRow1(int rowIndex) {
@@ -25,8 +25,27 @@ public:
         return result;
     }
     
+    // Brute Force Approach 2
+    // T.C. = O(rowIndex*rowIndex)
+    // S.C. = O(1) extra space
+    vector<int> getRow2(int rowIndex) {
+        vector<vector<int>> result(rowIndex+1);
+            
+        for(int i = 0; i < rowIndex+1; i++) {
+            result[i] = vector<int> (i+1, 1);
+            for(int j = 1; j < i; j++) {
+                result[i][j] = result[i-1][j] + result[i-1][j-1]; 
+            }
+        }
+        
+        return result[rowIndex];
+    }
+    
     vector<int> getRow(int rowIndex) {
         // Brute Force Approach
-        return getRow1(rowIndex);
+        // return getRow1(rowIndex);
+        
+        // Optimal Approach 1
+        return getRow2(rowIndex);
     }
 };
