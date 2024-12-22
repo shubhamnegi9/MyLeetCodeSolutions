468de6cb083ea2f4994b1c6d0d0d0314090a0463
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-22 17:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (57.28%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -25,8 +25,82 @@ public:
         return result;
     }
     
+    // Better Approach
+    // T.C. = O(n)
+    // S.C. = O(n)
+    vector<int> majorityElement2(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        int minCount = (n/3)+1;
+        vector<int> result;
+            
+        for(int& ele: nums) {
+            mp[ele]++;
+            if(mp[ele] == minCount)
+                result.push_back(ele);
+            
+            if(result.size() == 2)
+                break;
+        }
+        
+        return result;
+    }
+    
+    // Optimal Approach    
+    // T.C. = O(n) + O(n)
+    // S.C. = O(1) 
+    vector<int> majorityElement3(vector<int>& nums) {
+        int n = nums.size();
+        int ele1 = INT_MIN, ele2 = INT_MIN, count1 = 0, count2 = 0;
+        
+        for(int& num: nums) {
+            if(count1 == 0 && num != ele2) {
+                count1 = 1;
+                ele1 = num;
+            } 
+            else if(count2 == 0 && num != ele1) {
+                count2 = 1;
+                ele2 = num;
+            }
+            else if(num == ele1) {
+                count1++;
+            }
+            else if(num == ele2) {
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        // Checking if el1 and el2 are majority element or not
+        int minCount = (n/3)+1;
+        vector<int> result;
+        count1 = 0, count2 = 0;
+        for(int& num: nums) {
+            if(num == ele1) 
+                count1++;
+            if(num == ele2)
+                count2++;
+        }
+        if(count1 >= minCount)
+            result.push_back(ele1);
+        if(count2 >= minCount)
+            result.push_back(ele2);
+        
+        return result;
+    }
+    
+    
     vector<int> majorityElement(vector<int>& nums) {
         // Brute Force Approach
-        return majorityElement1(nums);
+        // return majorityElement1(nums);
+        
+        // Better Approach
+        // return majorityElement2(nums);
+        
+        // Optimal Approach 
+        return majorityElement3(nums);
     }
 };
