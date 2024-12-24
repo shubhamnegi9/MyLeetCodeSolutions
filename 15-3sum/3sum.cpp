517c243275b09a72e3fb6ea03fbc4baaf587418e
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-24 08:35:20 +0530

    Time: 48 ms (66.90%) | Memory: 29.1 MB (54.04%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,49 +1,81 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    // Brute Force Approach
+    // T.C. = O(n^3) * O(log(no. of triplets))
+    // S.C. = O(no. of triplets) extra space for set + O(no. of triplets) for vector for returning result
+    vector<vector<int>> threeSum1(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> triplets;
-
-        // Sort given array
-        sort(nums.begin(), nums.end());
-
+        set<vector<int>> tripletSet;
+        
         for(int i = 0; i < n; i++) {
-            // Skipping same nums[i] elements
-            // to avoid duplicate triplets
+            for(int j = i+1; j < n; j++) {
+                for(int k = j+1; k < n; k++) {
+                    if(nums[i]+nums[j]+nums[k] == 0){
+                        vector<int> triplet = {nums[i], nums[j], nums[k]};
+                        sort(triplet.begin(), triplet.end());
+                        tripletSet.insert(triplet);
+                    }
+                }
+            }
+        }
+        
+        vector<vector<int>> result(tripletSet.begin(), tripletSet.end());
+        return result;
+    }
+    
+    // Better Approach
+    // T.C. = O(n^2) * O(log(no. of triplets))
+    // S.C. = O(n) for hashSet + O(no. of triplets) extra space for set + O(no. of triplets) for vector for returning result
+    vector<vector<int>> threeSum2(vector<int>& nums) {
+        int n = nums.size();
+        set<vector<int>> tripletSet;
+        
+        for(int i = 0; i < n; i++) {
+            unordered_set<int> hashSet;     // O(1)
+            for(int j = i+1; j < n; j++) {
+                int third = -(nums[i]+nums[j]);
+                if(hashSet.find(third) != hashSet.end()) {
+                    vector<int> triplet = {nums[i], nums[j], third};
+                    sort(triplet.begin(), triplet.end());
+                    tripletSet.insert(triplet);
+                }
+                hashSet.insert(nums[j]);
+            }
+        }
+        
+        vector<vector<int>> result(tripletSet.begin(), tripletSet.end());
+        return result;
+    }
+    
+    vector<vector<int>> threeSum3(vector<int>& nums) {
+        int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        vector<vector<int>> result;
+        
+        for(int i = 0; i < n; i++) {
+            
             if(i > 0 && nums[i] == nums[i-1])
                 continue;
             
             int j = i+1;
             int k = n-1;
-
-            // Iterate until j does not becomes k or cross k 
+            
             while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+                int sum = nums[i]+nums[j]+nums[k];
 
                 if(sum < 0) {
-                    // Move j to next element
                     j++;
                 }
                 else if(sum > 0) {
-                    // Move k to previous element
                     k--;
                 }
                 else {
-                    // triplet found
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    triplets.push_back(temp);
-
-                    // Move j to next element and 
-                    // k to previous element
+                    result.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
 
-                    /* Keep moving j to next elements
-                    and k to previous elements
-                    if same element found.
-                    Also check the condition j < k everytime
-                    to prevent overflow.
-                    */
                     while(j < k && nums[j] == nums[j-1]) {
                         j++;
                     }
@@ -54,6 +86,18 @@ public:
                 }
             }
         }
-        return triplets;
+        
+        return result;
+    }
+    
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // Brute Force Approach
+        // return threeSum1(nums);
+        
+        // Better Approach
+        // return threeSum2(nums);
+        
+        // Optimal Approach
+        return threeSum3(nums);
     }
 };
