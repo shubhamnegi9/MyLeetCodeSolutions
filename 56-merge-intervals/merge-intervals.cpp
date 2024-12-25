1f32cb0dc914f177adcab059d4a8092967bd4974
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-25 18:21:28 +0530

    Time: 3 ms (91.02%) | Memory: 23.8 MB (40.22%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -32,8 +32,33 @@ public:
         return mergedIntervals;
     }
     
+    // Optimal Approach
+    // T.C. = O(nlogn) + O(n)
+    // S.C. = O(n)
+    vector<vector<int>> merge2(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        
+        sort(intervals.begin(), intervals.end());
+        
+        vector<vector<int>> mergedIntervals;
+        
+        for(int i = 0; i < n; i++) {
+            if(mergedIntervals.empty() || intervals[i][0] > mergedIntervals.back()[1]) {
+                mergedIntervals.push_back(intervals[i]);
+            } 
+            else if(intervals[i][0] <= mergedIntervals.back()[1]){
+                mergedIntervals.back()[1] = max(mergedIntervals.back()[1], intervals[i][1]);
+            }
+        }
+        
+        return mergedIntervals;
+    }
+    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         // Brute Force Approach
-        return merge1(intervals);
+        // return merge1(intervals);
+        
+        // Optimal Approach
+        return merge2(intervals);
     }
 };
