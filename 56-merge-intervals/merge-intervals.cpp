ff286309b9f0d6b2380923036bc9057c5053eba0
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-25 18:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (40.22%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    // Brute Force Approach
+    // T.C. = O(nlogn) + O(2*n)
+    // S.C. = O(n)
+    vector<vector<int>> merge1(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        vector<vector<int>> mergedIntervals;
-
-        // Sorting intervals
+        
         sort(intervals.begin(), intervals.end());
-
+        
+        vector<vector<int>> mergedIntervals;
+        
         for(int i = 0; i < n; i++) {
-            // If it is first pair or if its interval do not overlap with the 
-            // last pair in mergedIntervals
-            if(mergedIntervals.empty() || intervals[i][0] > mergedIntervals.back()[1]) {
-                // add this pair in merged interval
-                mergedIntervals.push_back(intervals[i]);
+            if(!mergedIntervals.empty() && intervals[i][0] <= mergedIntervals.back()[1]) {
+                continue;
             }
-
-            // If the current pair can be merged with last pair in merged interval
-            if(intervals[i][0] <= mergedIntervals.back()[1]) {
-                // Update the ending point of merged pair
-                mergedIntervals.back()[1] = max(mergedIntervals.back()[1], intervals[i][1]);
+            
+            int startTime = intervals[i][0];
+            int endTime = intervals[i][1];
+            
+            for(int j = i+1; j < n; j++) {
+                if(intervals[j][0] <= endTime) {
+                    endTime = max(endTime, intervals[j][1]);
+                }
+                else {
+                    break;
+                }
             }
+            mergedIntervals.push_back({startTime, endTime});
         }
-
+        
         return mergedIntervals;
+    }
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // Brute Force Approach
+        return merge1(intervals);
     }
 };
