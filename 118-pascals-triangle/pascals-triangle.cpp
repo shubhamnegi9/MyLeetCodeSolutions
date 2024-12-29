2e34f445285f1c9f69e8df027dcd337d6efbf12a
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-29 18:01:22 +0530

    Time: 2 ms (14.99%) | Memory: 9.6 MB (50.26%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -13,8 +13,33 @@ public:
 
         return result;
     }
+
+    int findnCr(int n, int r) {
+        int result = 1;
+        for(int i = 0; i < r; i++) {
+            result *= (n-i);
+            result /= (i+1);
+        }
+
+        return result;
+    }
+
+    vector<vector<int>> generate2(int numRows) {
+        vector<vector<int>> result(numRows);
+
+        for(int i = 1; i <= numRows; i++) {
+            result[i-1] = vector<int>(i, 1);
+            for(int j = 1; j <= i; j++) {
+                result[i-1][j-1] = findnCr(i-1, j-1);
+            } 
+        }
+
+        return result;
+    }
     
     vector<vector<int>> generate(int numRows) {
-        return generate1(numRows);
+        // return generate1(numRows);
+
+        return generate2(numRows);
     }
 };
