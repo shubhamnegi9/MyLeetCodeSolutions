b1e3a0ff8bb49b6d50e344c729e532961a43be41
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-29 20:45:35 +0530

    Time: 19 ms (78.69%) | Memory: 17.3 MB (57.43%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -25,7 +25,51 @@ public:
         return vector<vector<int>>(st.begin(), st.end());
     }
     
+    vector<vector<int>> fourSum2(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> result;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            for(int j = i + 1; j < n; j++) {
+                if(j > i+1 && nums[j] == nums[j-1])
+                    continue;
+                int k = j+1;
+                int l = n-1;
+                while(k < l) {
+                    long long sum = static_cast<long long>(nums[i])+nums[j]+nums[k]+nums[l];
+
+                    if(sum > target) {
+                        l--;
+                    }
+                    else if(sum < target) {
+                        k++;
+                    }
+                    else {
+                        result.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+
+                        while(k < l && nums[k] == nums[k-1]) {
+                            k++;
+                        }
+                        while(k < l && nums[l] == nums[l+1]) {
+                            l--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+    
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        return fourSum1(nums, target);
+        // return fourSum1(nums, target);
+
+        return fourSum2(nums, target);
     }
 };
