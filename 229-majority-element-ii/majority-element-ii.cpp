8391ad6eb179bd1d474e81fa05a4d669981231f2
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2024-12-29 18:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (43.76%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     
     vector<int> majorityElement1(vector<int>& nums) {
-        set<int> st;
         int n = nums.size();
+        vector<int> result;
 
         for(int i = 0; i < n; i++) {
-            int count = 0;
-            for(int j = i; j < n; j++) {
-                if(nums[j] == nums[i]) {
-                    count++;
-                    if(count > n/3) {
-                        st.insert(nums[j]);
+            if(result.empty() || nums[i] != result[0]) {
+                int count = 0;
+                for(int j = i; j < n; j++) {
+                    if(nums[j] == nums[i]) {
+                        count++;
                     }
+                }
+                if(count > n/3) {
+                    result.push_back(nums[i]);
+                }
+                if(result.size() == 2) {
+                    break;
                 }
             }
         }
 
-        vector<int> result(st.begin(), st.end());
+        sort(result.begin(), result.end());
         return result;
     }
 
@@ -36,10 +41,16 @@ public:
         vector<int> result(st.begin(), st.end());
         return result;
     }
+
+    // vector<int> majorityElement3(vector<int>& nums) {
+    
+    // }
     
     vector<int> majorityElement(vector<int>& nums) {
-        // return majorityElement1(nums);
+        return majorityElement1(nums);
 
-        return majorityElement2(nums);
+        // return majorityElement2(nums);
+
+        // return majorityElement3(nums);
     }
 };
