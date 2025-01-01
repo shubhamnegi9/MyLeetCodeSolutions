6403897f30e39112c0bd9cb5526528cd1dd93ef6
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-01 19:55:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (23.35%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -3,17 +3,17 @@ public:
     int maxScore(string s) {
         int ones = count(s.begin(), s.end(), '1');
         int zeroes = 0;
-        int ans = 0;
-        for(int i = 0; i < s.size()-1; i++) {
+        int maxScore = 0;
+
+        for(int i = 0; i < s.length()-1; i++) {     // Iterate till (s.length()-2) for left substring as the right substring cannot be empty
             if(s[i] == '0') {
                 zeroes++;
-            }
-            else {
+            } else {
                 ones--;
             }
-            ans = max(ans, zeroes + ones);
+            maxScore = max(maxScore, zeroes+ones);
         }
-
-        return ans;
+        
+        return maxScore;
     }
 };
