9289f9ca404c51588afae5d8c4ffdd04cfd18398
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-01 20:27:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (5.38%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -29,10 +29,8 @@ public:
     }
 
     void merge2(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        int p1 = m-1, p2 = 0;
-
         if(m > 0 && n > 0) {
+            int p1 = m-1, p2 = 0;
             while(p1 >= 0 && p2 < n) {
                 if(nums1[p1] > nums2[p2]) {
                     swap(nums1[p1], nums2[p2]);
