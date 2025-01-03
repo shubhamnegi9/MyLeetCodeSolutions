4265fed6d7b2fbcb7b4164865ee3fb4b8c0a2128
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-03 12:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (34.67%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Brute Force
     // T.C. = O(n^2)
     // S.C. = O(1)
     int maxProduct1(vector<int>& nums) {
@@ -14,8 +15,34 @@ public:
         }
         return maxProd;
     }
+
+    // Optimal Approach
+    // T.C. = O(n)
+    // S.C. = O(1)
+    int maxProduct2(vector<int>& nums) {
+        int n = nums.size();
+        int maxProd = INT_MIN;
+        int prefixProd = 1, suffixProd = 1;
+
+        for(int i = 0; i < n; i++) {
+            if(prefixProd == 0)
+                prefixProd = 1;
+            if(suffixProd == 0)
+                suffixProd = 1;
+
+            prefixProd *= nums[i];
+            suffixProd *= nums[n-1-i];
+            maxProd = max(maxProd, max(prefixProd, suffixProd));
+        }
+
+        return maxProd;
+    }
     
     int maxProduct(vector<int>& nums) {
-        return maxProduct1(nums);
+        // Brute Force
+        // return maxProduct1(nums);
+
+        // Optimal Approach
+        return maxProduct2(nums);
     }
 };
