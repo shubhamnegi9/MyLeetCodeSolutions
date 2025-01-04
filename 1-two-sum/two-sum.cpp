0d7ffab3327ec88d96020da72756ae62779215ac
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-04 19:15:20 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (33.85%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -15,7 +15,24 @@ public:
 
         return {-1, -1};
     }
+
+    // T.C. = O(n)
+    // S.C. = O(1)
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < n; i++) {
+            if(mp.find(target-nums[i]) != mp.end()) {
+                return {mp[target-nums[i]], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {-1, -1};
+    }
     vector<int> twoSum(vector<int>& nums, int target) {
-        return twoSum1(nums, target);
+        // return twoSum1(nums, target);
+
+        return twoSum2(nums, target);
     }
 };
