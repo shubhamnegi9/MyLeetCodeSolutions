21a96dea9edbc820b8c7bbe3317b0a8b1b23084d
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-04 19:44:19 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (43.60%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -17,8 +17,49 @@ public:
 
         return -1;
     }
+
+    // T.C. = O(2n)
+    // S.C. = O(n)
+    int majorityElement2(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+
+        for(int& ele: nums) {
+            mp[ele]++;
+        }
+
+        for(auto it: mp) {
+            if(it.second > n/2)
+                return it.first;
+        }
+        return -1;
+    }
+
+    // T.C. = O(n)
+    // S.C. = O(1)
+    int majorityElement3(vector<int>& nums) {
+        int n = nums.size();
+        int majorityEle = nums[0];
+        int count = 1;
+
+        for(int i = 1; i < n; i++) {
+            if(count == 0) {
+                majorityEle = nums[i];
+                count = 1;
+            }
+            else if(nums[i] == majorityEle)
+                count++;
+            else
+                count--;
+        }
+        return majorityEle;
+    }
     
     int majorityElement(vector<int>& nums) {
-        return majorityElement1(nums);
+        // return majorityElement1(nums);
+
+        // return majorityElement2(nums);
+
+        return majorityElement3(nums);
     }
 };
