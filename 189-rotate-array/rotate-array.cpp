78a422d08947262ae08cc185ff01fd40073d0f04
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-04 11:03:12 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (17.65%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
-    void rotateArray1(vector<int>& arr, int k, int n) {
+    // T.C. = O(n*k)
+    // S.C. = O(1)
+    void rotate1(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+
+        while(k--) {
+            int temp = nums[n-1];
+            for(int i = n-1; i > 0; i--) {
+                nums[i] = nums[i-1];
+            }
+            nums[0] = temp;
+        }
+    }
+
+    // T.C. = O(k) + O(n-k) + O(k) = O(n+k)
+    // S.C. = O(k)
+    void rotate2(vector<int>& nums, int k) {
+        int n = nums.size();
         k = k%n;
 
         vector<int> temp;
-
         for(int i = n-k; i < n; i++) {
-            temp.push_back(arr[i]);
+            temp.push_back(nums[i]);
         }
 
         for(int i = n-k-1; i >= 0; i--) {
-            arr[i+k] = arr[i];
-        }
+            nums[i+k] = nums[i];
+        } 
 
         for(int i = 0; i < k; i++) {
-            arr[i] = temp[i];
+            nums[i] = temp[i];
         }
-    }
-    
-    void rotateArray2(vector<int>& arr, int k, int n) {
-        k = k%n;
-
-        reverse(arr.begin(), arr.end());
-        reverse(arr.begin(), arr.begin()+k);
-        reverse(arr.begin()+k, arr.end());
     }
     
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        
-        // rotateArray1(nums, k, n);
-        
-        rotateArray2(nums, k, n);
+        // return rotate1(nums, k);
+
+        return rotate2(nums, k);
     }
 };
