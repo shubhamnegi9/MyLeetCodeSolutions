6ed233a52c329769741ac775c3a929076f4ee8d0
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-04 11:56:43 +0530

    Time: 19 ms (8.07%) | Memory: 28.1 MB (5.02%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,69 +1,40 @@
 class Solution {
 public:
-    
+    // T.C. = O(n^2)
+    // S.C. = O(1)
     int missingNumber1(vector<int>& nums) {
         int n = nums.size();
         for(int i = 0; i <= n; i++) {
-            bool found = 0;
-            for(int j = 0; j < n; j++) {
-                if(nums[j] == i) {
-                    found = 1;
+            int found = false;
+            for(int& ele: nums) {
+                if(ele == i) {
+                    found = true;
                     break;
                 }
             }
-            if(found == 0) {
+            if(!found)
                 return i;
-            }
         }
         return -1;
     }
-    
+
+    // T.C. = O(n) for inserting in set + O(n) for finding missing number
+    // S.C. = O(n) for set
     int missingNumber2(vector<int>& nums) {
-        unordered_set<int> st(nums.begin(), nums.end());
         int n = nums.size();
+        unordered_set<int> st(nums.begin(), nums.end());
         
         for(int i = 0; i <= n; i++) {
             if(st.find(i) == st.end()) {
                 return i;
             }
         }
-        
         return -1;
-    }
-    
-    int missingNumber3(vector<int>& nums) {
-        int XOR = 0;
-        int n = nums.size();
-        for(int i = 0; i <= n; i++) {
-            XOR ^= i;
-        }
-        
-        for(int ele: nums) {
-            XOR ^= ele;
-        }
-        
-        return XOR;
-    }
-    
-    int missingNumber4(vector<int>& nums) {
-        int n = nums.size();
-        int totalSum = n*(n+1)/2;
-        
-        int sum = 0;
-        for(int ele: nums) {
-            sum+=ele;
-        }
-        
-        return totalSum-sum;
     }
     
     int missingNumber(vector<int>& nums) {
         // return missingNumber1(nums);
-        
-        // return missingNumber2(nums);
-        
-        // return missingNumber3(nums);
-        
-        return missingNumber4(nums);
+
+        return missingNumber2(nums);
     }
 };
