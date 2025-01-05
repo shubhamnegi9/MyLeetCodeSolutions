160153b507d1ea1842080fdf8c2dffc0e4357c07
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-05 08:18:02 +0530

    Time: 8 ms (53.33%) | Memory: 128.4 MB (53.23%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -7,11 +7,11 @@ public:
         vector<int> positives;
         vector<int> negatives;
 
-        for(int& ele: nums) {
-            if(ele < 0)
-                negatives.push_back(ele);
+        for(int& num: nums) {
+            if(num < 0)
+                negatives.push_back(num);
             else
-                positives.push_back(ele);
+                positives.push_back(num);
         }
 
         for(int i = 0; i < n/2; i++) {
@@ -23,25 +23,24 @@ public:
     }
 
     // T.C. = O(n)
-    // S.C. = O(n)
+    // S.C. = O(1)
     vector<int> rearrangeArray2(vector<int>& nums) {
         int n = nums.size();
-        int positiveIdx = 0, negativeIdx = 1;
         vector<int> result(n, 0);
+        int posIndex = 0, negIndex = 1;
 
         for(int i = 0; i < n; i++) {
-            if(nums[i] >= 0) {
-                result[positiveIdx] = nums[i];
-                positiveIdx+=2;
+            if(nums[i] < 0) {
+                result[negIndex] = nums[i];
+                negIndex+=2;
             } else {
-                result[negativeIdx] = nums[i];
-                negativeIdx+=2;
+                result[posIndex] = nums[i];
+                posIndex+=2;
             }
         }
 
         return result;
     }
-
     vector<int> rearrangeArray(vector<int>& nums) {
         // return rearrangeArray1(nums);
 
