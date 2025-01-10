aa6544d4c9889e3b75b35805488c44feaa654ead
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-10 08:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (5.07%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    
-    // Brute Force Approach with extra space
+    // Brute Force Approach using extra space
     // T.C. = O(m*n)*O(m+n)
     // S.C. = O(m*n)
     void setZeroes1(vector<vector<int>>& matrix) {
-        vector<vector<int>> temp(matrix);
+        vector<vector<int>> temp = matrix;
         int m = matrix.size();
         int n = matrix[0].size();
-        
+
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(matrix[i][j] == 0) {
@@ -21,180 +20,12 @@ public:
                 }
             }
         }
-        
+
         matrix = temp;
     }
     
-    // Brute Force Approach without extra space 
-    // (Can be used if matrix contains positive elements only, since -1 will conflict with the matrix values)
-    // T.C. = O(m*n)*O(m+n) + O(m*n)
-    // S.C. = O(1)
-    void setZeroes2(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == 0) {
-                    for(int k = 0; k < m; k++) {
-                        if(matrix[k][j] != 0)
-                            matrix[k][j] = -1;
-                    }
-                    for(int k = 0; k < n; k++) {
-                        if(matrix[i][k] != 0)
-                           matrix[i][k] = -1;
-                    }
-                }
-            }
-        }
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == -1)
-                    matrix[i][j] = 0;
-            }
-        }
-    }
-    
-    // Better Approach using row and col vectors
-    // T.C. = 2 * O(m*n)
-    // S.C. = O(m+n)
-    void setZeroes3(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> row(m, 0);
-        vector<int> col(n, 0);
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
-                }
-            }
-        }
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(row[i] == 1 || col[j] == 1)
-                    matrix[i][j] = 0;
-            }
-        }
-    }
-    
-    // Optimal Approach
-    // T.C. = 2 * O(m*n)
-    // S.C. = O(1)
-    void setZeroes4(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int col0 = 1;
-        
-        // Filling row vector (matrix[i][0]) and column vector (matrix[0][j], col0) with 0s
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == 0) {
-                    // Filling row vector
-                    matrix[i][0] = 0;
-                    
-                    // Filling column vector
-                    if(j != 0)
-                        matrix[0][j] = 0;
-                    else
-                        col0 = 0;
-                }
-            }
-        }
-        
-        // Setting remaining elements to 0s based on row and column vector values
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0)
-                    matrix[i][j] = 0;
-            }
-        }
-        
-        // Setting column vector values to 0s first based on matrix[0][0]
-        if(matrix[0][0] == 0) {
-            for(int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        
-        // Setting row vector values to 0s then based on col0
-        if(col0 == 0) {
-            for(int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
-        }
-    }
-    
-    // Another Optimal Approach
-    // T.C. = O(m*n) overall
-    // S.C. = O(1)
-    void setZeroes5(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        bool firstRowZero = false;
-        bool firstColZero = false;
-        
-        for(int i = 0; i < m; i++) {
-            if(matrix[i][0] == 0) {
-                firstColZero = true;
-                break;
-            }
-        }
-        
-        for(int j = 0; j < n; j++) {
-            if(matrix[0][j] == 0) {
-                firstRowZero = true;
-                break;
-            }
-        }
-        
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                if(matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-        
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        
-        if(firstRowZero) {
-            for(int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        if(firstColZero) {
-            for(int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
-        }
-    }
-    
     void setZeroes(vector<vector<int>>& matrix) {
-        // Brute Force Approach with extra space
-        // return setZeroes1(matrix);
-        
-        // Brute Force Approach without extra space
-        // return setZeroes2(matrix);
-        
-        // Better Approach using row and col vectors
-        // return setZeroes3(matrix);
-        
-        // Optimal Approach
-        // return setZeroes4(matrix);
-        
-        // Another Optimal Approach
-        return setZeroes5(matrix);
+        // Brute Force Approach using extra space
+        return setZeroes1(matrix);   
     }
 };
