95633aab5534adfdd5c4a108702f11d674d77960
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-01-11 20:26:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (19.31%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -83,6 +83,102 @@ public:
             }
         }
     }
+
+    // Optimal Approach
+    // T.C. = 2*O(m*n) + O(m+n)
+    // S.C. = O(1)
+    void setZeroes4(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int col0 = 1;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if(j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0) {
+            for(int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(col0 == 0) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    // Another Optimal Approach
+    // T.C. = O(m+n) + 2*O(m*n) + O(m+n)
+    // S.C. = O(1)
+    void setZeroes5(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        bool firstRowZero = false, firstColumnZero = false;
+
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) {
+                firstColumnZero = true;
+                break;
+            }
+        }
+
+        for(int j = 0; j < n; j++) {
+            if(matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(firstRowZero) {
+            for(int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(firstColumnZero) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
     
     void setZeroes(vector<vector<int>>& matrix) {
         // Brute Force Approach using extra space
@@ -92,6 +188,12 @@ public:
         // return setZeroes2(matrix);
 
         // Better Approach
-        return setZeroes3(matrix);
+        // return setZeroes3(matrix);
+
+        // Optimal Approach
+        // return setZeroes4(matrix);
+
+        // Another Optimal Approach
+        return setZeroes5(matrix);
     }
 };
