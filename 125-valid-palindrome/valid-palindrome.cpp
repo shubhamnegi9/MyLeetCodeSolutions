0f9bcda976b2ac75b61c893d04474da2279421ea
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-02-17 08:59:12 +0530

    Time: 4 ms (17.78%) | Memory: 10.6 MB (9.56%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
-    // Using Recursion
-    bool checkPalindrome(string s, int i, int j) {
-        if(i > j) {
-            return true;
-        }
-        
-        if(s[i] != s[j])
-            return false;
-        
-        return checkPalindrome(s, i+1, j-1);
-    }
-    
     bool isPalindrome(string s) {
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-        // cout << s << endl;
-        
-        string str;
-        for(char& ch: s) {
-            if(isalnum(ch))
-                str+=ch;
+        string temp = "";
+        for(char ch: s) {
+            if(isalnum(ch)) {
+                temp+=ch;
+            }
         }
-        // cout << str << endl;
-        int n = str.length();
-        
-        // Using Recursion
-        // return checkPalindrome(str, 0, n-1);
-        
-        // Using Loops
-        int i = 0, j = n-1;
-        while(i <= j) {
-            if(str[i] != str[j])
-                return false;
-            i++;
-            j--;
-        }
-        return true;
+        transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+        cout << temp << endl;
+        string reversed = temp;
+        reverse(reversed.begin(), reversed.end());
+        return reversed == temp;
     }
 };
