eddc734ebc3248c4e5379d7cbcc2287a7b81d138
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-04-27 12:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (50.71%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    // T.C. = O(n)
-    // S.C. = O(n) + O(n)
+    // Brute Force
+    // T.C. = O(nlogn) + O(n)
+    // S.C. = O(n)
     int removeDuplicates1(vector<int>& nums) {
-        set<int> st(nums.begin(), nums.end());
-        vector<int> temp(st.begin(), st.end());
-
-        int n = temp.size();       
-        for(int i = 0; i < n; i++) {
-            nums[i] = temp[i];
+        set<int> st;
+        for(int& ele: nums) {
+            st.insert(ele);
         }
-        return n;
+
+        int i = 0;
+        for(auto ele: st) {
+            nums[i] = ele;
+            i++;
+        }
+
+        return st.size();
     }
-    
+
+    // Optimal Approach
+    // T.C. = O(n)
+    // S.C. = O(1)
     int removeDuplicates2(vector<int>& nums) {
         int n = nums.size();
         int i = 0;
         for(int j = 1; j < n; j++) {
             if(nums[j] != nums[i]) {
+                nums[i+1] = nums[j];
                 i++;
-                swap(nums[i], nums[j]);
             }
         }
         return i+1;
     }
 
     int removeDuplicates(vector<int>& nums) {
+        // Brute Force
         // return removeDuplicates1(nums);
 
+        // Optimal Approach
         return removeDuplicates2(nums);
     }
 };
