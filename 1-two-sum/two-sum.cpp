e7ee38b51bbd12c82df1895f37ba21ee09dd5085
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-04 17:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (49.81%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
+    // Brute Force Approach
     // T.C. = O(n^2)
     // S.C. = O(1)
     vector<int> twoSum1(vector<int>& nums, int target) {
         int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j)
+                    continue;
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
 
+        return {-1, -1};
+    }
+
+    // Better Brute Force 
+    // T.C. = O(n^2)
+    // S.C. = O(1)
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        int n = nums.size();
         for(int i = 0; i < n; i++) {
             for(int j = i+1; j < n; j++) {
                 if(nums[i] + nums[j] == target) {
@@ -15,24 +33,33 @@ public:
 
         return {-1, -1};
     }
-
+    
+    // Optimal Approach
     // T.C. = O(n)
-    // S.C. = O(1)
-    vector<int> twoSum2(vector<int>& nums, int target) {
+    // S.C. = O(n)
+    vector<int> twoSum3(vector<int>& nums, int target) {
         int n = nums.size();
-        unordered_map<int, int> mp;
+        unordered_map<int, int> mpp;
 
         for(int i = 0; i < n; i++) {
-            if(mp.find(target-nums[i]) != mp.end()) {
-                return {mp[target-nums[i]], i};
+            int num = target - nums[i];
+            if(mpp.find(num) != mpp.end()) {
+                return {mpp[num], i};
             }
-            mp[nums[i]] = i;
+            mpp[nums[i]] = i;
         }
+
         return {-1, -1};
     }
+
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Brute Force Approach
         // return twoSum1(nums, target);
 
-        return twoSum2(nums, target);
+        // Better Brute Force 
+        // return twoSum2(nums, target);
+
+        // Optimal Approach
+        return twoSum3(nums, target);
     }
 };
