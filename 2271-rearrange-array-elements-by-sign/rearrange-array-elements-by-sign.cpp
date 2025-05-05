c1b10473a72237eab5cf467ed4e108e78ec85a38
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-05 22:06:36 +0530

    Time: 6 ms (64.29%) | Memory: 139 MB (26.86%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+    // Brute Force Approach
     // T.C. = O(n) + O(n/2)
-    // S.C. = O(n)
-    vector<int> rearrangeArray1(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> positives;
-        vector<int> negatives;
+    // S.C. = 2*O(n/2)
+    vector<int> rearrangeArray1(vector<int>& nums, int n) {
+        vector<int> positives, negatives;
 
-        for(int& num: nums) {
-            if(num < 0)
-                negatives.push_back(num);
+        for(int& ele: nums) {
+            if(ele >= 0)
+                positives.push_back(ele);
             else
-                positives.push_back(num);
+                negatives.push_back(ele); 
         }
 
         for(int i = 0; i < n/2; i++) {
@@ -21,29 +20,9 @@ public:
 
         return nums;
     }
-
-    // T.C. = O(n)
-    // S.C. = O(1)
-    vector<int> rearrangeArray2(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result(n, 0);
-        int posIndex = 0, negIndex = 1;
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] < 0) {
-                result[negIndex] = nums[i];
-                negIndex+=2;
-            } else {
-                result[posIndex] = nums[i];
-                posIndex+=2;
-            }
-        }
-
-        return result;
-    }
     vector<int> rearrangeArray(vector<int>& nums) {
-        // return rearrangeArray1(nums);
-
-        return rearrangeArray2(nums);
+        int n = nums.size();
+        // Brute Force Approach
+        return rearrangeArray1(nums, n);
     }
 };
