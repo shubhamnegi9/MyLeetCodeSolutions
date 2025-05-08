1f353d965e398667af4e5033f6db66142829bf38
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-08 23:34:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (69.81%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,11 +1,32 @@
 class Solution {
 public:
+
+    void solve(vector<int>& nums, vector<vector<int>>& result, vector<int>& ans, vector<int>& visited, int n) {
+        // Base Case
+        if(ans.size() == n) {
+            result.push_back(ans);
+            return;
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(visited[i] == 0) {
+                ans.push_back(nums[i]);
+                visited[i] = 1;
+                solve(nums, result, ans, visited, n);
+                
+                // Reset values while backtracking
+                visited[i] = 0;
+                ans.pop_back();
+            }
+        }
+    }
+
     vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> result;
-        sort(nums.begin(), nums.end());
-        do {
-            result.push_back(nums);
-        }  while (next_permutation(nums.begin(), nums.end()));
+        vector<int> ans;
+        vector<int> visited(n, 0);
+        solve(nums, result, ans, visited, n);
         return result;
     }
 };
