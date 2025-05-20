2faa1df3a4434479bcd2fcffdd7d3b804e76fc07
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-20 22:48:44 +0530

    Time: 16 ms (86.71%) | Memory: 60 MB (92.71%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    bool isFoundUsingLS(vector<int>& nums, int x) {
-        for(int& ele: nums) {
-            if(ele == x)
+    bool isNextElementPresent(vector<int>& nums, int ele) {
+        for(int& num: nums) {
+            if(ele == num)
                 return true;
         }
+
         return false;
     }
 
@@ -12,30 +13,33 @@ public:
     // T.C. = O(n^2)
     // S.C. = O(1)
     int longestConsecutive1(vector<int>& nums, int n) {
-        int maxCount = 0;
+        int maxLen = 0;
+
         for(int i = 0; i < n; i++) {
-            int count = 1;
-            int x = nums[i];
-            while(isFoundUsingLS(nums, x+1)) {
-                count++;
-                x++;
-            }       
-            maxCount = max(maxCount, count);    
+            int currEle = nums[i];
+            int len = 1;
+            while(isNextElementPresent(nums, currEle+1)) {
+                currEle++;
+                len++;
+            }
+            maxLen = max(maxLen, len);
         }
 
-        return maxCount;
+        return maxLen;
     }
 
-    bool isFoundUsingBS(vector<int>& nums, int x, int low, int high) {
-        while(low <= high) {
-            int mid = low + (high-low)/2;
+    bool isNextElementPresentBinary(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n-1;
 
-            if(nums[mid] == x) {
+        while(low <= high) {
+            int mid = low+(high-low)/2;
+            if(nums[mid] == target)
                 return true;
-            } else if(nums[mid] > x) {
-                high = mid-1;
-            } else {
+            else if(target > nums[mid]) {
                 low = mid+1;
+            } else {
+                high = mid-1;
             }
         }
 
@@ -43,90 +47,60 @@ public:
     }
 
     // Better Brute Force Approach
-    // T.C. = 2*O(nlogn)
+    // T.C. = O(nlogn) for sorting + O(n*logn)
     // S.C. = O(1)
     int longestConsecutive2(vector<int>& nums, int n) {
-        int maxCount = 0;
+        int maxLen = 0;
 
+        // Sort the array to perform binary search
         sort(nums.begin(), nums.end());
 
         for(int i = 0; i < n; i++) {
-            int count = 1;
-            int x = nums[i];
-            while(isFoundUsingBS(nums, x+1, 0, n-1)) {
-                count++;
-                x++;
-            }       
-            maxCount = max(maxCount, count);    
+            int currEle = nums[i];
+            int len = 1;
+            while(isNextElementPresentBinary(nums, currEle+1)) {
+                currEle++;
+                len++;
+            }
+            maxLen = max(maxLen, len);
         }
 
-        return maxCount;
+        return maxLen;
     }
 
     // Better Approach
-    // T.C. = O(nlogn) + O(n)
+    // T.C. = O(nlogn) for sorting + O(n)
     // S.C. = O(1)
     int longestConsecutive3(vector<int>& nums, int n) {
-        int maxCount = INT_MIN, count = 0, lastEle = INT_MAX;
-
-        if(n == 0)
-            return 0;
-
+        // Sort the array to bring consecutive elements together
         sort(nums.begin(), nums.end());
 
+        int maxLen = 0, len = 0, lastEle = INT_MIN;
+
         for(int i = 0; i < n; i++) {
-            if(nums[i]-1 == lastEle) {
-                count++;
-                lastEle = nums[i];
-            } else if(nums[i] != lastEle) {
-                count = 1;
+            if(nums[i] == lastEle+1) {
+                len++;
                 lastEle = nums[i];
             }
-            maxCount = max(maxCount, count);
-        }
-
-        return maxCount;
-    }
-    
-    // Optimal Approach
-    // T.C. = O(n) + O(2n)
-    // S.C. = O(n)
-    int longestConsecutive4(vector<int>& nums, int n) {
-        int maxCount = 1, count = 0;
-
-        if(n == 0)
-            return 0;
-
-        unordered_set<int> st(nums.begin(), nums.end());
-
-        // Iterate in set (Not in given array)
-        for(auto ele: st) { 
-            if(st.find(ele-1) == st.end()) {
-                int currEle = ele;
-                count = 1;
-                while(st.find(currEle+1) != st.end()) {
-                    count++;
-                    currEle++;
-                }
-                maxCount = max(maxCount, count);
+            else if(nums[i] != lastEle) {
+                len = 1;
+                lastEle = nums[i];
             }
+            maxLen = max(maxLen, len);
         }
 
-        return maxCount;
+        return maxLen;
     }
     
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
         // Brute Force Approach
-        // return longestConsecutive1(nums, n);
+        // return  longestConsecutive1(nums, n);
 
         // Better Brute Force Approach
         // return longestConsecutive2(nums, n);
 
         // Better Approach
-        // return longestConsecutive3(nums, n);
-
-        // Optimal Approach
-        return longestConsecutive4(nums, n);
+        return longestConsecutive3(nums, n);
     }
 };
