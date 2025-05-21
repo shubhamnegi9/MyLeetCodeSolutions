7761e0e713989b538216d96e4095e26717e6f00a
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-21 22:34:06 +0530

    Time: 5 ms (16.61%) | Memory: 26.4 MB (8.52%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -21,10 +21,32 @@ public:
 
         return result;
     }
+
+    // Better Approach
+    // T.C. = O(n)
+    // S.C. = O(n) for map
+    vector<int> majorityElement2(vector<int>& nums, int n) {
+        unordered_map<int, int> mpp;
+        vector<int> result;
+
+        for(int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
+            if(mpp[nums[i]] == (int)n/3+1) {
+                result.push_back(nums[i]);
+            }
+            if(result.size() == 2)
+                break;
+        }
+
+        return result;
+    }
     
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
         // Brute Force Approach
-        return majorityElement1(nums, n);
+        // return majorityElement1(nums, n);
+
+        // Better Approach
+        return majorityElement2(nums, n);
     }
 };
