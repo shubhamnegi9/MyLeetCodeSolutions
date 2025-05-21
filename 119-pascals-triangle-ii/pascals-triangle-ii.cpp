85a10884643d6bfc5677df1228750f5d520bcd4a
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-21 21:44:11 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (53.69%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,91 +1,32 @@
 class Solution {
 public:
     
-    int findnCr(int n, int r) {
-        long long res = 1;
-        
+    int findNcR(int n, int r) {
+        long long ans = 1;
+
         for(int i = 0; i < r; i++) {
-            res = (long long) res * (n-i);  // Handle signed int overflow
-            res /= (i+1);
+            ans *= (n-i);
+            ans /= (i+1);
         }
-        
-        return (int) res;
+
+        return (int) ans;
     }
-    
-    // Brute Force Approach 1
-    // T.C. = O(rowIndex*rowIndex)
-    // S.C. = O(1) extra space
+
+    // Brute Force Approach
+    // T.C. = O(n*r)
+    // S.C. = O(n)
     vector<int> getRow1(int rowIndex) {
-        vector<int> result(rowIndex+1);
-        
-        for(int c = 1; c <= rowIndex+1; c++) {
-            result[c-1] = findnCr(rowIndex, c-1);
+        vector<int> result;
+        int n = rowIndex+1;
+
+        for(int col = 1; col <= n; col++) {
+            result.push_back(findNcR(n-1, col-1));
         }
-        
         return result;
     }
     
-    // Brute Force Approach 2
-    // T.C. = O(rowIndex*rowIndex)
-    // S.C. = O(rowIndex*rowIndex) extra space
-    vector<int> getRow2(int rowIndex) {
-        vector<vector<int>> result(rowIndex+1);
-            
-        for(int i = 0; i < rowIndex+1; i++) {
-            result[i] = vector<int> (i+1, 1);
-            for(int j = 1; j < i; j++) {
-                result[i][j] = result[i-1][j] + result[i-1][j-1]; 
-            }
-        }
-        
-        return result[rowIndex];
-    }
-    
-    // Better Approach space-wise
-    // T.C. = O(rowIndex*rowIndex)
-    // S.C. = O(1) extra space
-    vector<int> getRow3(int rowIndex) {
-        vector<int> prev;
-        vector<int> curr(rowIndex+1, 1);
-        for(int i = 0; i < rowIndex+1; i++) {
-            for(int j = 1; j < i; j++) {
-                curr[j] = prev[j] + prev[j-1]; 
-            }
-            prev = curr;
-        }
-        
-        return curr;
-    }
-    
-    // Optimal Approach
-    // T.C. = O(rowIndex)
-    // S.C. = O(1) extra space
-    vector<int> getRow4(int rowIndex) {
-        int n = rowIndex+1;
-        long long res = 1;
-        vector<int> ans;
-        ans.push_back((int) res);
-        
-        for(int i = 1; i < n; i++) {
-            res = res * (n-i);
-            res /= i;
-            ans.push_back((int) res);
-        }
-        
-        return ans;
-    }
-    
     vector<int> getRow(int rowIndex) {
-        // Brute Force Approach 1
-        // return getRow1(rowIndex);
-        
-        // Brute Force Approach 2
-        // return getRow2(rowIndex);
-        
-        // Better Approach space-wise
-        // return getRow3(rowIndex);
-        
-        // Optimal Approach
-        return getRow4(rowIndex);
+        // Brute Force Approach
+        return getRow1(rowIndex);
     }
 };
