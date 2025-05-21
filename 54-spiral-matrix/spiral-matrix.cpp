768a4583c85c0ed256c2b7c8d248e7d111b932bd
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-21 09:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.47%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,96 +1,40 @@
 class Solution {
 public:
-    // Optimal Approach 1
-    vector<int> spiralOrder1(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int left = 0, right = n-1, top =- 0, bottom = m-1;
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        int top = 0, bottom = m-1, left = 0, right = n-1;
         vector<int> result;
-        
-        while(left <= right && top <= bottom) {
-            
+
+        while(top <= bottom && left <= right) {
             // Right
-            for(int i = left; i <= right; i++) {
-                result.push_back(matrix[top][i]);
+            for(int j = left; j <= right; j++) {
+                result.push_back(matrix[top][j]);
             }
             top++;
-            
-            // Down
+
+            // Bottom
             for(int i = top; i <= bottom; i++) {
                 result.push_back(matrix[i][right]);
             }
             right--;
-            
-            // Left
+
             if(top <= bottom) {
-                for(int i = right; i >= left; i--) {
-                    result.push_back(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-            
-            // Top
-            if(left <= right) {
-                for(int i = bottom; i >= top; i--) {
-                    result.push_back(matrix[i][left]);
-                }
-                left++;
-            }
-        }
-        
-        return result;
-    }
-    
-    // Optimal Approach 2
-    vector<int> spiralOrder2(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int left = 0, right = n-1, top =- 0, bottom = m-1;
-        vector<int> result;
-        int id = 0;
-        
-        while(left <= right && top <= bottom) {
-            
-            if(id == 0) {
-                // Right
-                for(int i = left; i <= right; i++) {
-                    result.push_back(matrix[top][i]);
-                }
-                top++;
-            }
-            else if(id == 1) {
-                // Down
-                for(int i = top; i <= bottom; i++) {
-                    result.push_back(matrix[i][right]);
-                }
-                right--;
-            }
-            else if(id == 2) {
                 // Left
-                for(int i = right; i >= left; i--) {
-                    result.push_back(matrix[bottom][i]);
+                for(int j = right; j >= left; j--) {
+                    result.push_back(matrix[bottom][j]);
                 }
                 bottom--;
             }
-            else {
+
+            if(left <= right) {
                 // Top
                 for(int i = bottom; i >= top; i--) {
                     result.push_back(matrix[i][left]);
                 }
                 left++;
             }
-            
-            id = (id+1)%4;
         }
-        
+
         return result;
-    }
-    
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        // Optimal Approach 1
-        // return spiralOrder1(matrix);
-        
-        // Optimal Approach 2
-        return spiralOrder2(matrix);
     }
 };
