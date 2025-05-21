ed623135cd3d5420593f51debd2735c2d4126a40
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-21 23:32:48 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (23.67%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -40,6 +40,48 @@ public:
 
         return result;
     }
+
+    // Optimal Approach
+    // T.C. = O(n) + O(n)
+    // S.C. = O(1) 
+    vector<int> majorityElement3(vector<int>& nums, int n) {
+        vector<int> result;
+        int ele1 = INT_MIN, ele2 = INT_MIN;
+        int count1 = 0, count2 = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(count1 == 0 && nums[i] != ele2) {
+                count1 = 1;
+                ele1 = nums[i];
+            }
+            else if(count2 == 0 && nums[i] != ele1) {
+                count2 = 1;
+                ele2 = nums[i];
+            }
+            else if(nums[i] == ele1) {
+                count1++;
+            } else if(nums[i] == ele2) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0, count2 = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == ele1)
+                count1++;
+            if(nums[i] == ele2)
+                count2++;
+        }
+        if(count1 > (int)n/3)
+            result.push_back(ele1);
+        if(count2 > (int)n/3)
+            result.push_back(ele2);
+
+        return result;        
+    }
     
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
@@ -47,6 +89,9 @@ public:
         // return majorityElement1(nums, n);
 
         // Better Approach
-        return majorityElement2(nums, n);
+        // return majorityElement2(nums, n);
+
+        // Optimal Approach
+        return majorityElement3(nums, n);
     }
 };
