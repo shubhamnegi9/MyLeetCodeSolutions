fce492af6098237f7b4e5829ce7939c12177a596
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-21 09:32:23 +0530

    Time: 60 ms (40.99%) | Memory: 51 MB (23.89%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,50 +1,69 @@
 class Solution {
 public:
+    
     // Brute Force Approach
+    // T.C. = O(n^3)
+    // S.C. = O(1)
+    int subarraySum1(vector<int>& nums, int k, int n) {
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                int sum = 0;
+                for(int l = i; l <= j; l++) {
+                    sum += nums[l];
+                }
+                if(sum == k)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    // Better Approach
     // T.C. = O(n^2)
     // S.C. = O(1)
-    int subarraySum1(vector<int>& nums, int k) {
-        int n = nums.size();
+    int subarraySum2(vector<int>& nums, int k, int n) {
         int count = 0;
-        
         for(int i = 0; i < n; i++) {
             int sum = 0;
             for(int j = i; j < n; j++) {
                 sum += nums[j];
-                if(sum == k) {
+                if(sum == k)
                     count++;
-                }
             }
         }
-        
+
         return count;
     }
-    
+
     // Optimal Approach
     // T.C. = O(n)
-    // S.C. = o(n)
-    int subarraySum2(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int, int> mp;
-        mp[0] = 1;
-        
-        int sum = 0, count = 0;
+    // S.C. = O(n) for map
+    int subarraySum3(vector<int>& nums, int k, int n) {
+        int count = 0, sum = 0;
+        unordered_map<int, int> mpp;
+        mpp[0] = 1;
+
         for(int i = 0; i < n; i++) {
-            sum+=nums[i];
-            if(mp.find(sum-k) != mp.end()) {
-                count+=mp[sum-k];
-            }
-            mp[sum]++;
+            sum += nums[i];
+            count += mpp[sum-k];
+            mpp[sum]++;
         }
-        
+
         return count;
     }
     
     int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+
         // Brute Force Approach
-        // return subarraySum1(nums, k);
-        
+        // return subarraySum1(nums, k, n);
+
+        // Better Approach
+        // return subarraySum2(nums, k, n);
+
         // Optimal Approach
-        return subarraySum2(nums, k);
+        return subarraySum3(nums, k, n);
     }
 };
