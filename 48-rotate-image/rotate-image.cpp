f1871375bad55ccbfffe6cc997bbe39c01a01b80
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-21 08:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (68.65%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     // Brute Force Approach
-    // T.C. = O(n*n)
-    // S.C. = O(n*n)
+    // T.C. = O(n^2)
+    // S.C. = O(n^2)
     void rotate1(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<vector<int>> result(n, vector<int> (n, 0));
-        
+        int n = matrix.size();    
+        vector<vector<int>> result(n, vector<int>(n, 0));
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 result[j][n-1-i] = matrix[i][j];
             }
         }
-        
+
         matrix = result;
     }
-    
+
     // Optimal Approach
-    // T.C. = 2*O(n*n)
+    // T.C. = 2*O(n^2)
     // S.C. = O(1)
     void rotate2(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        
-        // Finding transpose of matrix
+
+        // Finding tramspose
         for(int i = 0; i < n; i++) {
             for(int j = i; j < n; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        
-        // Reverse each row of matrix
-        for(vector<int> &row: matrix) {
-            reverse(row.begin(), row.end());
+
+        // Reverse each row
+        for(int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
-    
+
     void rotate(vector<vector<int>>& matrix) {
         // Brute Force Approach
-        // return rotate1(matrix);
-        
+        // rotate1(matrix);
+
         // Optimal Approach
-        return rotate2(matrix);
+        rotate2(matrix);
     }
 };
