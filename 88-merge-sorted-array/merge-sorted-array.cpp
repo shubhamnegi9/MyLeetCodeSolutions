36d520176eeabbd4806f5b4bcf7cb5b618621ea6
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-24 19:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (8.86%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -33,7 +33,7 @@ public:
     }
 
     // Optimal Approach
-    // T.C. = O(m+n) + O(n)
+    // T.C. = O(min(m,n)) + O(mlogm) + O(nlogn) + O(n)
     // S.C. = O(1)
     void merge2(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m-1, j = 0;
@@ -51,6 +51,51 @@ public:
         sort(nums1.begin(), nums1.begin()+m);
         sort(nums2.begin(), nums2.end());
 
+        // Move remaining n elements from nums2 to nums1
+        for(int i = m; i < m+n; i++) {
+            nums1[i] = nums2[i-m];
+        }
+    }
+
+    void swapIfGreater(vector<int>& nums1, vector<int>& nums2, int i, int j) {
+        if(nums1[i] > nums2[j])
+            swap(nums1[i], nums2[j]);
+    }
+
+    // Optimal Approach using gap method
+    // T.C. = O(log(m+n))*O(m+n) + O(n)
+    // S.C. = O(1)
+    void merge3(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len = m+n;
+        int gap = len/2 + len%2;
+
+        while(gap > 0) {
+            int i = 0, j = i+gap;
+
+            while(j < len) {
+                // i is in first array and j is in second array
+                if(i < m && j >= m) {
+                    swapIfGreater(nums1, nums2, i, j-m);
+                }
+                // i and j both are in second array
+                else if(i >= m) {
+                    swapIfGreater(nums2, nums2, i-m, j-m);
+                }
+                // i and j both are in first array
+                else {
+                    swapIfGreater(nums1, nums1, i, j);
+                }
+                i++;
+                j++;
+            }
+
+            if(gap == 1)
+                break;
+            
+            gap = gap/2 + gap%2;
+        }
+
+        // Move remaining n elements from nums2 to nums1
         for(int i = m; i < m+n; i++) {
             nums1[i] = nums2[i-m];
         }
@@ -61,6 +106,9 @@ public:
         // return merge1(nums1, m, nums2, n);
 
         // Optimal Approach
-        return merge2(nums1, m, nums2, n);
+        // return merge2(nums1, m, nums2, n);
+
+        // Optimal Approach using gap method
+        return merge3(nums1, m, nums2, n);
     }
 };
