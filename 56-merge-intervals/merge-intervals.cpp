df1f98a2619b0db39aada55f2c647ad0744c3805
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-24 18:07:57 +0530

    Time: 8 ms (28.91%) | Memory: 23.8 MB (80.30%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,51 +1,68 @@
 class Solution {
 public:
-    
+    // Brute Force Approach
+    // T.C. = O(nlogn) + O(n^2)
+    // S.C. = O(1) for solving problem
     vector<vector<int>> merge1(vector<vector<int>>& intervals) {
         int n = intervals.size();
+        vector<vector<int>> result;
+
+        // Sort the intervals based on start times
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> mergedIntervals;
 
         for(int i = 0; i < n; i++) {
-            if(mergedIntervals.empty() || intervals[i][0] > mergedIntervals.back()[1]) {
-                int start = intervals[i][0];
-                int end = intervals[i][1];
+            int start = intervals[i][0];
+            int end = intervals[i][1];
 
-                if(i < n-1) {
-                    for(int j = i+1; j < n; j++) {
-                        if(intervals[j][0] <= end) {
-                            end = max(end, intervals[j][1]);
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                mergedIntervals.push_back({start, end});
+            // Current interval is already present in merged interval, so skip it
+            if(!result.empty() && end <= result.back()[1]) {
+                continue;
             }
+
+            for(int j = i+1; j < n; j++) {
+                if(intervals[j][0] <= end) {
+                    // Current interval can be added to merged interval
+                    end = max(end, intervals[j][1]);
+                } else {
+                    // Current interval cannot be added to merged interval. Also skip further intervals
+                    break;
+                }
+            }
+
+            result.push_back({start, end});
         }
 
-        return mergedIntervals;
+        return  result;
     }
-    
+
+    // Optimal Approach
+    // T.C. = O(nlogn) + O(n)
+    // S.C. = O(1) for solving problem
     vector<vector<int>> merge2(vector<vector<int>>& intervals) {
         int n = intervals.size();
+        vector<vector<int>> result;
+
+        // Sort the intervals based on start times
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> mergedIntervals;
 
         for(int i = 0; i < n; i++) {
-            if(mergedIntervals.empty() || intervals[i][0] > mergedIntervals.back()[1]) 
-                mergedIntervals.push_back(intervals[i]);
-            else {
-                mergedIntervals.back()[1] = max(mergedIntervals.back()[1], intervals[i][1]);
+            // Current interval cannot be merged to previous interval
+            if(result.empty() || (!result.empty() && intervals[i][0] > result.back()[1])) {
+                result.push_back(intervals[i]);
+            } else {
+                // Current interval can be merged to previous interval
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
             }
         }
 
-        return mergedIntervals;
+        return result;
     }
     
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // Brute Force Approach
         // return merge1(intervals);
 
+        // Optimal Approach
         return merge2(intervals);
     }
 };
