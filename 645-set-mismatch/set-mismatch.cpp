ef7886e18847d7b4704d4cfcab7109be18309451
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-24 19:28:19 +0530

    Time: 30 ms (25.44%) | Memory: 34.7 MB (21.76%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,94 +1,62 @@
 class Solution {
 public:
-    // Better Approach Using hashing
-    // T.C. = O(2n), S.C. = O(n)
-    vector<int> findErrorNumsBetter(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> hash(n+1);
-        for(int i = 0; i < n; i++) { 
-            hash[nums[i]]++;
+    // Brute Force Approach
+    // T.C. = O(n^2)
+    // S.C. = O(1)
+    vector<int> findErrorNums1(vector<int>& nums, int n) {
+        int repeating = -1, missing = -1;
+
+        for(int ele = 1; ele <= n; ele++) {
+            int count = 0;
+            for(int i = 0; i < n; i++) {
+                if(nums[i] == ele) {
+                    count++;
+                }
+            }
+            if(count == 0) {
+                missing = ele;
+            }
+            else if(count == 2) {
+                repeating = ele;
+            }
+            if(missing != -1 && repeating != -1) {
+                break;
+            }
         }
 
-        int repeating = -1, missing = -1;
-        for(int i = 1; i <=n; i++) {
-            if(hash[i] == 2)
-                repeating = i;
-            if(hash[i] == 0)
-                missing = i;
-            
-            if(repeating != -1 && missing != -1)
-                break;
-        }
         return {repeating, missing};
     }
 
-    // Optimal Approach
-    // T.C. = O(n), S.C. = O(1)
-    vector<int> findErrorNumsOptimal(vector<int>& nums) {
-        int n = nums.size();
-        long long Sn = n*(n+1)/2;
-        long long S2n = ((long long)n * (long long)(n+1) * (long long)(2*n+1))/6;
+    // Better Approach
+    // T.C. = O(n) + O(n)
+    // S.C. = O(n) for map
+    vector<int> findErrorNums2(vector<int>& nums, int n) {
+        int repeating = -1, missing = -1;
+        unordered_map<int, int> mpp;
 
-        long long S = 0, S2 = 0;
-        for(int i = 0; i < n; i++) {
-            S += nums[i];
-            S2+= (long long)nums[i] * (long long)nums[i];
-        }
-        long long diff = S - Sn;              // repeating - missing
-        long long add = (S2 - S2n) / diff;    // repeating + missing
-        long long repeating = (diff + add)/2;
-        long long missing = repeating - diff;
-        return {(int) repeating,  (int) missing};
-
-    }
-
-    // Optimal Approach using XOR
-    vector<int> findErrorNumsOptimal2(vector<int>& nums) {
-        
-        int XR = 0, n = nums.size();
-
-        for(int i = 0; i < n; i++) {
-            XR ^= nums[i];
-            XR ^= (i+1);
+        for(int& ele: nums) {
+            mpp[ele]++;
         }
 
-        int rightmostSetBit = XR & ~(XR-1);
-        
-        int oneBit = 0, zeroBit = 0;
-        for(int i = 0; i < n; i++) {
-            if((nums[i] & rightmostSetBit) != 0) {
-                oneBit ^= nums[i];
-            } else {
-                zeroBit ^= nums[i];
-            }
-
-            if(((i+1) & rightmostSetBit) != 0) {
-                oneBit ^= (i+1);
-            } else {
-                zeroBit ^= (i+1);
+        for(int num = 1; num <= n; num++) {
+            if(mpp[num] == 0)
+                missing = num;
+            else if(mpp[num] == 2)
+                repeating = num;
+            if(missing != -1 && repeating != -1) {
+                break;
             }
         }
 
-        int count = 0;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == zeroBit)
-                count++;
-        }
-
-        if(count == 0)  // zeroBit is missing
-            return {oneBit, zeroBit};   
-        else    // zeroBit is repeating
-            return {zeroBit, oneBit};
+        return {repeating, missing};
     }
-
+    
     vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        // Brute Force Approach
+        // return findErrorNums1(nums, n);
+
         // Better Approach
-        // return findErrorNumsBetter(nums);
-
-        // Optimal Approach using sum
-        // return findErrorNumsOptimal(nums);
-
-        // Optimal Approach using XOR
-        return findErrorNumsOptimal2(nums);
+        return findErrorNums2(nums, n);
     }
 };
