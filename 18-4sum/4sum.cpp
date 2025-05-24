cf7d6db674bb153da5be02581a1404cb98696e0b
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-24 17:36:17 +0530

    Time: 20 ms (69.70%) | Memory: 17.3 MB (84.69%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    
-    vector<vector<int>> fourSum1(vector<int>& nums, int target) {
-        int n = nums.size();
-        set<vector<int>> st; // To store unique quadruplets
+    // Better Approach
+    // T.C. = O(n^3)*O(m), m = no. of unique quards
+    // S.C. = O(m) for set + O(n) for hashSet + O(m) for result vector
+    vector<vector<int>> fourSum1(vector<int>& nums, int target, int n) {
+        set<vector<int>> st;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                unordered_set<long long> hashSet; // Use long long to prevent overflow
-                for (int k = j + 1; k < n; k++) {
-                    long long sum = static_cast<long long>(nums[i]) + nums[j] + nums[k];
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                unordered_set<long long> hashSet;
+                for(int k = j+1; k < n; k++) {
+                    long long sum = static_cast<long long>(nums[i])+nums[j]+nums[k];
                     long long fourth = static_cast<long long>(target) - sum;
-
-                    if (hashSet.find(fourth) != hashSet.end()) {
-                        vector<int> quad = {nums[i], nums[j], nums[k], static_cast<int>(fourth)};
-                        sort(quad.begin(), quad.end());
-                        st.insert(quad);
+                    if(hashSet.find(fourth) != hashSet.end()) {
+                        vector<int> v = {nums[i], nums[j], nums[k], static_cast<int>(fourth)};
+                        sort(v.begin(), v.end());
+                        st.insert(v);
                     }
                     hashSet.insert(nums[k]);
                 }
             }
         }
 
-        return vector<vector<int>>(st.begin(), st.end());
+        vector<vector<int>> result(st.begin(),st.end());
+        return result;
     }
     
-    vector<vector<int>> fourSum2(vector<int>& nums, int target) {
-        int n = nums.size();
+    // Optimal Approach
+    // T.C. = O(nlogn) + O(n^2)*O(n)
+    // S.C. = O(m) for result vector
+    vector<vector<int>> fourSum2(vector<int>& nums, int target, int n) {
         vector<vector<int>> result;
 
         sort(nums.begin(), nums.end());
@@ -34,25 +37,22 @@ public:
         for(int i = 0; i < n; i++) {
             if(i > 0 && nums[i] == nums[i-1])
                 continue;
-            for(int j = i + 1; j < n; j++) {
-                if(j > i+1 && nums[j] == nums[j-1])
+            for(int j = i+1; j < n; j++) {
+                if(j != i+1 && nums[j] == nums[j-1])
                     continue;
-                int k = j+1;
-                int l = n-1;
+                
+                int k = j+1, l = n-1;
+
                 while(k < l) {
                     long long sum = static_cast<long long>(nums[i])+nums[j]+nums[k]+nums[l];
-
-                    if(sum > target) {
-                        l--;
-                    }
-                    else if(sum < target) {
+                    if(sum < target) 
                         k++;
-                    }
+                    else if(sum > target) 
+                        l--;
                     else {
                         result.push_back({nums[i], nums[j], nums[k], nums[l]});
                         k++;
                         l--;
-
                         while(k < l && nums[k] == nums[k-1]) {
                             k++;
                         }
@@ -68,8 +68,12 @@ public:
     }
     
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        // return fourSum1(nums, target);
+        int n = nums.size();
 
-        return fourSum2(nums, target);
+        // Better Approach
+        // return fourSum1(nums, target, n);
+
+        // Optimal Approach
+        return fourSum2(nums, target, n);
     }
 };
