2e845e50cf84fe19b59bdb1ca08f806c06d97a06
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-26 20:04:29 +0530

    Time: 3 ms (15.58%) | Memory: 17.8 MB (57.23%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    // Brute Force
+    // Brute Force Approach
     // T.C. = O(n^2)
     // S.C. = O(1)
-    int maxProduct1(vector<int>& nums) {
-        int n = nums.size();
+    int maxProduct1(vector<int>& nums, int n) {
         int maxProd = INT_MIN;
         for(int i = 0; i < n; i++) {
             int prod = 1;
@@ -19,8 +18,7 @@ public:
     // Optimal Approach 1
     // T.C. = O(n)
     // S.C. = O(1)
-    int maxProduct2(vector<int>& nums) {
-        int n = nums.size();
+    int maxProduct2(vector<int>& nums, int n) {
         int maxProd = INT_MIN;
         int prefixProd = 1, suffixProd = 1;
 
@@ -29,42 +27,21 @@ public:
                 prefixProd = 1;
             if(suffixProd == 0)
                 suffixProd = 1;
-
+                
             prefixProd *= nums[i];
-            suffixProd *= nums[n-1-i];
-            maxProd = max(maxProd, max(prefixProd, suffixProd));    // or maxProd = max({maxProd, prefixProd, suffixProd});
+            suffixProd *= nums[n-i-1];
+            maxProd = max({maxProd, prefixProd, suffixProd});
         }
 
         return maxProd;
     }
-
-    // Optimal Approach 2
-    // T.C. = O(n)
-    // S.C. = O(1)
-    int maxProduct3(vector<int>& nums) {
-        int n = nums.size();
-        int currMax = nums[0];
-        int currMin = nums[0];
-        int maxProd = nums[0];
-
-        for(int i = 1; i < n; i++) {
-            int temp = max({nums[i], nums[i]*currMax, nums[i]*currMin});
-            currMin = min({nums[i], nums[i]*currMax, nums[i]*currMin});
-            currMax = temp;
-            maxProd = max(maxProd, currMax);
-        }
-
-        return maxProd;
-    }
-
+    
     int maxProduct(vector<int>& nums) {
-        // Brute Force
-        // return maxProduct1(nums);
+        int n = nums.size();
+        // Brute Force Approach
+        // return maxProduct1(nums, n);
 
         // Optimal Approach 1
-        // return maxProduct2(nums);
-
-        // Optimal Approach 2
-        return maxProduct3(nums);
+        return maxProduct2(nums, n);
     }
 };
