886ee3971f8835cbfc1e8e2f0cce1d0d28bde1ad
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-26 20:11:19 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (80.00%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    // Iterative Way
-    int search1(vector<int>& nums, int target, int n) {
-        int low = 0, high = n-1;
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
 
         while(low <= high) {
-            int mid = low + (high-low)/2;
-
+            int mid = low+(high-low)/2;
+            
             if(nums[mid] == target) {
                 return mid;
-            } else if(target > nums[mid]) {
+            } else if(nums[mid] < target) {
                 low = mid+1;
             } else {
                 high = mid-1;
@@ -17,31 +16,5 @@ public:
         }
 
         return -1;
-    }
-
-    // Recursive Way
-    int search2(vector<int>& nums, int low, int high, int target) {
-        // Base case
-        if(low > high)
-            return -1;
-        
-        int mid = low + (high-low)/2;
-        if(nums[mid] == target) {
-            return mid;
-        } else if(target > nums[mid]) {
-            return search2(nums, mid+1, high, target);
-        } else {
-            return search2(nums, low, mid-1, target);
-        }
-    }
-    
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        // Iterative Way
-        // return search1(nums, target, n);
-
-        // Recursive Way
-        return search2(nums, 0, n-1, target);
     }
 };
