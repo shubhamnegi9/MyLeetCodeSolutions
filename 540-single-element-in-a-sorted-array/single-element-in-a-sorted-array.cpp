a31af0ca0122a7032c2264da4e5ebdd498edab4f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-29 08:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (67.70%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -25,9 +25,47 @@ public:
 
         return -1;
     }
+
+    // Optimal Approach
+    // T.C. = O(logn)
+    // S.C. = O(1)
+    int singleNonDuplicate2(vector<int>& nums) {
+        int n = nums.size();
+
+        // Edge cases
+        if(n == 1)
+            return nums[0];
+        if(nums[0] != nums[1])
+            return nums[0];
+        if(nums[n-1] != nums[n-2])
+            return nums[n-1];
+        
+        int low = 1, high = n-2;
+
+        while(low <= high) {
+            int mid = low+(high-low)/2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) 
+                return nums[mid];
+            
+            // We are on left half of mid (even, odd)
+            if((mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 == 1 && nums[mid] == nums[mid-1])) {
+                low = mid+1;        // Eliminate left half
+            } 
+            // We are on right half of mid (odd, even)
+            else {
+                high = mid-1;       // Eliminate right half
+            }
+        }
+
+        return -1;
+    }
     
     int singleNonDuplicate(vector<int>& nums) {
         // Brute Force Approach
-        return singleNonDuplicate1(nums);
+        // return singleNonDuplicate1(nums);
+
+        // Optimal Approach
+        return singleNonDuplicate2(nums);
     }
 };
