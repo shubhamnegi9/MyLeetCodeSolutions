0f99680b85247ed64c031a3c5a63a167a6ff7197
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-06-05 23:35:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (66.72%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -15,9 +15,51 @@ public:
 
         return -1;
     }
+
+    // Optimal Approach
+    // T.C. = O(logn)
+    // S.C. = O(1)
+    int findPeakElement2(vector<int>& nums) {
+        int n = nums.size();
+        // Single element is peak
+        if(n == 1)
+            return 0;
+        
+        // First element is peak
+        if(nums[0] > nums[1])
+            return 0;
+        
+        // Last element is peak
+        if(nums[n-1] > nums[n-2])
+            return n-1;
+
+        int low = 1, high = n-2;
+
+        while(low <= high) {
+            int mid = low+(high-low)/2;
+
+            // Element at mid is peak
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+                return mid;
+            }
+            // mid is on increasing slope
+            else if(nums[mid] > nums[mid-1]) {
+                low = mid+1;
+            }
+            // mid is on decreasing slope or at the inverted peak
+            else {
+                high = mid-1;
+            }
+        }
+
+        return -1;
+    }
     
     int findPeakElement(vector<int>& nums) {
         // Brute Force Approach
-        return findPeakElement1(nums);
+        // return findPeakElement1(nums);
+
+        // Optimal Approach
+        return findPeakElement2(nums);
     }
 };
