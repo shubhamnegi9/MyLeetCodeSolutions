ecda562819a09c7703b753c22ee01d92d0a68f52
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-06-15 17:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (59.66%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -15,8 +15,31 @@ public:
         return k;
     }
 
+    // Optimal Approach
+    // T.C. = O(logn)
+    // S.C. = O(1)
+    int findKthPositive2(vector<int>& arr, int k) {
+        int n = arr.size();
+        int low = 0, high = n-1;
+
+        while(low <= high) {
+            int mid = low+(high-low)/2;
+            int missing = arr[mid] - (mid+1);   // Number of missing numbers till mid
+
+            if(missing < k) 
+                low = mid+1;    // Eliminate left half of mid
+            else
+                high = mid-1;   // Elimintae right half of mid
+        }
+
+        return high+1+k;    // OR return (low+k);
+    }
+
     int findKthPositive(vector<int>& arr, int k) {
         // Brute Force Approach
-        return findKthPositive1(arr, k);
+        // return findKthPositive1(arr, k);
+
+        // Optimal Approach
+        return findKthPositive2(arr, k);
     }
 };
