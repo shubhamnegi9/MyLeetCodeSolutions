8fde3fbec76b1f0c16fedbeba1895ab321b3c4ab
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-06-22 12:25:50 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (43.46%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -22,9 +22,52 @@ public:
 
         return {-1, -1};
     }
-    
+
+    int findMaxElementRow(vector<vector<int>>& mat, int m, int n, int col) {
+        int maxEle = -1, row = 0;
+        for(int i = 0; i < m; i++) {    
+            if(mat[i][col] > maxEle) {
+                maxEle = mat[i][col];
+                row = i;
+            }
+        }
+
+        return row;
+    }
+
+    // Optimal approach
+    // T.C. = O(m * logn)
+    // S.C. = O(1)
+    vector<int> findPeakGrid2(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        int low = 0, high = n-1;
+
+        while(low <= high) {
+            int mid = (low+high)/2; 
+            // Finding row index of maximum element in mid column
+            int row = findMaxElementRow(mat, m, n, mid);
+            // Checking for out of bounds
+            int left = (mid-1 >= 0) ? mat[row][mid-1] : -1;
+            int right = (mid+1 < n) ? mat[row][mid+1] : -1;
+            
+            if(mat[row][mid] > left && mat[row][mid] > right) {
+                return {row, mid};
+            } else if(left > mat[row][mid]) {
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+
+        return {-1, -1};
+    }
+
+
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         // Brute Force Approach
-        return findPeakGrid1(mat);
+        // return findPeakGrid1(mat);
+
+        // Optimal approach
+        return findPeakGrid2(mat);
     }
 };
