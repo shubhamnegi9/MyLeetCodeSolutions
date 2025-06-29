a88c557045d4bd1e2dc8703042a0974be0dec7aa
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-06-29 20:59:32 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (25.38%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -34,8 +34,28 @@ public:
         return temp;
     }
     
+    // Optimal Approach
+    // T.C. = O(n/2)
+    // S.C. = O(1)
+    ListNode* middleNode2(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // We need to move the pointers until fast != NULL (In case of even length LL) 
+        // and until fast->next != NULL (In case of odd length LL)  
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+    
     ListNode* middleNode(ListNode* head) {
         // Brute Force Approach
-        return middleNode1(head);
+        // return middleNode1(head);
+
+        // Optimal Approach
+        return middleNode2(head);
     }
 };
