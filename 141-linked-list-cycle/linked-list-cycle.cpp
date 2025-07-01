9c351721e50c517d0c7bc4d0529e080a8aa1fd84
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-07-01 10:31:29 +0530

    Time: 20 ms (5.13%) | Memory: 15.1 MB (7.05%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,63 +8,27 @@
  */
 class Solution {
 public:
-    // Using Brute Force
-    // T.C. = O(n), S.C. = O(n)
-    bool hasCycleUsingBrute(ListNode *head) {
+    // Brute Force Approach
+    // T.C. = O(n) * O(1) for unordered map
+    // S.C. = O(n) for map
+    bool hasCycle1(ListNode *head) {
+        ListNode* temp = head;
+        unordered_map<ListNode*, int> mpp;
 
-        // Empty node and single node cannot make cycle
-        if(head == NULL || head->next == NULL) {
-            return false;
-        }
-
-        // Set for storing the nodes traversed so far
-        set<ListNode*> s;
-
-        while(head) {
-            // If node found in set, then we have reached to same node again.
-            // This node is begining of cycle.
-            if(s.find(head) != s.end()) {
+        while(temp != NULL) {
+            if(mpp.find(temp) != mpp.end()) {
                 return true;
-            }
-            // Insert current node in set
-            s.insert(head);
-            head = head->next;
+            } 
+
+            mpp[temp] = 1;
+            temp = temp->next;
         }
 
-        // No cycle is present 
         return false;
     }
-
-    // Using Slow and Fast Pointers
-    bool hasCycleUsingOptimal(ListNode* head) {
-        // Empty node and single node cannot make cycle
-        if(head == NULL || head->next == NULL) {
-            return NULL;
-        }
-
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        while(fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            // Cycle is detected in linked list
-            if(slow == fast)
-                return true;
-        }
-
-        // In case while loop is not returned due to slow==fast, 
-        // it means that there is no cycle in linked list.
-        return false;
-    }
-
+    
     bool hasCycle(ListNode *head) {
-        // Using Brute Force
-        // return hasCycleUsingBrute(head);
-
-        // Using Optimal Approach
-        return hasCycleUsingOptimal(head);
-
+        // Brute Force Approach
+        return hasCycle1(head);
     }
 };
