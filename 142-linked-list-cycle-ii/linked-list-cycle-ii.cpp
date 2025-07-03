df26bd28514b691b4f627000d2f6ca9efe4dca04
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-07-03 22:30:40 +0530

    Time: 15 ms (7.30%) | Memory: 13.7 MB (8.48%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -8,73 +8,28 @@
  */
 class Solution {
 public:
-    // Using Brute Force
-    // T.C. = O(n), S.C. = O(n)
-    ListNode* detectCycleUsingBrute(ListNode *head) {
+    // Brute Force Approach
+    // T.C. = O(n) * O(2*logn) for fetching and inserting in ordered map
+    //      = O(n) * O(2) for fetching and inserting in unordered map
+    // S.C. = O(n) for map
+    ListNode *detectCycle1(ListNode *head) {
+        ListNode* temp = head;
+        unordered_map<ListNode*, int> mpp;
 
-        // Empty node and single node cannot make cycle
-        if(head == NULL || head->next == NULL) {
-            return NULL;
+        while(temp) {
+            if(mpp.find(temp) != mpp.end()) {
+                return temp;
+            } 
+            
+            mpp[temp] = 1;
+            temp = temp->next;
         }
 
-        // Set for storing the nodes traversed so far
-        set<ListNode*> s;
-
-        while(head) {
-            // If node found in set, then we have reached to same node again.
-            // This node is begining of cycle.
-            if(s.find(head) != s.end()) {
-                return head;
-            }
-            // Insert current node in set
-            s.insert(head);
-            head = head->next;
-        }
-
-        // No cycle is present 
         return NULL;
     }
-
-    // Using Slow and Fast Pointers
-    ListNode* detectCycleUsingOptimal(ListNode* head) {
-        // Empty node and single node cannot make cycle
-        if(head == NULL || head->next == NULL) {
-            return NULL;
-        }
-
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        while(fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            // Cycle is detected in linked list
-            if(slow == fast)
-                break;
-        }
-
-        // In case while loop is not break due to slow==fast, 
-        // it means that there is no cycle in linked list.
-        if(slow != fast)
-            return NULL;
-        
-        // At this point, it is confirmed that cycle must be present.
-        // Initialize pointer p and move slow and p pointers by one step.
-        // The point where they both meet must be the beginning node of cycle.
-        ListNode* p = head;
-        while(p != slow) {
-            p = p->next;
-            slow = slow->next;
-        }
-        return p;    // or return slow;
-    }
-
+    
     ListNode *detectCycle(ListNode *head) {
-        // Using Brute Force
-        // return detectCycleUsingBrute(head);
-
-        // Using Optimal Approach
-        return detectCycleUsingOptimal(head);
+        // Brute Force Approach
+        return detectCycle1(head);
     }
 };
