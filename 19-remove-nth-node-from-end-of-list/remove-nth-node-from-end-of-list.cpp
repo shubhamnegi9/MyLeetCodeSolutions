8c4a9fc1d9a28daa50e44bf6abc43632507a29e7
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-07-06 11:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (91.73%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,104 +10,44 @@
  */
 class Solution {
 public:
-    /*
-        Using Two Passes
-        Time Complexity = O(len) + O(len-n)     
-                        = O(2*len) in worst case
-        Space Complexity = O(1)                
-        len = length of Linked List, n = nth node to be deleted from end 
-    */
-    ListNode* removeUsingTwoPass(ListNode* head, int n) {
-        // For tracking previous node
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        int sz = 0;
+    // Brute Force Approach
+    ListNode* removeNthFromEnd1(ListNode* head, int n) {
+        ListNode* temp = head;
+        int count = 0;
 
-        // Finding the size of LL 
-        while(curr) {
-            sz+=1;
-            curr = curr->next;
+        while(temp != NULL) {
+            count++;
+            temp = temp->next;
         }
 
-        // Node to remove from front/beginning
-        int nodeToRemove = sz - n;
-        // For tracking current node 
-        int currNode = 0;
-        curr = head;
-
-        if(nodeToRemove == 0) {
-            // This is the first node in LL to be removed
-            // Simply move head to next node to remove it
+        // Edge Case
+        if(n == count) {
+            // Remove node at head 
             ListNode* newHead = head->next;
-            delete (head);  // Delete the memory allocated to head node
+            delete head;
             return newHead;
         }
 
-        // Iterate till the end of LL
-        while(curr) {
-            if(currNode == nodeToRemove) {
-                // Link prev node to next node of curr to remove curr node
-                ListNode* temp = curr;
-                prev->next = curr->next;
-                curr = curr->next;
-                delete(temp);   // Delete the memory allocated to curr node
+        int res = count-n;
+        temp = head;
+        while(temp != NULL) {
+            // First reduce res then check for (res==0)
+            res--;
+            if(res == 0) {
                 break;
             }
-            else {
-                currNode++;
-                // Make curr node as prev node before moving to next
-                prev = curr;
-                curr = curr->next;
-            }
+            temp = temp->next;
         }
 
+        // At this point, temp will be pointing to previous node of the node to be removed
+        ListNode* delNode = temp->next;
+        temp->next = temp->next->next;
+        delete delNode;
         return head;
     }
-
-    /*
-        Using One Pass
-        T.C. = O(n) for the fast pointer
-        S.C. = O(1)
-    */
-    ListNode* removeUsingOnePass(ListNode* head, int n) {
-        // Fast and Slow Pointers
-        ListNode* fast = head;
-        ListNode* slow = head;
-
-        // Moving fast pointer to 'n' nodes ahead 
-        for(int i = 0; i < n; i++) {
-            fast = fast->next;
-        }
-
-        // If fast pointer reaches after end of LL
-        // (in case n = length of LL)
-        if(fast == NULL) {
-            // Node to be removed from end is the first node in the LL
-            // Move head to next node to remove it and return new head
-            ListNode* newHead = head->next;
-            delete(head);
-            return newHead;
-        }
-
-        // Iterate until fast pointer is pointing to last node
-        while(fast->next) {
-            // Move fast and slow pointer simultaneously
-            fast = fast->next;
-            slow = slow->next;
-        }
-
-        // slow pointer is now pointing to one node before the node to be deleted from end
-        ListNode* nodeToDelete = slow->next;
-        // Linking to node which is next to the node to be deleted 
-        slow->next = slow->next->next;
-        delete(nodeToDelete);
-
-        return head;
-    }
-
+    
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // return removeUsingTwoPass(head, n);
-
-        return removeUsingOnePass(head, n);
+        // Brute Force Approach
+        return removeNthFromEnd1(head, n);
     }
 };
