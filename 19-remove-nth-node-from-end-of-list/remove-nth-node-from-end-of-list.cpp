e964560ade620794be64c606232a93ea05255e97
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-07-06 11:20:29 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (24.54%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,6 +11,8 @@
 class Solution {
 public:
     // Brute Force Approach
+    // T.C. = O(len) + O(len-n) = 2*O(len) if n=1
+    // S.C. = O(1)
     ListNode* removeNthFromEnd1(ListNode* head, int n) {
         ListNode* temp = head;
         int count = 0;
@@ -46,8 +48,44 @@ public:
         return head;
     }
     
+    // Optimal Approach
+    // T.C. = O(n) + O(len-n) = O(len)
+    // S.C. = O(1)
+    ListNode* removeNthFromEnd2(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        // Move fast pointer by n steps
+        for(int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+
+        // Edge Case
+        if(fast==NULL) {
+            // Remove head node
+            ListNode* newHead = head->next;
+            delete head;
+            return newHead;
+        }
+
+        // Move slow and fast pointers by 1 step until fast pointer reaches at last node
+        while(fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        // At this point, slow pointer will be pointing to previous node of the node to be removed
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delete delNode;
+        return head;
+    }
+    
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         // Brute Force Approach
-        return removeNthFromEnd1(head, n);
+        // return removeNthFromEnd1(head, n);
+
+        // Optimal Approach
+        return removeNthFromEnd2(head, n);
     }
 };
