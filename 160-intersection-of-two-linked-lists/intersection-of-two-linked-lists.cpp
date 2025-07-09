ead2b042f66f7d99af111b4122fe9d0e891145bf
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-07-09 23:43:40 +0530

    Time: 28 ms (96.73%) | Memory: 18.4 MB (81.69%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -78,12 +78,42 @@ public:
             return findCollisionPoint(headB, headA, n1-n2);
         }
     }
+
+    // Optimal Approach
+    // T.C. = O(n1+n2)
+    // S.C. = O(1)
+    ListNode *getIntersectionNode3(ListNode *headA, ListNode *headB) {
+        // Edge Case
+        if(headA == NULL || headB == NULL)
+            return NULL;
+        
+        ListNode* t1 = headA;
+        ListNode* t2 = headB;
+
+        while(t1 != t2) {
+            t1 = t1->next;
+            t2 = t2->next;
+
+            if(t1 == t2)
+                return t1;      // OR  return t2;
+            
+            if(t1==NULL)                
+                t1 = headB;
+            if(t2==NULL)
+                t2 = headA;
+        }
+
+        return t1;      // OR  return t2;
+    }
     
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         // Brute Force Approach
         // return getIntersectionNode1(headA, headB);
 
         // Better Approach
-        return getIntersectionNode2(headA, headB);
+        // return getIntersectionNode2(headA, headB);
+
+        // Optimal Approach
+        return getIntersectionNode3(headA, headB);
     }
 };
