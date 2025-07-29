92a34e2876335ecba6efabc2159067c78498d2f0
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-07-29 22:38:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (98.59%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // Approach 1 
+    int hammingWeight1(int n) {
+        int count = 0;
+        while(n > 1) {
+            count += (n&1);     // Equivalent to:  if(n%2==1) count++
+            n >>= 1;            // Equivalent to:  n = n/2; 
+        }
+        if(n==1)
+            count++;
+
+        return count;
+    }
+
+    int hammingWeight2(int n) {
+        int count = 0;
+        while(n) {
+            n = n & (n-1);
+            count++; 
+        }
+
+        return count;
+    }
+    
+    int hammingWeight(int n) {
+        // Approach 1 
+        return hammingWeight1(n);
+
+        // Approach 2
+        // return hammingWeight2(n);
+
+        // Approach 3 (Using STL function)
+        // return __builtin_popcount(n);
+
+    }
+};
