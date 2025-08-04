9d080b792f159ae0326c509c4373af86ccd14e07
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-08-05 00:11:36 +0530

    Time: 16 ms (8.99%) | Memory: 24 MB (5.84%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        int XOR = 0;
-        for(int ele: nums) {
-            XOR ^= ele;
+    // Brute Force Approach
+    // T.C. = O(n) + O(m)
+    // S.C. = O(m)
+    int singleNumber1(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+
+        for(int& num: nums) {
+            mpp[num]++;
         }
-        
-        return XOR;
+
+        for(auto it: mpp) {
+            if(it.second == 1) {
+                return it.first;
+            }
+        }
+
+        return -1;
+    }
+    
+    int singleNumber(vector<int>& nums) {
+        // Brute Force Approach
+        return singleNumber1(nums);
     }
 };
