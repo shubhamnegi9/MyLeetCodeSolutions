2c6c0fb81173069077be9917e34c30c7f9ca241f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-08-09 00:27:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (80.29%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    // Brute Force Approach
+    // T.C. = O(n) + O(m), m = size of map = n/2+1
+    // S.C. = O(m)
+    vector<int> singleNumber1(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+
+        for(int& num: nums) {
+            mpp[num]++;
+        }
+
+        vector<int> ans;
+        for(auto it: mpp) {
+            if(it.second == 1) {
+                ans.push_back(it.first);
+            }
+        }
+
+        return ans;
+    }
+
+    // Optimal Approach
+    // T.C. = O(2n)
+    // S.C. = O(1)
+    vector<int> singleNumber2(vector<int>& nums) {
+        long XORR = 0;
+
+        for(int& num: nums) {
+            XORR ^= num;
+        }
+
+        int rightMost = (XORR & (XORR-1)) ^ XORR;
+
+        int b1 = 0, b2 = 0;
+        for(int& num: nums) {
+            if(num & rightMost) {
+                b1 ^= num;
+            } else {
+                b2 ^= num;
+            }
+        }
+
+        return {b1, b2};
+    }
+    
+    vector<int> singleNumber(vector<int>& nums) {
+        // Brute Force Approach
+        // return singleNumber1(nums);
+
+        // Optimal Approach
+        return singleNumber2(nums);
+    }
+};
