a754a7c8bc48806941511e3f9e340f315e7f170f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-08-13 00:23:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (26.73%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -2,49 +2,34 @@ class MyQueue {
 public:
     stack<int> s1;
     stack<int> s2;
-    int peekEle;
-    
     MyQueue() {
         
     }
     
-    // T.C. = O(1), S.C. = O(n)
     void push(int x) {
-        // Saving first element in s1 as peek element
-        if(s1.empty())
-            peekEle = x;
-        s1.push(x);   // T.C. = O(1)
+        while(!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+        while(!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
     
-    // Amortized T.C. = O(1)    (Most of the operations taken O(1) compared to O(n))
-    // S.C. = O(1)
     int pop() {
-        if(s2.empty()) {           // T.C. = O(n)
-            // Move all elements from s1 to s2
-            while(!s1.empty()) {
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
-
-        int poppedEle = s2.top();   // T.C. = O(1)
-        s2.pop();                   
-        return poppedEle;
+        int x = s1.top();
+        s1.pop();
+        return x;
     }
     
-    // T.C. = O(1), S.C. = O(1)
     int peek() {
-        // If s2 is not empty, return top of s2
-        if(!s2.empty()) {       
-            return s2.top();    // T.C. = O(1)
-        }
-
-        return peekEle;         // T.C. = O(1)
+        return s1.top();
     }
     
-    // T.C. = O(1), S.C. = O(1)
     bool empty() {
-        return s1.empty() && s2.empty();
+        return s1.empty();
     }
 };
 
