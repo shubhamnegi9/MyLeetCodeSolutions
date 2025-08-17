6b251040ff09a8cc5661e58de49416584b2afa78
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-08-17 14:18:02 +0530

    Time: 1 ms (85.03%) | Memory: 28.5 MB (62.51%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -19,9 +19,35 @@ public:
 
         return nge;
     }
+
+    // Optimal Approach
+    // T.C. = O(2n) + O(2n) = O(4n)
+    // S.C. = O(2n) for stack + O(n) for NGE array 
+    vector<int> nextGreaterElements2(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> nge(n, -1);
+        stack<int> st;
+
+        for(int i = 2*n-1; i >= 0; i--) {
+            while(!st.empty() && st.top() <= nums[i%n]) {   // Use (i%n) in place of i 
+                st.pop();
+            }
+
+            if(i < n) {
+                nge[i] = st.empty() ? -1 : st.top();    // NGE to only be stored for elements in range 0 to n-1
+            }
+
+            st.push(nums[i%n]);  // Use (i%n) in place of i 
+        }
+
+        return nge;
+    }
     
     vector<int> nextGreaterElements(vector<int>& nums) {
         // Brute Force Approach
-        return nextGreaterElements1(nums);
+        // return nextGreaterElements1(nums);
+
+        // Optimal Approach
+        return nextGreaterElements2(nums);
     }
 };
