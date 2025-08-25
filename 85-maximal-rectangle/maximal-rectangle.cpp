c152b7d9f63a5b77a8149cd66330b9134dadefcb
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-08-25 23:38:12 +0530

    Time: 6 ms (66.63%) | Memory: 18.2 MB (63.84%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -29,9 +29,10 @@ public:
         return maxArea;
     }
 
+    // Optimal Approach using prefix sum matrix
     // T.C. = O(m*n) + O(n*2*m)
     // S.C. = O(m*n) for pSumMatrix + O(m) for stack
-    int maximalRectangle(vector<vector<char>>& matrix) {
+    int maximalRectangle1(vector<vector<char>>& matrix) {
         int n = matrix.size();      // rows 
         int m = matrix[0].size();   // cols
         vector<vector<int>> pSumMatrix(n, vector<int>(m, 0));       // Prefix Sum Matrix of type int
@@ -53,5 +54,43 @@ public:
         }
 
         return maxArea;
+    }
+
+    // Space Optimized Approach using prefix sum array
+    // T.C. = O(m) + O(2m) + O(n*(m+2m))
+    // S.C. = O(m) for pSumArray + O(m) for stack
+    int maximalRectangle2(vector<vector<char>>& matrix) {
+        int n = matrix.size();      // rows 
+        int m = matrix[0].size();   // cols
+        vector<int> pSumArr(m);       // Prefix Sum Array of type int
+
+        for(int i = 0; i < m; i++) {
+            pSumArr[i] = matrix[0][i] - '0';       // Copying first row of matrix to pSumArray
+        }
+
+        int maxArea = largestRectangleArea(pSumArr);
+
+        for(int i = 1; i < n; i++) {            // Computing pSumArray from second row onwards
+            for(int j = 0; j < m; j++) {
+                int val = matrix[i][j] - '0';
+                if(val == 1) {
+                    pSumArr[j]+=1;
+                }
+                else {
+                    pSumArr[j] = 0;
+                }
+            }
+            maxArea = max(maxArea, largestRectangleArea(pSumArr));
+        }
+
+        return maxArea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        // Optimal Approach using prefix sum matrix
+        // return maximalRectangle1(matrix);
+
+        // Space Optimized Approach using prefix sum array
+        return maximalRectangle2(matrix);
     }
 };
