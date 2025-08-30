6d0814965749182a49ae77ead0418519fd6d47fc
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-08-30 13:58:49 +0530

    Time: 3 ms (83.13%) | Memory: 11.5 MB (83.58%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     // Brute Force Approach
+    // T.C. = O(n^2)
+    // S.C. = O(256)
     int lengthOfLongestSubstring1(string s) {
         int n = s.length();
         int maxLen = 0;
         for(int i = 0; i < n; i++) {
-            int hash[256] = {0};    // Reset/define the hash array whenever we have new starting point of substring
+            int hash[256];    
+            memset(hash, 0, sizeof(hash)); // Reset/define the hash array whenever we have new starting point of substring
             for(int j = i; j < n; j++) {
-                if(hash[s[j]] == 1)     // Repeating character
+                if(hash[s[j]] == 1)     // s[j] is repeating character
                     break;
                 
                 int len = j-i+1;
@@ -19,8 +22,37 @@ public:
         return maxLen;
     }
     
+    // Optimal Approach
+    // T.C. = O(n)
+    // S.C. = O(256)
+    int lengthOfLongestSubstring2(string s) {
+        int n = s.length();
+        int maxLen = 0;
+        int l = 0, r = 0;
+        int hash[256];
+        memset(hash, -1, sizeof(hash));    
+
+        while(r < n) {
+            if(hash[s[r]] != -1) {      // s[r] is present in hash array
+                if(hash[s[r]] >= l) {   // s[r] is repeating character
+                    l = hash[s[r]]+1;       // move l to one position ahead of hash[s[r]]
+                }
+            }
+
+            int len = r-l+1;
+            maxLen = max(maxLen, len);
+            hash[s[r]] = r;             // Store index r of char at s[r] in hash array
+            r++;
+        }
+
+        return maxLen;
+    }
+    
     int lengthOfLongestSubstring(string s) {
         // Brute Force Approach
-        return lengthOfLongestSubstring1(s);
+        // return lengthOfLongestSubstring1(s);
+
+        // Optimal Approach
+        return lengthOfLongestSubstring2(s);
     }
 };
