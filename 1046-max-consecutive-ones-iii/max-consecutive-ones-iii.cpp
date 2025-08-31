dddcebae5f19f27b17a6cfb50329f03029d3b35b
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-08-31 17:40:20 +0530

    Time: 0 ms (100.00%) | Memory: 69.7 MB (16.38%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -0,0 +1,86 @@
+class Solution {
+public:
+    // Brute Force Approach
+    // T.C. = O(n^2)
+    // S.C. = O(1)
+    int longestOnes1(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxLen = 0;
+
+        for(int i = 0; i < n; i++) {
+            int zeroes = 0;     // Reset zeroes when starting point of subarray changes  
+            for(int j = i; j < n; j++) {
+                if(nums[j] == 0) {
+                    zeroes++;
+                }
+                if(zeroes <= k) {
+                    maxLen = max(maxLen, j-i+1);
+                } else {
+                    break;      // Break from further iteration as we don't want the subarrays having no. of zeros > k
+                }
+            }
+        }
+        
+        return maxLen;
+    }
+
+    // Better Approach
+    // T.C. = O(2n)
+    // S.C. = O(1)
+    int longestOnes2(vector<int>& nums, int k) {
+        int l = 0, r = 0, maxLen = 0, n = nums.size(), zeroes = 0;
+
+        while(r < n) {
+            if(nums[r] == 0)
+                zeroes++;
+            
+            while(l < r && zeroes > k) {
+                if(nums[l] == 0)
+                    zeroes--;
+                l++;
+            }
+
+            if(zeroes <= k)
+                maxLen = max(maxLen, r-l+1);
+            r++;
+        }
+
+        return maxLen;
+    }
+
+    // Optimal Approach
+    // T.C. = O(n)
+    // S.C. = O(1)
+    int longestOnes3(vector<int>& nums, int k) {
+        int l = 0, r = 0, maxLen = 0, n = nums.size(), zeroes = 0;
+
+        while(r < n) {
+            if(nums[r] == 0)
+                zeroes++;
+            
+            if(l < r && zeroes > k) {
+                if(nums[l] == 0)
+                    zeroes--;
+                l++;
+            }
+
+            if(zeroes <= k)
+                maxLen = max(maxLen, r-l+1);
+
+            r++;
+        }
+
+        return maxLen;
+    }
+    
+    int longestOnes(vector<int>& nums, int k) {
+        // Brute Force Approach
+        // return longestOnes1(nums, k);
+
+        // Better Approach
+        // return longestOnes2(nums, k);
+
+        // Optimal Approach
+        return longestOnes3(nums, k);
+    }
+};
