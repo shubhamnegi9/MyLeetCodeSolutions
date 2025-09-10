cd5251a67b7141e2b17f83979938b8ad53cc0fe4
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-09-10 13:31:30 +0530

    Time: 7 ms (66.52%) | Memory: 11.9 MB (7.94%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,49 +1,89 @@
 class Solution {
 public:
+    // Brute Force Approach
+    // T.C. = O(n^2) * O(m)
+    // S.C. = O(256) for hash array
+    string minWindow1(string s, string t) {
+        int n = s.length(), m = t.length();
+        int sIndex = -1, minLen = INT_MAX;
+
+        for(int i = 0; i < n; i++) {
+            // Reset hashArr as well as count for substring with new starting point
+            vector<int> hashArr(256, 0);
+            int count = 0;
+            
+            // Maintain the count of characters in hashArr b/w i to n-1
+            for(int j = 0; j < m; j++) {
+                hashArr[t[j]]++;
+            }
+
+            for(int j = i; j < n; j++) {
+                // Before reducing count of s[j], check if count of s[j] > 0, then increment count
+                if(hashArr[s[j]] > 0) {
+                    count++;
+                }
+                hashArr[s[j]]--;
+
+                if(count == m) {
+                    // Update minLen and sIndex
+                    if(j-i+1 < minLen) {
+                        minLen = j-i+1;
+                        sIndex = i;
+                    }
+                }
+            }
+        }
+
+        return (sIndex == -1) ? "" : s.substr(sIndex, minLen);
+    }
+
+    // Optimal Approach
+    // T.C. = O(2n) for sliding window + O(m) for store the count of characters of t in map
+    // S.C. = O(256) for map/ hashArr
+    string minWindow2(string s, string t) {
+        int n = s.length(), m = t.length();
+        int l = 0, r = 0, count = 0, sIndex = -1, minLen = INT_MAX;
+        unordered_map<int, int> mpp;    // Can use hashArr also
+
+        // Store the count of characters of t in map
+        for(int i = 0; i < m; i++) {
+            mpp[t[i]]++;
+        }
+
+        while(r < n) {
+            // Before reducing count of s[r], check if count of s[r] > 0, then increment count
+            if(mpp[s[r]] > 0) {
+                count++;
+            }
+            mpp[s[r]]--;
+
+            // Shrinking the window until count == m
+            while(count == m) {
+                // Update minLen and sIndex
+                if(r-l+1 < minLen) {
+                    minLen = r-l+1;
+                    sIndex = l;
+                }
+
+                mpp[s[l]]++;
+                // After increasing count of s[l], check if count of s[l] > 0, then decrement count
+                if(mpp[s[l]] > 0) {
+                    count--;
+                }
+                l++;
+            }
+
+            r++;
+        }
+
+        return (sIndex == -1) ? "" : s.substr(sIndex, minLen);
+    }
+    
     string minWindow(string s, string t) {
-        
-        int n = s.length();
+        // Brute Force Approach
+        // return minWindow1(s, t);
 
-
-        if(t.size() > n)    // s cannot contain all chars of t
-            return "";  
-        
-        int i = 0, j = 0;
-        int requiredCount = t.length();
-        int minWindowSize = INT_MAX;
-        int start_i = 0;
-
-        unordered_map<char, int> mpp;
-        // Storing count of all chars of t in map
-        for(char &ch: t) {
-            mpp[ch]++;
-        }
-
-        while(j < n) {
-            if(mpp[s[j]] > 0) {
-                requiredCount--;
-            }
-            mpp[s[j]]--;
-
-            while(requiredCount == 0) {     // Window found
-                int currWindowSize = j-i+1;
-                if(currWindowSize < minWindowSize) {
-                    minWindowSize = currWindowSize;  // Finding min window size
-                    start_i = i;    // Initialize start_i as start index of the min size window only 
-                }
-
-                mpp[s[i]]++;
-
-                if(mpp[s[i]] > 0) {
-                    requiredCount++;
-                }
-                i++;    // Shrink Window
-            }
-
-            j++;    // Expand Window
-        }
-
-        return (minWindowSize == INT_MAX) ? "" : s.substr(start_i, minWindowSize);
-        // s.substr(start_i, minWindowSize) : Return 'minWindowSize' characters from start_i 
+        // Optimal Approach
+        return minWindow2(s, t);
     }
 };
