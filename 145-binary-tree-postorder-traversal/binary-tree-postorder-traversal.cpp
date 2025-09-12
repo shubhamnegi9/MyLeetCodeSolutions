635085f888f72488269dd060c8195b90570a8c96
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-09-12 22:26:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (92.40%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -41,8 +41,32 @@ public:
         return result;
 
     }
+
+    vector<int> postorderTraversalUsing1Stack(TreeNode* root) {
+        vector<int> result;
+        if(root == NULL)
+            return result;
+        
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            if(node->left)
+                st.push(node->left);
+            if(node->right)
+                st.push(node->right);
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
     
     vector<int> postorderTraversal(TreeNode* root) {
-        return postorderTraversalUsing2Stack(root);
+        // return postorderTraversalUsing2Stack(root);
+
+        return postorderTraversalUsing1Stack(root);
     }
 };
