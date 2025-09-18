54ebff13020fac63d00fd1b201276013b9aac8bd
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-09-19 00:45:26 +0530

    Time: 3 ms (39.38%) | Memory: 16.4 MB (28.13%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root == NULL)
+            return ans;
+        
+        queue<pair<TreeNode*, pair<int, int>>> q;    // We need to store 3 things thats why
+        map<int, map<int, multiset<int>>> mpp;
+
+        q.push({root, {0, 0}});      // Root node is at horizontal distance 0 and vertical distance 0
+
+        while(!q.empty()) {
+            auto p = q.front();
+            q.pop();
+
+            TreeNode* node = p.first;
+            int x = p.second.first;     // Horizontal distance of node
+            int y = p.second.second;    // Vertical distance of node
+
+            mpp[x][y].insert(node->val);    // node->val is added in multiset 
+
+            if(node->left) {
+                q.push({node->left, {x-1, y+1}});
+            }
+
+            if(node->right) {
+                q.push({node->right, {x+1, y+1}});
+            }
+        }
+
+        for(auto p: mpp) {
+            vector<int> col;
+            for(auto q: p.second) {
+                // One way to push in col vector is to iterate in q.second (multiset) and push in col vector using push_back()
+                // Another way to push in col vector is by below using iterators
+                // Using insert method, we need to specify at which position we need to insert in col vector
+                col.insert(col.end(), q.second.begin(), q.second.end());
+            }
+            ans.push_back(col);
+        }
+
+        return ans;
+    }
+};
