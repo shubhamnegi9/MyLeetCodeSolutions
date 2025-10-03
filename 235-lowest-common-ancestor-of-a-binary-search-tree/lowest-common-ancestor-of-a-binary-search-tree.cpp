a7cd339a3982f689ac59e4e1fb438191e649f44b
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-10-03 19:42:00 +0530

    Time: 20 ms (77.08%) | Memory: 23.6 MB (33.49%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,7 +10,10 @@
 
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    // Using recursive approach
+    // T.C. = O(h)
+    // S.C. = O(h) recursive stack space
+    TreeNode* lowestCommonAncestor1(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL)
             return NULL;
         
@@ -18,15 +21,55 @@ public:
 
         // Both p and q are present in right subtree
         if(p->val > currVal && q->val > currVal) {
-            return lowestCommonAncestor(root->right, p, q);
+            return lowestCommonAncestor1(root->right, p, q);
         }
 
         // Both p and q are present in left subtree
         if(p->val < currVal && q->val < currVal) {
-            return lowestCommonAncestor(root->left, p, q);
+            return lowestCommonAncestor1(root->left, p, q);
         }
 
         // root is LCA
         return root;
+    }
+
+    // Using iterative approach
+    // T.C. = O(h)
+    // S.C. = O(1)
+    TreeNode* lowestCommonAncestor2(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL)
+            return NULL;
+        
+        while(root != NULL) {
+            int currVal = root->val;
+
+            // Both p and q are present in right subtree
+            if(p->val > currVal && q->val > currVal) {
+                root = root->right;
+            }
+
+            // Both p and q are present in left subtree
+            else if(p->val < currVal && q->val < currVal) {
+                root = root->left;
+            }
+
+            // Otherwise, current node is the split point => LCA
+            else {
+                // root is LCA
+                return root;
+            }
+        }
+
+        return NULL;
+    }
+
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // Using recursive approach
+        // return lowestCommonAncestor1(root, p, q);
+
+        // Using iterative approach
+        return lowestCommonAncestor2(root, p, q);
+
     }
 };
