85bc048b13a493a03f2a1d1c4d0fcc45942cafd9
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-10-16 21:14:33 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (79.75%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,19 +3,20 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
-
-        int i = 0, j = 0, m = nums1.size(), n = nums2.size();
+        int n1 = nums1.size(), n2 = nums2.size();
+        int i = 0, j = 0;
         vector<int> result;
 
-        while(i < m && j < n) {
+        while(i < n1 && j < n2) {
             if(nums1[i] < nums2[j]) {
                 i++;
-            } else if(nums2[j] < nums1[i]) {
+            }
+            else if(nums2[j] < nums1[i]) {
                 j++;
-            } else {
-                if(result.size() == 0 || nums1[i] != result.back()) {
+            }
+            else {
+                if(result.empty() || result.back() != nums1[i])
                     result.push_back(nums1[i]);
-                }
                 i++;
                 j++;
             }
